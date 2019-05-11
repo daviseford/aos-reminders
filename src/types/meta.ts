5c95f106badb42990cf6203cf93897ea1b82220d
Davis Ford <davis.e.ford@gmail.com>
2019-05-11 18:23:00 -0400

    Initial commit

---
+++ src/types/meta.ts
@@ -0,0 +1,48 @@
+// Phase Stage Types
+export type TPhaseStart = 'PHASE_START'
+export type TPhaseDuring = 'PHASE_DURING'
+export type TPhaseEnd = 'PHASE_END'
+
+// Phase Stage Exports
+export const PHASE_START: TPhaseStart = 'PHASE_START'
+export const PHASE_DURING: TPhaseDuring = 'PHASE_DURING'
+export const PHASE_END: TPhaseEnd = 'PHASE_END'
+
+
+// Phases
+export type THeroPhase = 'HERO_PHASE'
+export type TMovementPhase = 'MOVEMENT_PHASE'
+export type TShootingPhase = 'SHOOTING_PHASE'
+export type TChargePhase = 'CHARGE_PHASE'
+export type TCombatPhase = 'COMBAT_PHASE'
+export type TBattleshockPhase = 'BATTLESHOCK_PHASE'
+
+// Phases Exports
+export const HERO_PHASE: THeroPhase = 'HERO_PHASE'
+export const MOVEMENT_PHASE: TMovementPhase = 'MOVEMENT_PHASE'
+export const SHOOTING_PHASE: TShootingPhase = 'SHOOTING_PHASE'
+export const CHARGE_PHASE: TChargePhase = 'CHARGE_PHASE'
+export const COMBAT_PHASE: TCombatPhase = 'COMBAT_PHASE'
+export const BATTLESHOCK_PHASE: TBattleshockPhase = 'BATTLESHOCK_PHASE'
+
+
+// Turn Types
+export type TSetup = 'SETUP'
+export type TTurnStart = 'TURN_START'
+export type TTurnEnd = 'TURN_END'
+export type TTurnOne = 'TURN_ONE'
+export type TTurnTwo = 'TURN_TWO'
+export type TTurnThree = 'TURN_THREE'
+export type TTurnFour = 'TURN_FOUR'
+export type TTurnFive = 'TURN_FIVE'
+
+// Turn Exports
+export const SETUP: TSetup = 'SETUP'
+export const TURN_START: TTurnStart = 'TURN_START'
+export const TURN_END: TTurnEnd = 'TURN_END'
+export const TURN_ONE: TTurnOne = 'TURN_ONE'
+export const TURN_TWO: TTurnTwo = 'TURN_TWO'
+export const TURN_THREE: TTurnThree = 'TURN_THREE'
+export const TURN_FOUR: TTurnFour = 'TURN_FOUR'
+export const TURN_FIVE: TTurnFive = 'TURN_FIVE'
+
