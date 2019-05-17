eb7f6d5f9a3a655c6280bfc54ccb1a128624bff3
Davis Ford <davis.e.ford@gmail.com>
2019-05-17 12:01:48 -0400

    Update select_army.tsx

--- src/components/input/select_army.tsx
+++ src/components/input/select_army.tsx
@@ -5,11 +5,31 @@ import * as SeraphonArmy from '../../army/seraphon'
 import { IUnits } from 'types/army'
 import './select_army.css'
 
+type TFocusType = 'unit' | 'artifact' | 'battalion'
+type TUpdateState = (val: string, idx: number, type: TFocusType) => any
+
 export const SelectArmy = props => {
   const [units, setUnits] = useState([] as string[])
+  const [battalions, setBattalions] = useState([] as string[])
+  const [artifacts, setArtifacts] = useState([] as string[])
 
-  const updateState = (val: string, idx: number) => {
-    let newState = [...units]
+  const updateState: TUpdateState = (val, idx, type) => {
+    const focus = {
+      unit: {
+        fn: setUnits,
+        state: units,
+      },
+      artifact: {
+        fn: setArtifacts,
+        state: artifacts,
+      },
+      battalion: {
+        fn: setBattalions,
+        state: battalions,
+      },
+    }[type]
+
+    let newState = [...focus.state]
     if (val) {
       newState[idx] = val
     } else {
@@ -19,16 +39,47 @@ export const SelectArmy = props => {
         newState.splice(idx, 1)
       }
     }
-    setUnits(newState)
+    focus.fn(newState)
   }
 
   return (
     <div className="SelectArmy">
+      <h3>Add Units:</h3>
       {_.range(0, units.length + 1).map(idx => {
-        return <Row units={SeraphonArmy.Units} handleChange={updateState} idx={idx} val={units[idx] || ''} />
+        return (
+          <Row units={SeraphonArmy.Units} handleChange={updateState} idx={idx} val={units[idx] || ''} type={'unit'} />
+        )
       })}
 
-      <p>State: {units.join(', ')}</p>
+      <h3>Add Artifacts:</h3>
+      {_.range(0, artifacts.length + 1).map(idx => {
+        return (
+          <Row
+            units={SeraphonArmy.Artifacts}
+            handleChange={updateState}
+            idx={idx}
+            val={artifacts[idx] || ''}
+            type={'artifact'}
+          />
+        )
+      })}
+
+      <h3>Battalions:</h3>
+      {_.range(0, battalions.length + 1).map(idx => {
+        return (
+          <Row
+            units={SeraphonArmy.Battalions}
+            handleChange={updateState}
+            idx={idx}
+            val={battalions[idx] || ''}
+            type={'battalion'}
+          />
+        )
+      })}
+
+      <p>Units: {units.join(', ')}</p>
+      <p>Battalions: {battalions.join(', ')}</p>
+      <p>Artifacts: {artifacts.join(', ')}</p>
     </div>
   )
 }
@@ -40,7 +91,7 @@ const Row = (props: ISelect) => {
       <button
         onClick={e => {
           e.preventDefault()
-          props.handleChange('', props.idx)
+          props.handleChange('', props.idx, props.type)
         }}
         disabled={!props.val}
       >
@@ -52,19 +103,20 @@ const Row = (props: ISelect) => {
 
 interface ISelect {
   units: IUnits
-  handleChange: (val: string, idx: number) => any
+  handleChange: TUpdateState
   idx: number
   val: string
+  type: TFocusType
 }
 
 const Select = (props: ISelect) => {
   return (
     <Fragment>
-      <select onChange={e => props.handleChange(e.target.value, props.idx)}>
+      <select onChange={e => props.handleChange(e.target.value, props.idx, props.type)}>
         {props.val ? (
           <option value={props.val}>{props.units[props.val].name}</option>
         ) : (
-          <option value={''}>{'-- Select a Unit --'}</option>
+          <option value={''}>{`-- Select ${props.type} --`}</option>
         )}
         {Object.keys(props.units).map((k, i) => {
           if (props.val === k) return null // Prevent showing duplicate
