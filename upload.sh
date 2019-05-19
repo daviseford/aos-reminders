7b3c9314f1d157beecb118d179f4ce3b3bc5145f
Davis Ford <davis.e.ford@gmail.com>
2019-05-19 11:26:46 -0400

    Fix the build script

--- upload.sh
+++ upload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-SITE_S3='s3://daviseford.com/aos-reminders/'    # Your S3 bucket address
-SITE_BUILD_DIR='./build/'   # Where your site is generated
-CF_DIST_ID='EOV559H6J3O6V'  # Cloudfront Distribution ID
-CF_PATH='/aos-reminders/*'  # Cloudfront Path to invalidate
+SITE_S3='s3://daviseford.com/aos-reminders/' # Your S3 bucket address
+SITE_BUILD_DIR='./build/'                    # Where your site is generated
+CF_DIST_ID='EOV559H6J3O6V'                   # Cloudfront Distribution ID
+CF_PATH='/aos-reminders/*'                   # Cloudfront Path to invalidate
 yarn build
-aws s3 sync --delete --size-only ${SITE_BUILD_DIR} ${SITE_S3} --exclude "*build_log.txt" --exclude "*.idea*" --exclude "*.sh" --exclude "*.git*" --exclude "*.DS_Store"
+aws s3 sync --delete ${SITE_BUILD_DIR} ${SITE_S3} --exclude "*build_log.txt" --exclude "*.idea*" --exclude "*.sh" --exclude "*.git*" --exclude "*.DS_Store"
 aws cloudfront create-invalidation --distribution-id ${CF_DIST_ID} --paths ${CF_PATH}
 echo 'Deployed to https://daviseford.com/aos-reminders/'
