6a169352144b776e81eeaf6af43ca0052b4c2447
Davis Ford <davis.e.ford@gmail.com>
2019-06-04 17:31:24 -0400

    Update sample army index (fixes #39 )

--- src/army/sample_army/index.ts
+++ src/army/sample_army/index.ts
@@ -5,4 +5,7 @@ import Game from './game'
 import Abilities from './abilities'
 
 // Copy paste this directory to kickstart a new army
+// Remember to add your army to:
+//      - meta/factions.ts
+//      - utils/getArmy.ts
 export { Abilities, Artifacts, Battalions, Game, Traits, Units }
