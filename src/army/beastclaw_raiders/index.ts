3fafc6139c6d95620291d48f566cda3dd2d85fdf
Davis Ford <davis.e.ford@gmail.com>
2019-06-05 21:44:39 -0400

    Initial commit of Beastclaw Raiders

---
+++ src/army/beastclaw_raiders/index.ts
@@ -0,0 +1,11 @@
+import Artifacts from './artifacts'
+import Traits from './traits'
+import { Battalions, Units } from './units'
+import Game from './game'
+import Abilities from './abilities'
+
+// Copy paste this directory to kickstart a new army
+// Remember to add your army to:
+//      - meta/factions.ts
+//      - utils/getArmy.ts
+export { Abilities, Artifacts, Battalions, Game, Traits, Units }
--- src/meta/factions.ts
+++ src/meta/factions.ts
@@ -1,6 +1,7 @@
 import { sortBy } from 'lodash'
 
 // Supported Faction Types
+export type TBeastclawRaiders = 'BEASTCLAW_RAIDERS'
 export type TGloomspiteGitz = 'GLOOMSPITE_GITZ'
 export type TIdonethDeepkin = 'IDONETH_DEEPKIN'
 export type TIronjawz = 'IRONJAWZ'
@@ -8,6 +9,7 @@ export type TSeraphon = 'SERAPHON'
 export type TSylvaneth = 'SYLVANETH'
 
 // Exported Faction Names
+export const BEASTCLAW_RAIDERS: TBeastclawRaiders = 'BEASTCLAW_RAIDERS'
 export const GLOOMSPITE_GITZ: TGloomspiteGitz = 'GLOOMSPITE_GITZ'
 export const IDONETH_DEEPKIN: TIdonethDeepkin = 'IDONETH_DEEPKIN'
 export const IRONJAWZ: TIronjawz = 'IRONJAWZ'
@@ -15,8 +17,16 @@ export const SERAPHON: TSeraphon = 'SERAPHON'
 export const SYLVANETH: TSylvaneth = 'SYLVANETH'
 
 // Supported Factions
-export type TSupportedFaction = TSeraphon | TGloomspiteGitz | TSylvaneth | TIdonethDeepkin | TIronjawz
+export type TSupportedFaction =
+  | TBeastclawRaiders
+  | TGloomspiteGitz
+  | TIdonethDeepkin
+  | TIronjawz
+  | TSeraphon
+  | TSylvaneth
+
 export const SUPPORTED_FACTIONS: TSupportedFaction[] = sortBy([
+  BEASTCLAW_RAIDERS,
   GLOOMSPITE_GITZ,
   IDONETH_DEEPKIN,
   IRONJAWZ,
