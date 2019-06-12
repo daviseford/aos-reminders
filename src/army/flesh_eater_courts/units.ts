458cce78e68654fc8f9f3a634788762bcd14e744
Davis Ford <davis.e.ford@gmail.com>
2019-06-11 21:42:07 -0400

    Update units.ts

--- src/army/flesh_eater_courts/units.ts
+++ src/army/flesh_eater_courts/units.ts
@@ -4,12 +4,152 @@ import { HERO_PHASE } from 'types/phases'
 // Unit Names
 export const Units: TUnits = [
   {
-    name: ``,
+    name: `Abhorrant Ghoul King`,
     effects: [
       {
         name: ``,
         desc: ``,
-        when: [HERO_PHASE],
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Abhorrant Ghoul King on Royal Terrorgheist`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Abhorrant Ghoul King on Royal Zombie Dragon`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Crypt Ghast Courtier`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Crypt Haunter Courtier`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Crypt Infernal Courtier`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Varghulf Courtier`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Abhorrant Archregent`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Crypt Ghouls`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Crypt Horrors`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Crypt Flayers`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Royal Terrorgheist`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Royal Zombie Dragon`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Gristlegore Royal Terrorgheist`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Gristlegore Royal Zombie Dragon`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
       },
     ],
   },
@@ -18,12 +158,102 @@ export const Units: TUnits = [
 // Battalions
 export const Battalions: TBattalions = [
   {
-    name: ``,
+    name: `Abattoir`,
     effects: [
       {
         name: ``,
         desc: ``,
-        when: [HERO_PHASE],
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Attendants at Court`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Deadwatch`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Cannibal Court`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Ghoul Patrol`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: "King's Ghouls",
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Royal Family`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Royal Menagerie`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Royal Mordants`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `The Arcasanctorian Guard`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
       },
     ],
   },
