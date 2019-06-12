2b26a1138a9fe6c2b8a7d935f097149e5d90db71
Davis Ford <davis.e.ford@gmail.com>
2019-06-11 22:41:25 -0400

    Add Death artifacts and traits (fixes #50  and #46 )

--- src/army/grand_alliances/death/traits.ts
+++ src/army/grand_alliances/death/traits.ts
@@ -1,14 +1,64 @@
 import { TCommandTraits } from 'types/army'
-import { TURN_ONE_START_OF_ROUND } from 'types/phases'
+import { DURING_GAME, SHOOTING_PHASE, COMBAT_PHASE, HERO_PHASE, BATTLESHOCK_PHASE } from 'types/phases'
 
 const CommandTraits: TCommandTraits = [
   {
-    name: ``,
+    name: `Ruler of the Night (Death)`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [TURN_ONE_START_OF_ROUND],
+        name: `Ruler of the Night (Death)`,
+        desc: `Friendly DEATH units are affected by the Deathless Minions battle trait if they are within 12" of this general rather than only 6".`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Predator of the Shadows (Death)`,
+    effects: [
+      {
+        name: `Predator of the Shadows (Death)`,
+        desc: `Add 1 to hit and wound rolls for this general if they are in cover.`,
+        when: [SHOOTING_PHASE, COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Death Incarnate (Death)`,
+    effects: [
+      {
+        name: `Death Incarnate (Death)`,
+        desc: `In your hero phase, pick an enemy unit within 3" of this general and roll 2D6. If the roll equals or exceeds the enemy unit’s Bravery characteristic, inflict D3 mortal wounds on the unit.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Master of the Black Arts (Death)`,
+    effects: [
+      {
+        name: `Master of the Black Arts (Death)`,
+        desc: `This general is a WIZARD who knows the Arcane Bolt and Mystic Shield spells. If this general is already a wizard, add 1 to all of their casting and unbinding rolls instead.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Red Fury (Death)`,
+    effects: [
+      {
+        name: `Red Fury (Death)`,
+        desc: `The first time this general is picked to fight in each combat phase, roll a dice at the end of that fight. On a 5+ they can immediately fight again.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Supernatural Horror (Death)`,
+    effects: [
+      {
+        name: `Supernatural Horror (Death)`,
+        desc: `In the battleshock phase, double the number of models that flee from enemy units while they are within 12" of this general.`,
+        when: [BATTLESHOCK_PHASE],
       },
     ],
   },
