265a418f0837e2da9efc98954d7e5245c2054124
Davis Ford <davis.e.ford@gmail.com>
2019-06-15 21:20:28 -0400

    Adding units and traits

--- src/army/skaven/traits.ts
+++ src/army/skaven/traits.ts
@@ -1,14 +1,263 @@
 import { TCommandTraits } from 'types/army'
-import { HERO_PHASE } from 'types/phases'
+import {
+  HERO_PHASE,
+  COMBAT_PHASE,
+  DURING_GAME,
+  BATTLESHOCK_PHASE,
+  SHOOTING_PHASE,
+  START_OF_HERO_PHASE,
+  START_OF_COMBAT_PHASE,
+  MOVEMENT_PHASE,
+} from 'types/phases'
 
 const CommandTraits: TCommandTraits = [
+  {
+    name: `Malevolent (Pestilens)`,
+    effects: [
+      {
+        name: `Malevolent (Pestilens)`,
+        desc: `You can re-roll wound rolls of 1 for attacks made with melee weapons by this general.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Verminous Valour (Pestilens)`,
+    effects: [
+      {
+        name: `Verminous Valour (Pestilens)`,
+        desc: `Before you allocate a wound or mortal wound to this general, you can roll a dice. Subtract 1 from the roll if this general is a MONSTER or WAR MACHINE. On a 4+, instead of allocating the wound or mortal wound to this general, you can allocate it to a friendly SKAVENTIDE unit within 3" of this general.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Savage Overlord (Pestilens)`,
+    effects: [
+      {
+        name: `Savage Overlord (Pestilens)`,
+        desc: `Add 1 to the Bravery characteristic of friendly SKAVENTIDE units while they are wholly within 18" of this general.`,
+        when: [BATTLESHOCK_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Master of Rot and Ruin (Pestilens)`,
+    effects: [
+      {
+        name: `Master of Rot and Ruin (Pestilens)`,
+        desc: `You can re-roll the dice that determines if a prayer chanted by this general is answered.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Architect of Death (Pestilens)`,
+    effects: [
+      {
+        name: `Architect of Death (Pestilens)`,
+        desc: `You can re-roll wound rolls for attacks made with missile weapons by friendly CLANS PESTILENS units while they are wholly within 18" of this general.`,
+        when: [SHOOTING_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Diseased (Pestilens)`,
+    effects: [
+      {
+        name: `Diseased (Pestilens)`,
+        desc: `At the start of your hero phase, roll a dice if this general is within 3" of any enemy units. On a 4+ inflict D3 mortal wounds on 1 enemy unit within 3" of this general.`,
+        when: [START_OF_HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Malevolent (Verminus)`,
+    effects: [
+      {
+        name: `Malevolent (Verminus)`,
+        desc: `You can re-roll wound rolls of 1 for attacks made with melee weapons by this general.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Verminous Valour (Verminus)`,
+    effects: [
+      {
+        name: `Verminous Valour (Verminus)`,
+        desc: `Before you allocate a wound or mortal wound to this general, you can roll a dice. Subtract 1 from the roll if this general is a MONSTER or WAR MACHINE. On a 4+, instead of allocating the wound or mortal wound to this general, you can allocate it to a friendly SKAVENTIDE unit within 3" of this general.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Savage Overlord (Verminus)`,
+    effects: [
+      {
+        name: `Savage Overlord (Verminus)`,
+        desc: `Add 1 to the Bravery characteristic of friendly SKAVENTIDE units while they are wholly within 18" of this general.`,
+        when: [BATTLESHOCK_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Brutal Fury (Verminus)`,
+    effects: [
+      {
+        name: `Brutal Fury (Verminus)`,
+        desc: `Once per battle, at the start of the combat phase, you can add 3 to the Attacks characteristic of this general’s melee weapons until the end of that phase.`,
+        when: [START_OF_COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Powerful (Verminus)`,
+    effects: [
+      {
+        name: `Powerful (Verminus)`,
+        desc: `Add 1 to this general’s Wounds characteristic.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Devious Adversary (Verminus)`,
+    effects: [
+      {
+        name: `Devious Adversary (Verminus)`,
+        desc: `If the unmodified hit roll for an attack made with a melee weapon that targets this general is a 1, add 1 to the Attacks characteristic of this general’s melee weapons until the end of that phase.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Malevolent (Moulder)`,
+    effects: [
+      {
+        name: `Malevolent (Moulder)`,
+        desc: `You can re-roll wound rolls of 1 for attacks made with melee weapons by this general.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Verminous Valour (Moulder)`,
+    effects: [
+      {
+        name: `Verminous Valour (Moulder)`,
+        desc: `Before you allocate a wound or mortal wound to this general, you can roll a dice. Subtract 1 from the roll if this general is a MONSTER or WAR MACHINE. On a 4+, instead of allocating the wound or mortal wound to this general, you can allocate it to a friendly SKAVENTIDE unit within 3" of this general.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Savage Overlord (Moulder)`,
+    effects: [
+      {
+        name: `Savage Overlord (Moulder)`,
+        desc: `Add 1 to the Bravery characteristic of friendly SKAVENTIDE units while they are wholly within 18" of this general.`,
+        when: [BATTLESHOCK_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Moulder Supreme (Moulder)`,
+    effects: [
+      {
+        name: `Moulder Supreme (Moulder)`,
+        desc: `When you use the Prized Creations battle trait and pick 1 friendly CLANS MOULDER FIGHTING BEAST model for this general, you can either add 3 to that model’s Wounds characteristic instead of D3, or add D6 to that model’s Wounds characteristic instead of D3.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Hordemaster (Moulder)`,
+    effects: [
+      {
+        name: `Hordemaster (Moulder)`,
+        desc: `When this general uses the Unleash More-more Beasts! command ability, you receive a new unit on a roll of 4+ instead of 5+.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Burly (Moulder)`,
+    effects: [
+      {
+        name: `Burly (Moulder)`,
+        desc: `Add 1 to this general’s Wounds characteristic.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Malevolent (Eshin)`,
+    effects: [
+      {
+        name: `Malevolent (Eshin)`,
+        desc: `You can re-roll wound rolls of 1 for attacks made with melee weapons by this general.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Verminous Valour (Eshin)`,
+    effects: [
+      {
+        name: `Verminous Valour (Eshin)`,
+        desc: `Before you allocate a wound or mortal wound to this general, you can roll a dice. Subtract 1 from the roll if this general is a MONSTER or WAR MACHINE. On a 4+, instead of allocating the wound or mortal wound to this general, you can allocate it to a friendly SKAVENTIDE unit within 3" of this general.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Savage Overlord (Eshin)`,
+    effects: [
+      {
+        name: `Savage Overlord (Eshin)`,
+        desc: `Add 1 to the Bravery characteristic of friendly SKAVENTIDE units while they are wholly within 18" of this general.`,
+        when: [BATTLESHOCK_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Unrivalled Killer (Eshin)`,
+    effects: [
+      {
+        name: `Unrivalled Killer (Eshin)`,
+        desc: `You can re-roll hit rolls for attacks made by this general that target the enemy HERO chosen for the Masters of Murder battle trait.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Shadowmaster (Eshin)`,
+    effects: [
+      {
+        name: `Shadowmaster (Eshin)`,
+        desc: `While this general is within 1" of a terrain feature, this general is not visible to enemy models while they are more than 6" from this general.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Incredible Agility (Eshin)`,
+    effects: [
+      {
+        name: `Incredible Agility (Eshin)`,
+        desc: `This general can fly.`,
+        when: [MOVEMENT_PHASE],
+      },
+    ],
+  },
   {
     name: ``,
     effects: [
       {
         name: ``,
         desc: ``,
-        when: [HERO_PHASE],
+        when: [],
       },
     ],
   },
--- src/army/skaven/units.ts
+++ src/army/skaven/units.ts
@@ -4,7 +4,408 @@ import { HERO_PHASE } from 'types/phases'
 // Unit Names
 export const Units: TUnits = [
   {
-    name: ``,
+    name: `Clanrats`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Stormvermin`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Spiteclaw's Swarm`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Verminlord Warbringer'`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Clawlord`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Skaven Clawlord on Brood Horror',`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Skritch Spiteclaw',`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Doomwheel`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Warp Lightning Cannon',`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Doom-Flayer`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Ratling Gun`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Warpfire Thrower`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Warp-Grinder`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Poisoned Wind Mortar Weapon Team`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Skryre Acolytes`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Warplock Jezzails`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Stormfiends`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Warlock Engineer`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Warlock Bombardier`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Arch-Warlock',`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Plagueclaw',`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Plague Monks`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Plague Censer Bearers`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Plague Priest`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Plague Priest on Plague Furnace`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Verminlord Corruptor`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Brood Horror`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Hell Pit Abomination`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Master Moulder',`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Wolf Rats`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Giant Rats`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Rat Swarms`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Rat Ogors`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Packmasters`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+
+  {
+    name: `Skaven Chieftain With Battle Standard`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Skavenslaves`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Deathrunner`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Verminlord Deceiver'`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Deathmaster',`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Night Runners`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Gutter Runners`,
     effects: [
       {
         name: ``,
@@ -18,7 +419,237 @@ export const Units: TUnits = [
 // Battalions
 export const Battalions: TBattalions = [
   {
-    name: ``,
+    name: `Congregation of Filth',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Fleshmeld Menagerie',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Foulrain Congregation',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Claw-horde',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Plaguesmog Congregation',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Slinktalon',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Virulent Procession',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Warpcog Convocation',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Arkhspark Voltik',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Gascloud Chokelung',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Gautfyre Skorch',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Rattlegauge Warplock',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Whyrlblade Threshik',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Skatchnik's Warpcoven",tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Congregation of Filth',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Foulrain Congregation',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Plaguesmog Congregation',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Virulent Procession',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Clan Skryre',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Arkhspark Voltik',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Gascloud Chokelung',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Gautfyre Skorch',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Rattlegauge Warplock',tion`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Whyrlblade Threshik',tion`,
     effects: [
       {
         name: ``,
