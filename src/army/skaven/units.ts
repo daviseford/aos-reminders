acb981c45c517abf1b032818d95808d06001e04d
Davis Ford <davis.e.ford@gmail.com>
2019-06-18 22:52:25 -0400

    Update units.ts

--- src/army/skaven/units.ts
+++ src/army/skaven/units.ts
@@ -1,5 +1,12 @@
 import { TBattalions, TUnits } from 'types/army'
-import { HERO_PHASE } from 'types/phases'
+import {
+  HERO_PHASE,
+  START_OF_HERO_PHASE,
+  DURING_GAME,
+  SHOOTING_PHASE,
+  MOVEMENT_PHASE,
+  COMBAT_PHASE,
+} from 'types/phases'
 
 // Unit Names
 export const Units: TUnits = [
@@ -34,7 +41,7 @@ export const Units: TUnits = [
     ],
   },
   {
-    name: `Verminlord Warbringer'`,
+    name: `Verminlord Warbringer`,
     effects: [
       {
         name: ``,
@@ -54,7 +61,7 @@ export const Units: TUnits = [
     ],
   },
   {
-    name: `Skaven Clawlord on Brood Horror',`,
+    name: `Skaven Clawlord on Brood Horror`,
     effects: [
       {
         name: ``,
@@ -64,7 +71,7 @@ export const Units: TUnits = [
     ],
   },
   {
-    name: `Skritch Spiteclaw',`,
+    name: `Skritch Spiteclaw`,
     effects: [
       {
         name: ``,
@@ -84,7 +91,7 @@ export const Units: TUnits = [
     ],
   },
   {
-    name: `Warp Lightning Cannon',`,
+    name: `Warp Lightning Cannon`,
     effects: [
       {
         name: ``,
@@ -194,7 +201,7 @@ export const Units: TUnits = [
     ],
   },
   {
-    name: `Arch-Warlock',`,
+    name: `Arch-Warlock`,
     effects: [
       {
         name: ``,
@@ -204,7 +211,7 @@ export const Units: TUnits = [
     ],
   },
   {
-    name: `Plagueclaw',`,
+    name: `Plagueclaw`,
     effects: [
       {
         name: ``,
@@ -284,7 +291,7 @@ export const Units: TUnits = [
     ],
   },
   {
-    name: `Master Moulder',`,
+    name: `Master Moulder`,
     effects: [
       {
         name: ``,
@@ -375,7 +382,7 @@ export const Units: TUnits = [
     ],
   },
   {
-    name: `Verminlord Deceiver'`,
+    name: `Verminlord Deceiver`,
     effects: [
       {
         name: ``,
@@ -385,7 +392,7 @@ export const Units: TUnits = [
     ],
   },
   {
-    name: `Deathmaster',`,
+    name: `Deathmaster`,
     effects: [
       {
         name: ``,
@@ -419,241 +426,101 @@ export const Units: TUnits = [
 // Battalions
 export const Battalions: TBattalions = [
   {
-    name: `Congregation of Filth',tion`,
+    name: `Fleshmeld Menagerie`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `More-more-more Beasts!`,
+        desc: `When the Master Moulder from this battalion uses the Unleash More- more Beasts! command ability for a unit from the same battalion that has been destroyed, a new unit is added to your army on a roll of 4+ instead of 5+.`,
+        when: [DURING_GAME],
       },
     ],
   },
   {
-    name: `Fleshmeld Menagerie',tion`,
+    name: `Claw-horde`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Claw-picked`,
+        desc: `When the CLAWLORD from this battalion use the Gnash-gnaw on their Bones! command ability, instead of picking 1 unit wholly within 13" of the CLAWLORD, you can pick all of the units from the same battalion that are wholly within 13" of the CLAWLORD.`,
+        when: [COMBAT_PHASE],
       },
     ],
   },
   {
-    name: `Foulrain Congregation',tion`,
+    name: `Slinktalon`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Murder-slay, Now-now!`,
+        desc: `If the DEATHMASTER from this battalion is set up in hiding as a reserve unit, in the combat phase in which it is set up on the battlefield you can re-roll hit rolls for attacks made by units from the same battalion.`,
+        when: [COMBAT_PHASE],
       },
     ],
   },
   {
-    name: `Claw-horde',tion`,
+    name: `Virulent Procession`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Verminous Infestation`,
+        desc: `At the start of your hero phase, pick 1 terrain feature within 13" of this battalion’s VERMINLORD CORRUPTOR. Roll a dice for each enemy unit within 3" of that terrain feature. On a 4+ that unit suffers D3 mortal wounds.`,
+        when: [START_OF_HERO_PHASE],
       },
     ],
   },
   {
-    name: `Plaguesmog Congregation',tion`,
+    name: `Warpcog Convocation`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Arkhspark Voltik`,
+        desc: `In your shooting phase, you can pick 1 WARP LIGHTNING CANNON from this enginecoven that is within 13" of the WARLOCK ENGINEER from the same enginecoven, or the ARCH- WARLOCK from the same battalion. If you do so, subtract 1 from the power of that WARP LIGHTNING CANNON’s Warp Lightning Blast in that shooting phase (to a minimum power of 1).`,
+        when: [SHOOTING_PHASE],
+      },
+      {
+        name: `Gascloud Chokelung`,
+        desc: `You can re-roll hit rolls of 1 for attacks made with missile weapons by this enginecoven’s SKRYRE ACOLYTES and STORMFIENDS armed with Windlaunchers while they are wholly within 13" of the WARLOCK ENGINEER from the same enginecoven, or the ARCH- WARLOCK from the same battalion.`,
+        when: [SHOOTING_PHASE],
+      },
+      {
+        name: `Gautfyre Skorch`,
+        desc: `Up to 2 units can join each WARP- GRINDER from this enginecoven instead of only 1, as long as both of the units come from the same enginecoven as the WARP-GRINDER that they join tunnelling.`,
+        when: [DURING_GAME],
+      },
+      {
+        name: `Rattlegauge Warplock`,
+        desc: `You can re-roll hit rolls of 1 for attacks made with missile weapons by this enginecoven’s WARPLOCK JEZZAILS and RATLING GUNS while they are wholly within 13" of the WARLOCK ENGINEER from the same enginecoven, or the ARCH- WARLOCK from the same battalion.`,
+        when: [SHOOTING_PHASE],
+      },
+      {
+        name: `Whyrlblade Threshik`,
+        desc: `You can move a unit from this enginecoven an extra 3" when it starts the move wholly within 13" of the WARLOCK ENGINEER from the same enginecoven, or the ARCH- WARLOCK from the same battalion.`,
+        when: [MOVEMENT_PHASE],
       },
     ],
   },
   {
-    name: `Slinktalon',tion`,
+    name: `Congregation of Filth`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Plague Altar`,
+        desc: `Roll a dice each time you allocate a wound or mortal wound to a PLAGUE MONKS unit from this battalion while it is wholly within 18" of the same battalion’s PLAGUE PRIEST. On a 6 that wound or mortal wound is negated.`,
+        when: [DURING_GAME],
       },
     ],
   },
   {
-    name: `Virulent Procession',tion`,
+    name: `Foulrain Congregation`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Foetid Blessings`,
+        desc: `Add 1 to wound rolls for attacks made with missile weapons by units in this battalion while they are within 13" of the same battalion’s PLAGUE PRIEST.`,
+        when: [SHOOTING_PHASE],
       },
     ],
   },
   {
-    name: `Warpcog Convocation',tion`,
+    name: `Plaguesmog Congregation`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Arkhspark Voltik',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Gascloud Chokelung',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Gautfyre Skorch',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Rattlegauge Warplock',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Whyrlblade Threshik',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Skatchnik's Warpcoven",tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Congregation of Filth',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Foulrain Congregation',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Plaguesmog Congregation',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Virulent Procession',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Clan Skryre',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Arkhspark Voltik',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Gascloud Chokelung',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Gautfyre Skorch',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Rattlegauge Warplock',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Whyrlblade Threshik',tion`,
-    effects: [
-      {
-        name: ``,
-        desc: ``,
+        name: `Poisonous Miasma`,
+        desc: `You can re-roll the dice that determines if an enemy unit suffers any mortal wounds when you use the Poisonous Fumes ability if that enemy unit is within 3" of a unit from this battalion.`,
         when: [HERO_PHASE],
       },
     ],
