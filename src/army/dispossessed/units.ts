e07999dc5442f44992e936d548bdada82b3789f4
Tom Gattenhof <tom@pixeltom.net>
2019-06-27 09:32:54 +1000

    Unit update

--- src/army/dispossessed/units.ts
+++ src/army/dispossessed/units.ts
@@ -1,5 +1,5 @@
 import { TBattalions, TUnits } from '../../types/army'
-import { HERO_PHASE, DURING_GAME } from '../../types/phases'
+import { HERO_PHASE, DURING_GAME, COMBAT_PHASE, MOVEMENT_PHASE, BATTLESHOCK_PHASE } from '../../types/phases'
 
 // Unit Names
 export const Units: TUnits = [
@@ -49,9 +49,24 @@ export const Units: TUnits = [
     name: `Unforged`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Runic Axes`,
+        desc: `The Unforged launches a deadly flurry of blows. You can re-roll all hit rolls of 1 for an Unforged.`,
+        when: [DURING_GAME],
+      },
+      {
+        name: `Epic Deathblow`,
+        desc: `If an Unforged is slain in the combat phase, roll a dice before it is removed. On a roll of 4 or more, you can inflict D3 mortal wounds on the enemy unit that struck the fatal blow (inflict D6 mortal wounds instead if a Chaos model struck the final blow).`,
+        when: [COMBAT_PHASE],
+      },
+      {
+        name: `Nemesis`,
+        desc: `Attacks made by an Unforged inflict double Damage against CHAOS units.`,
+        when: [DURING_GAME],
+      },
+      {
+        name: `The Bigger They Are...`,
+        desc: `You can add 1 to any wound rolls for an Unforged if the target of the attack has a Wounds characteristic of more than 1.`,
+        when: [DURING_GAME],
       },
     ],
   },
@@ -59,15 +74,45 @@ export const Units: TUnits = [
     name: `Hammerers`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Throng Musician`,
+        desc: `Models in this unit can be Hornblowers or Drummers. When a unit containing any Hornblowers or Drummers runs, they can ‘Sound the Advance’. If they do so, do not roll a dice to see how far the unit runs; instead, they can move up to an extra 4". `,
+        when: [MOVEMENT_PHASE],
+      },
+      {
+        name: `Standard Bearer`,
+        desc: `Models in this unit may be Standard Bearers. If you fail a battleshock test for a unit that has any Standard Bearers, halve the number of models that flee (rounding up).`,
+        when: [BATTLESHOCK_PHASE],
+      },
+      {
+        name: `Kingsguard`,
+        desc: `You do not need to take battleshock tests for this unit if it is within 16" of a DISPOSSESSED HERO from your army in the battleshock phase.`,
+        when: [BATTLESHOCK_PHASE],
       },
     ],
   },
   {
     name: `Ironbreakers`,
     effects: [
+      {
+        name: `Icon Bearer`,
+        desc: `Models in this unit may be Icon Bearers. Roll a dice if an enemy spell affects a unit with any Icon Bearers. On a roll of 5 or more, that spell has no effect on the unit (but it will affect other units normally).`,
+        when: [HERO_PHASE],
+      },
+      {
+        name: `Drummer`,
+        desc: `Models in this unit can be Drummers. When a unit containing any Drummers runs, the can 'Sound the Advance'. If they do so, do not roll a dice to see how far the unit runs; instead, they can move up to an extra 4".`,
+        when: [MOVEMENT_PHASE],
+      },
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+      },
       {
         name: ``,
         desc: ``,
