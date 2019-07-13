14455048e9747fb1a57b1fb1531fefaf2ea781f7
Chris Turner <cturnerfoto@gmail.com>
2019-07-13 10:50:07 +0100

    added LoN battalions

--- src/army/legions_of_nagash/units.ts
+++ src/army/legions_of_nagash/units.ts
@@ -1443,62 +1443,92 @@ export const Units: TUnits = [
 // Battalions
 export const Battalions: TBattalions = [
   {
-    name: ``,
+    name: `The First Cohort`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [],
+        name: `Ceaseless Vigil`,
+        desc: `Each time a wound or mortal wound is allocated to Nagash, you may pick a MORGHAST unit from the First Cohort within 3" of him and roll a dice. On a 3+ you may allocate that wound or mortal wound to the chosen MORGHAST unit instead.`,
+        when: [DURING_GAME],
+      },
+      {
+        name: `Eternal Servitude`,
+        desc: `If Nagash uses his Deathly Invocation ability on any SUMMONABLE unit from the First Cohort, you can treat the D3 result as 3 instead of rolling the dice.`,
+        when: [START_OF_HERO_PHASE],
       },
     ],
   },
   {
-    name: ``,
+    name: `Lords of Sacrament`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [],
+        name: `Unearthly Focus`,
+        desc: `In your hero phase, each WIZARD from the Lords of Sacrament may cast an additional spell whilst they are within 6" of the battalion's Mortis Engine.`,
+        when: [HERO_PHASE],
+      },
+      {
+        name: `Swirling Spirits`,
+        desc: `In the shooting phase, add 1 to save rolls for units from the Lords of Sacrament whilst they are within 6" of the battalion's Mortis Engine.`,
+        when: [SHOOTING_PHASE],
       },
     ],
   },
   {
-    name: ``,
+    name: `Deathmarch`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [],
+        name: `Unbreaking Ranks`,
+        desc: `At the start of your hero phase, you can return 1 slain model to each Deathmarch unit that is within 9" of the battalion's WIGHT KING.`,
+        when: [START_OF_HERO_PHASE],
+      },
+      {
+        name: `March of the Dead`,
+        desc: `In your hero phase, the WIGHT KING and all units from his Deathmarch that are within 9" of him can make a move of up to 4" as if it were your Movement phase (models cannot run as part of this move).`,
+        when: [HERO_PHASE],
       },
     ],
   },
   {
-    name: ``,
+    name: `Castellans of the Crimson Keep`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [],
+        name: `The Shifting Keep`,
+        desc: `At the start of the game, after territories have been determined, but before any units have been set up, pick a battlefield edge. Instead of setting up this battalion, you may place it to one side and say that it is set up in the Crimson Keep. At the end of any of your movement phases you may set up any units from this battalion wholly within 6" of the chosen battlefield edge and more than 9" from enemy models.`,
+        when: [DURING_SETUP, END_OF_MOVEMENT_PHASE],
+      },
+      {
+        name: `In the Shadow of the Keep`,
+        desc: `Whilst they are within 15" of the battlefield edge picked as described in the Shifting Keep ability above, you can re-roll failed hit rolls for the Templar Lances or Blades used by units of Blood Knights in this battalion.`,
+        when: [COMBAT_PHASE],
       },
     ],
   },
   {
-    name: ``,
+    name: `Court of Nulahmia`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [],
+        name: `The Adevore`,
+        desc: `Whilst she is within 9" of this battalion's Bloodseeker Palanquin, you may re-roll failed hit rolls for Neferata if the target is an enemy HERO.`,
+        when: [COMBAT_PHASE],
+      },
+      {
+        name: `Scent of Blood`,
+        desc: `At the start of your movement phase, units from the Court of Nulahmia that are within 9" of the battalion's Bloodseeker Palanquin may add 4" to their Move characteristic until the end of the phase.`,
+        when: [START_OF_MOVEMENT_PHASE],
       },
     ],
   },
   {
-    name: ``,
+    name: `Nightfall Pack`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [],
+        name: `Swooping Predators`,
+        desc: `On any turn in which they completed a successful charge move, add 1 to the Attacks characteristic of the Murderous Fangs and Talons of this battalion's Vargheists.`,
+        when: [COMBAT_PHASE],
+      },
+      {
+        name: `Call of the Night`,
+        desc: `If Mannfred uses his Deathly Invocation ability on any Skeleton Warriors unit from the Nightfall Pack, you may re-roll the D3 result for that unit.`,
+        when: [START_OF_HERO_PHASE],
       },
     ],
   },
