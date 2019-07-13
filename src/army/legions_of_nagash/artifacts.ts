e3f4b51e305f03cc736a426d853f6bca828f7468
Davis Ford <davis.e.ford@gmail.com>
2019-07-13 11:10:31 -0400

    Update artifacts.ts

--- src/army/legions_of_nagash/artifacts.ts
+++ src/army/legions_of_nagash/artifacts.ts
@@ -284,60 +284,60 @@ const Artifacts: TArtifacts = [
     ],
   },
   {
-    name: `The Stalking Blade (Soulblight Artefacts)`,
+    name: `The Stalking Blade (Soulblight)`,
     effects: [
       {
-        name: `The Stalking Blade (Soulblight Artefacts)`,
+        name: `The Stalking Blade (Soulblight)`,
         desc: `Pick one of the bearer's melee weapons. Keep a tally of the number of wounds allocated as a result of attacks made with that weapon. As soon as the total reaches 6 or more, the bearer heals D3 wounds that have been allocated to them, and you can add 1 to the Damage characteristic of that weapon for the rest of the battle.`,
         when: [DURING_GAME],
       },
     ],
   },
   {
-    name: `The Scabbing Plate (Soulblight Artefacts)`,
+    name: `The Scabbing Plate (Soulblight)`,
     effects: [
       {
-        name: `The Scabbing Plate (Soulblight Artefacts)`,
+        name: `The Scabbing Plate (Soulblight)`,
         desc: `At the end of any combat phase in which the bearer caused any wounds to be allocated to any enemy models, you can heal 1 wound that has been allocated to the bearer.`,
         when: [END_OF_COMBAT_PHASE],
       },
     ],
   },
   {
-    name: `Crimson Wing (Soulblight Artefacts)`,
+    name: `Crimson Wing (Soulblight)`,
     effects: [
       {
-        name: `Crimson Wing (Soulblight Artefacts)`,
+        name: `Crimson Wing (Soulblight)`,
         desc: `In your shooting phase, you can pick an enemy unit within 30" of the bearer and roll a dice. On a roll of 3+ that unit suffers 1 mortal wound.`,
         when: [SHOOTING_PHASE],
       },
     ],
   },
   {
-    name: `Sigil of the Sanguine Throne (Soulblight Artefacts)`,
+    name: `Sigil of the Sanguine Throne (Soulblight)`,
     effects: [
       {
-        name: `Sigil of the Sanguine Throne (Soulblight Artefacts)`,
+        name: `Sigil of the Sanguine Throne (Soulblight)`,
         desc: `Once per charge phase, you can re-roll a failed charge roll made for a friendly VAMPIRE unit within 12" of the bearer.`,
         when: [CHARGE_PHASE],
       },
     ],
   },
   {
-    name: `The Saccharine Goblet (Soulblight Artefacts)`,
+    name: `The Saccharine Goblet (Soulblight)`,
     effects: [
       {
-        name: `The Saccharine Goblet (Soulblight Artefacts)`,
+        name: `The Saccharine Goblet (Soulblight)`,
         desc: `At start of the combat phase, you can declare that the bearer will drink from the Saccharine Goblet. If you do so, you can add 1 to hit and wound rolls for the bearer until the end of the phase. However, if no wounds are allocated as a result of these attacks, then the bearer suffers D3 mortal wounds.`,
         when: [START_OF_COMBAT_PHASE],
       },
     ],
   },
   {
-    name: `Ruby Vial (Soulblight Artefacts)`,
+    name: `Ruby Vial (Soulblight)`,
     effects: [
       {
-        name: `Ruby Vial (Soulblight Artefacts)`,
+        name: `Ruby Vial (Soulblight)`,
         desc: `Once per battle, in your hero phase, you can declare that the bearer will open the Ruby Vial. If you do so, then until your next hero phase, subtract 1 from the Bravery characteristic of enemy units and subtract 1 from the result of any charge rolls made for them.`,
         when: [HERO_PHASE],
       },
