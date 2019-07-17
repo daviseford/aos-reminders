2c413404d27e6dfb5f1cd11b017de39489bd9f32
Davis Ford <davis.e.ford@gmail.com>
2019-07-17 17:24:18 -0400

    Refactor toolbar, removed Print button separate file

--- src/components/input/toolbar.tsx
+++ src/components/input/toolbar.tsx
@@ -1,55 +1,92 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState } from 'react'
 import { SUPPORTED_FACTIONS, TSupportedFaction } from 'meta/factions'
 import { SelectOne, TDropdownOption } from './select'
 import { ValueType } from 'react-select/lib/types'
-import PrintButton from 'components/print/button'
+import { logPrintEvent } from 'utils/analytics'
 
 interface IToolbarProps {
-  setAllyValue: IAddAlly['setValue']
+  setAllyValue: IAddAllySelect['setAllyValue']
   factionName: TSupportedFaction
 }
 
 const Toolbar = (props: IToolbarProps) => {
   const { setAllyValue } = props
+  const [hasAlly, setHasAlly] = useState(false)
+
+  const handleAllyClick = e => {
+    e.preventDefault()
+    const newVal = !hasAlly
+    setHasAlly(newVal)
+    if (!newVal) setAllyValue({ value: '' } as TDropdownOption)
+  }
+
   return (
-    <div className="row">
-      <div className="col">
-        <PrintButton factionName={props.factionName} />
+    <>
+      <div className="row">
+        <div className="col">
+          <PrintButton factionName={props.factionName} />
+        </div>
+        <div className="col">
+          <AddAllyButton setAllyClick={handleAllyClick} hasAlly={hasAlly} />
+        </div>
       </div>
-      <div className="col">
-        <AddAlly setValue={setAllyValue} />
+      <div className="row" hidden={!hasAlly}>
+        <div className="col">
+          <AddAllySelect setAllyValue={setAllyValue} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
 export default Toolbar
 
-interface IAddAlly {
-  setValue: (selectValue: ValueType<TDropdownOption>) => void
+interface IAddAllyButton {
+  setAllyClick: (e: any) => void
+  hasAlly: boolean
 }
 
-const AddAlly = (props: IAddAlly) => {
-  const { setValue } = props
-  const [checked, setChecked] = useState(false)
-
-  const handleCheckClick = e => {
-    const newVal = !checked
-    setChecked(newVal)
-    if (!newVal) setValue({ value: '' } as TDropdownOption)
-  }
-
+const AddAllyButton = (props: IAddAllyButton) => {
+  const { hasAlly, setAllyClick } = props
   return (
-    <Fragment>
+    <>
       <div className="col">
-        <input type="checkbox" aria-label="Checkbox for following text input" onClick={handleCheckClick} />
-        {`  `}
-        {!checked ? `Add ` : null}
-        {`Ally `}
+        <button
+          className={`btn btn-block ${hasAlly ? `btn-outline-danger` : `btn-outline-dark`}`}
+          onClick={setAllyClick}
+        >
+          {hasAlly ? 'Remove' : 'Add'} Ally
+        </button>
       </div>
-      <div className="col-3" hidden={!checked}>
-        <SelectOne items={SUPPORTED_FACTIONS} setValue={setValue} hasDefault={true} toTitle={true} />
+    </>
+  )
+}
+
+interface IAddAllySelect {
+  setAllyValue: (selectValue: ValueType<TDropdownOption>) => void
+}
+
+const AddAllySelect = (props: IAddAllySelect) => {
+  const { setAllyValue } = props
+  return (
+    <>
+      <SelectOne items={SUPPORTED_FACTIONS} setValue={setAllyValue} hasDefault={true} toTitle={true} />
+    </>
+  )
+}
+const PrintButton = (props: { factionName: TSupportedFaction }) => {
+  const handlePrint = e => {
+    e.preventDefault()
+    logPrintEvent(props.factionName)
+    return window.print()
+  }
+  return (
+    <>
+      <div className="col">
+        <button className="btn btn-outline-dark btn-block" onClick={handlePrint}>
+          Print Page
+        </button>
       </div>
-    </Fragment>
+    </>
   )
 }
