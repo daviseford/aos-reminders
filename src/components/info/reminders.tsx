c847c67104fe25268c2e869fe22e8ef0fa5bccaf
Davis Ford <daviseford@users.noreply.github.com>
2019-07-21 20:54:22 -0400

    Add ability to hide rules (fixes #96) (#105)
    * Beginning work on this
    
    * Add tooltip
    
    * Update reminders.tsx
    
    * Update reminders.tsx
    
    * Update reminders.tsx

--- src/components/info/reminders.tsx
+++ src/components/info/reminders.tsx
@@ -1,10 +1,13 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import './reminders.css'
+import { IconContext } from 'react-icons'
+import { MdVisibility, MdVisibilityOff } from 'react-icons/md'
+import ReactTooltip from 'react-tooltip'
 import { processReminders } from 'utils/processReminders'
 import { titleCase } from 'utils/titleCase'
+import { TSupportedFaction } from 'meta/factions'
 import { ISelections, IAllySelections } from 'types/selections'
 import { IArmy } from 'types/army'
-import { TSupportedFaction } from 'meta/factions'
 import { ITurnAction } from 'types/data'
 
 interface IRemindersProps {
@@ -34,15 +37,19 @@ const Reminders = (props: IRemindersProps) => {
 }
 
 const Entry = (props: { when: string; actions: ITurnAction[]; idx: number; factionName: TSupportedFaction }) => {
+  const [numVisible, setNumVisible] = useState(props.actions.length)
+  const showEntry = () => setNumVisible(numVisible + 1)
+  const hideEntry = () => setNumVisible(numVisible - 1)
+
   return (
-    <div className="row d-block PageBreak">
+    <div className={`row d-block PageBreak ${!numVisible && `d-print-none`}`}>
       <div className="card border-dark my-3">
         <div className="card-header text-center">
           <h4 className="ReminderHeader">{titleCase(props.when)}</h4>
         </div>
         <div className="card-body">
           {props.actions.map((action, i) => (
-            <ActionText {...action} key={i} />
+            <ActionText {...action} key={i} showEntry={showEntry} hideEntry={hideEntry} />
           ))}
         </div>
       </div>
@@ -64,21 +71,52 @@ const getTitle = ({
   return condition
 }
 
-const ActionText = (props: ITurnAction) => {
-  const { name, desc, command_ability, tag } = props
+const VisibilityToggle = (props: { isVisible: boolean; setVisibility: (e) => void }) => {
+  const { isVisible, setVisibility } = props
+  const VisibilityComponent = isVisible ? MdVisibility : MdVisibilityOff
+  const hideTip = `${isVisible ? `Hidden rules` : `This rule`} will not be printed.`
   return (
     <>
-      <p className="ReminderEntry mb-2">
-        <span className="text-muted font-weight-bold">{getTitle(props)} - </span>
-        <b>
-          {command_ability && `Command Ability: `}
-          {name && `${name}`}
-          {tag && ` (${tag})`}
-        </b>
-        <br />
-        {desc}
-      </p>
+      <IconContext.Provider value={{ size: '1.3em' }}>
+        <VisibilityComponent onClick={setVisibility} data-tip={hideTip} />
+        <ReactTooltip place="bottom" type="light" effect="float" />
+      </IconContext.Provider>
     </>
+  )
+}
+
+interface IActionTextProps extends ITurnAction {
+  hideEntry: () => void
+  showEntry: () => void
+}
+
+const ActionText = (props: IActionTextProps) => {
+  const { name, desc, command_ability, tag, showEntry, hideEntry } = props
+  const [isVisible, setIsVisibile] = useState(true)
+  const handleVisibility = e => {
+    e.preventDefault()
+    setIsVisibile(!isVisible)
+    !isVisible ? showEntry() : hideEntry()
+  }
+
+  return (
+    <div className={`ReminderEntry mb-2 ${!isVisible && `d-print-none`}`}>
+      <div className="d-flex">
+        <div className="flex-grow-1">
+          <span className="text-muted font-weight-bold">{getTitle(props)} - </span>
+          <b>
+            {command_ability && `Command Ability: `}
+            {name && `${name}`}
+            {tag && ` (${tag})`}
+          </b>
+        </div>
+        <div className="px-2 d-print-none">
+          <VisibilityToggle isVisible={isVisible} setVisibility={handleVisibility} />
+        </div>
+      </div>
+
+      {isVisible && desc}
+    </div>
   )
 }
 
