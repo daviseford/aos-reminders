b0845aecde06770521ab2a0e9cf0aee44ce43dd3
thundergore <52839060+thundergore@users.noreply.github.com>
2019-07-22 23:02:27 +0200

    Stormhost layout matched to other army subfaction layouts
    Stormhost specific artifacts and allegiance abilities removed from respective files.
    
    Re-added in "traits.ts" with Khorne "traits.ts" layout.

--- src/army/stormcast_eternals/traits.ts
+++ src/army/stormcast_eternals/traits.ts
@@ -3,6 +3,7 @@ import {
   HERO_PHASE,
   CHARGE_PHASE,
   DURING_GAME,
+  START_OF_GAME,
   END_OF_SETUP,
   DURING_SETUP,
   COMBAT_PHASE,
@@ -14,6 +15,230 @@ import {
 } from 'types/phases'
 
 const CommandTraits: TCommandTraits = [
+  {
+    name: `Hammers of Sigmar`,
+    effects: [
+      {
+        name: `We Cannot Fail`,
+        desc: `Roll a dice each time you allocate a wound or mortal wound to a friendly HAMMERS OF SIGMAR unit wholly within 9" of this general. On a 6+, that wound or mortal wound is negated.`,
+        when: [DURING_GAME],
+      },
+      {
+        name: `First to be Forged`,
+        desc: `Add 1 to the Bravery characteristic of friendly HAMMERS OF SIGMAR units.`,
+        when: [BATTLESHOCK_PHASE],
+        allegiance_ability: true,
+      },
+      {
+        name: `God-forged Blade`,
+        desc: `Pick one of the bearer's melee weapons. If the unmodified hit roll for an attack made with the God-forged Blade is 6, add 1 to the Damage characteristic of that attack.`,
+        when: [COMBAT_PHASE],
+        artifact: true,
+      },
+      {
+        name: `Soul of the Stormhost`,
+        desc: `You can use this command ability when a friendly HAMMERS OF SIGMAR REDEEMER unit is destroyed. If you do so roll a dice. On a 5+ a new unit identical to the one that was destroyed is added to your army. Set up the new unit anywhere on the battlefield, more than 9" from any enemy models.`,
+        when: [COMBAT_PHASE],
+        command_ability: true,
+      },
+    ],
+  },
+  {
+    name: `Hallowed Knights`,
+    effects: [
+      {
+        name: `Martyr's Strength`,
+        desc: `Roll a dice if this general is slain in the combat phase. On a 2+ this general can make a pile-in move and then attack with all of the melee weapons it is armed with, before it is removed from play.`,
+        when: [COMBAT_PHASE],
+      },
+      {
+        name: `Only the Faithful`,
+        desc: `If a friendly HALLOWED KNIGHTS unit is affected by a spell or endless spell, roll a dice. On a 6+ ignore the effects of that spell on that unit.`,
+        when: [HERO_PHASE],
+        allegiance_ability: true,
+      },
+      {
+        name: `Parchment of Purity`,
+        desc: `In your hero phase, heal 1 wound allocated to the bearer.`,
+        when: [HERO_PHASE],
+        artifact: true,
+      },
+      {
+        name: `Holy Crusaders`,
+        desc: `You can use this command ability at the start of your hero phase. Pick a friendly HALLOWED KNIGHTS unit wholly within 9" of a friendly HALLOWED KNIGHTS HERO, or wholly within 18" of a friendly HALLOWED KNIGHTS HERO that is a general. Add 1 to run and charge rolls for that unit until your next hero phase. In addition, until your next hero phase, that unit can run and still charge later in the same turn.`,
+        when: [HERO_PHASE, MOVEMENT_PHASE, CHARGE_PHASE],
+        command_ability: true,
+      },
+    ],
+  },
+  {
+    name: `Celestial Vindicators`,
+    effects: [
+      {
+        name: `Single-minded Fury`,
+        desc: `Each time you roll an unmodified hit roll of 6 for this general, add 1 to the Damage characteristic of that attack.`,
+        when: [COMBAT_PHASE, SHOOTING_PHASE],
+      },
+      {
+        name: `Driven by Vengeance`,
+        desc: `You can re-roll hit rolls of 1 for attacks made by friendly CELESTIAL VINDICATORS units if they made a charge move in the same turn.`,
+        when: [COMBAT_PHASE],
+        allegiance_ability: true,
+      },
+      {
+        name: `Stormrage Blade`,
+        desc: `Pick one of the bearer's melee weapons. At the start of the combat phase, you can add 2 to the Attacks characteristic of this weapon until the end of that phase. If you do so, subtract 1 from save rolls for attacks that target the bearer until the end of that phase.`,
+        when: [START_OF_COMBAT_PHASE],
+        artifact: true,
+      },
+      {
+        name: `Righteous Hatred`,
+        desc: `You can use this command ability at the start of the combat phase. If you do so, you pick a friendly CELESTIAL VINDICATORS unit wholly within 9" of a friendly of a friendly CELESTIAL VINDICATORS HERO, or wholly within 18" of a friendly CELESTIAL VINDICATORS HERO that is a general. Add 1 to the Attacks characteristic of that unit's melee weapons until the end of that phase.`,
+        when: [START_OF_COMBAT_PHASE],
+        command_ability: true,
+      },
+    ],
+  },
+  {
+    name: `Anvils of the Heldenhammer`,
+    effects: [
+      {
+        name: `Deathly Aura`,
+        desc: `Subtract 1 from the Bravery characteristic of enemy units while they are within 6" of this general.`,
+        when: [BATTLESHOCK_PHASE],
+      },
+      {
+        name: `No True Death`,
+        desc: `You can re-roll failed battleshock tests for friendly ANVILS OF THE HELDENHAMMER units.`,
+        when: [BATTLESHOCK_PHASE],
+        allegiance_ability: true,
+      },
+      {
+        name: `Soulthief`,
+        desc: `Pick one of the bearer's melee weapons. At the end of the combat phase, roll a dice for each enemy model that was allocated any wounds caused by this weapon in that combat phase. On a 3+ that model suffers 1 mortal wound.`,
+        when: [END_OF_COMBAT_PHASE],
+        artifact: true,
+      },
+      {
+        name: `Heroes of Another Age`,
+        desc: `You can use this command ability in your hero phase. If you do so, you pick a friendly ANVILS OF THE HELDENHAMMER unit wholly within 9" of a friendly of a friendly ANVILS OF THE HELDENHAMMER HERO, or wholly within 18" of a friendly ANVILS OF THE HELDENHAMMER HERO that is a general. That unit can attack with all the missile weapons it is armed with, or make a pile-in move and attack with all of the melee weapons it is armed with.`,
+        when: [HERO_PHASE],
+        command_ability: true,
+      },
+    ],
+  },
+  {
+    name: `Knights Excelsior`,
+    effects: [
+      {
+        name: `Divine Executioner`,
+        desc: `Add 1 to the Damage characteristic of this general's melee weapons if the target is a HERO.`,
+        when: [COMBAT_PHASE],
+      },
+      {
+        name: `Storm of Annihilation`,
+        desc: `If a friendly KNIGHTS EXCELSIOR unit makes an attack that destroys an enemy unit, you can re-roll hit rolls of 1 for attacks made by that KNIGHTS EXCELSIOR unit for the rest of the battle.`,
+        when: [DURING_GAME],
+        allegiance_ability: true,
+      },
+      {
+        name: `Chains of Celestial Lightning`,
+        desc: `Once per battle, in your hero phase, the bearer can attempt to bind an enemy HERO or MONSTER model within 3". If they do so roll 3D6. Your opponent rolls 2D6 if the target is a HERO or 3D6 if it is a MONSTER or HERO MONSTER. If your roll is higher, until your next hero phase, halve the Move characteristic, run rolls and charge rolls for that enemy model, and halve the Attacks characteristic of its melee weapons. Round any fractions up.`,
+        when: [HERO_PHASE],
+        artifact: true,
+      },
+      {
+        name: `No Mercy`,
+        desc: `You can use this command ability in your hero phase. If you do so, you pick a friendly KNIGHTS EXCELSIOR unit wholly within 9" of a friendly KNIGHTS EXCELSIOR HERO, or wholly within 18" of a friendly ANVILS OF THE HELDENHAMMER HERO that is a general. You can re-roll wound rolls of 1 for attacks made by that unit until the end of the turn.`,
+        when: [HERO_PHASE, SHOOTING_PHASE, COMBAT_PHASE],
+        command_ability: true,
+      },
+    ],
+  },
+  {
+    name: `Celestial Warbringers`,
+    effects: [
+      {
+        name: `Portents and Omens`,
+        desc: `Once per turn, you can re-roll 1 failed hit roll or 1 failed wound roll for an attack made by this general, or 1 save roll for an attack that targets this general.`,
+        when: [COMBAT_PHASE, SHOOTING_PHASE],
+      },
+      {
+        name: `Fearless Foresight`,
+        desc: `At the start of the first battle round, after determining who has the first turn but before the first turn begins, you can pick D3 friendly CELESTIAL WARBRINGERS units and set them up again (any restrictions in the set-up instructions for the battleplan being used still apply).`,
+        when: [START_OF_GAME],
+        allegiance_ability: true,
+      },
+      {
+        name: `Hammers of Augury`,
+        desc: `At the end of the combat phase, you can pick 1 enemy unit within 3" of the bearer and roll a dice. On a 3+, that unit suffers 1 mortal wound and you can roll another dice. On a 4+, that unit suffers 1 extra mortal wound.`,
+        when: [END_OF_COMBAT_PHASE],
+        artifact: true,
+      },
+      {
+        name: `Astral Conjunction`,
+        desc: `You can use this command ability in your hero phase. If you do so, you pick a friendly CELESTIAL WARBRINGER WIZARD wholly within 9" of a friendly CELESTIAL WARBRINGER HERO, or wholly within 18" of a friendly CELESTIAL WARBRINGER HERO that is a general. Add 1 to casting rolls for that unit until the end of that phase.`,
+        when: [HERO_PHASE],
+        command_ability: true,
+      },
+    ],
+  },
+  {
+    name: `Tempest Lords`,
+    effects: [
+      {
+        name: `Bonds of Noble Duty`,
+        desc: `Add 1 to wound rolls for attacks made with this general's melee weapons while this general is within 6" of any other friendly TEMPEST LORDS units.`,
+        when: [COMBAT_PHASE],
+      },
+      {
+        name: `Grand Strategists`,
+        desc: `At the start of your hero phase roll a dice. On a 4+ you receive 1 extra command point.`,
+        when: [HERO_PHASE],
+        allegiance_ability: true,
+      },
+      {
+        name: `Patrician's Helm`,
+        desc: `If the bearer is on the battlefield, each time you spend a command point, roll a dice. On a 5+ you receive 1 extra command point.`,
+        when: [DURING_GAME],
+        artifact: true,
+      },
+      {
+        name: `Rousing Oratory`,
+        desc: `You can use this command ability at the start of the combat phase. If you do so, you pick a friendly TEMPEST LORDS unit wholly within 9" of a friendly TEMPEST LORDS HERO, or wholly within 18" of a friendly TEMPEST LORDS HERO that is a general. You can re-roll wound rolls of 1 for attacks made by that unit until your next hero phase.`,
+        when: [START_OF_COMBAT_PHASE],
+        command_ability: true,
+      },
+    ],
+  },
+  {
+    name: `Astral Templars`,
+    effects: [
+      {
+        name: `Dauntless Hunters`,
+        desc: `After set-up is complete, but before the battle begins, friendly ASTRAL TEMPLARS units wholly within 12" of this general can move up to 6".`,
+        when: [END_OF_SETUP],
+      },
+      {
+        name: `Beast Stalkers`,
+        desc: `Add 1 to hit rolls for attacks made by ASTRAL TEMPLARS units that target a MONSTER.`,
+        when: [COMBAT_PHASE, SHOOTING_PHASE],
+        allegiance_ability: true,
+      },
+      {
+        name: `Godbeast Plate`,
+        desc: `Subtract 1 from wound rolls for attacks made by a MONSTER that target the bearer.`,
+        when: [COMBAT_PHASE, SHOOTING_PHASE],
+        artifact: true,
+      },
+      {
+        name: `Cut off the Head`,
+        desc: `You can use this command ability at the start of the combat phase. If you do so, you pick a friendly ASTRAL TEMPLARS unit wholly within 9" of a friendly ASTRAL TEMPLARS HERO, or wholly within 18" of a friendly ASTRAL TEMPLARS HERO that is a general. Until the end of that phase, add 1 to wound rolls for attacks made by that unit that target a HERO.`,
+        when: [START_OF_COMBAT_PHASE],
+        command_ability: true,
+      },
+    ],
+  },
   {
     name: `Shielded by Faith (Aspects of Azyr)`,
     effects: [
@@ -251,86 +476,6 @@ const CommandTraits: TCommandTraits = [
         name: `Ear-bursting Roar (Mount Trait)`,
         desc: `At the start of the combat phase you can pick an enemy unit within 3" oof this model and roll a dice. On a 4+ subtract 1 from hit rolls for attacks made by that unit until the end of that phase.`,
         when: [START_OF_COMBAT_PHASE],
-      },
-    ],
-  },
-  {
-    name: `We Cannot Fail (Hammers of Sigmar)`,
-    effects: [
-      {
-        name: `We Cannot Fail (Hammers of Sigmar)`,
-        desc: `Roll a dice each time you allocate a wound or mortal wound to a friendly HAMMERS OF SIGMAR unit wholly within 9" of this general. On a 6+, that wound or mortal wound is negated.`,
-        when: [DURING_GAME],
-      },
-    ],
-  },
-  {
-    name: `Martyr's Strength (Hallowed Knights)`,
-    effects: [
-      {
-        name: `Martyr's Strength (Hallowed Knights)`,
-        desc: `Roll a dice if this general is slain in the combat phase. On a 2+ this general can make a pile-in move and then attack with all of the melee weapons it is armed with, before it is removed from play.`,
-        when: [COMBAT_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Single-minded Fury (Celestial Vindicators)`,
-    effects: [
-      {
-        name: `Single-minded Fury (Celestial Vindicators)`,
-        desc: `Each time you roll an unmodified hit roll of 6 for this general, add 1 to the Damage characteristic of that attack.`,
-        when: [COMBAT_PHASE, SHOOTING_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Deathly Aura (Anvils of the Heldenhammer)`,
-    effects: [
-      {
-        name: `Deathly Aura (Anvils of the Heldenhammer)`,
-        desc: `Subtract 1 from the Bravery characteristic of enemy units while they are within 6" of this general.`,
-        when: [BATTLESHOCK_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Divine Executioner (Knights Excelsior)`,
-    effects: [
-      {
-        name: `Divine Executioner (Knights Excelsior)`,
-        desc: `Add 1 to the Damage characteristic of this general's melee weapons if the target is a HERO.`,
-        when: [COMBAT_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Portents and Omens (Celestial Warbringers)`,
-    effects: [
-      {
-        name: `Portents and Omens (Celestial Warbringers)`,
-        desc: `Once per turn, you can re-roll 1 failed hit roll or 1 failed wound roll for an attack made by this general, or 1 save roll for an attack that targets this general.`,
-        when: [COMBAT_PHASE, SHOOTING_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Bonds of Noble Duty (Tempest Lords)`,
-    effects: [
-      {
-        name: `Bonds of Noble Duty (Tempest Lords)`,
-        desc: `Add 1 to wound rolls for attacks made with this general's melee weapons while this general is within 6" of any other friendly TEMPEST LORDS units.`,
-        when: [COMBAT_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Dauntless Hunters (Astral Templars)`,
-    effects: [
-      {
-        name: `Dauntless Hunters (Astral Templars)`,
-        desc: `After set-up is complete, but before the battle begins, friendly ASTRAL TEMPLARS units wholly within 12" of this general can move up to 6".`,
-        when: [END_OF_SETUP],
       },
     ],
   },
