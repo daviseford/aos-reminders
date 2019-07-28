43a2ac0eaba59e9d778181f3d07736ebc9fbc733
John Schramm <jas1535@hotmail.com>
2019-07-28 11:05:36 -0600

    * Updated units.
    * Updated abilities.
    
    * Added lore spells placeholder file (commented out).
    
    * Updated files based on existing errata.

--- src/army/nurgle/artifacts.ts
+++ src/army/nurgle/artifacts.ts
@@ -1,14 +1,212 @@
 import { TArtifacts } from 'types/army'
-import { HERO_PHASE } from 'types/phases'
+import {
+  BATTLESHOCK_PHASE,
+  COMBAT_PHASE,
+  END_OF_COMBAT_PHASE,
+  HERO_PHASE,
+  MOVEMENT_PHASE,
+  SHOOTING_PHASE,
+  START_OF_BATTLESHOCK_PHASE,
+  START_OF_COMBAT_PHASE,
+  START_OF_HERO_PHASE,
+} from 'types/phases'
 
 const Artifacts: TArtifacts = [
   {
-    name: ``,
+    name: `The Splithorn Helm (Rotbringers)`,
     effects: [
       {
-        name: ``,
-        desc: ``,
+        name: `The Splithorn Helm (Rotbringers)`,
+        desc: `Roll a dice each time you allocate a wound or mortal wound to the bearer.  On a 6+ the wound is negated.`,
+        when: [HERO_PHASE, SHOOTING_PHASE, COMBAT_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `Muttergrub (Rotbringers)`,
+    effects: [
+      {
+        name: `Muttergrub (Rotbringers)`,
+        desc: `If the bearer is a wizard, they can attempt to cast one additional spell.  If the bearer is not a wizard they can attempt to cast Foul Regenesis.  Does not provide the ability to unbind.`,
         when: [HERO_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `Rustfang (Rotbringers)`,
+    effects: [
+      {
+        name: `Rustfang (Rotbringers)`,
+        desc: `Pick one enemy unit within 3" of the bearer.  Subtract 1 from its save rolls for the rest of the battle.  You cannot use this ability more than once on the same enemy unit.`,
+        when: [START_OF_COMBAT_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `Flesh Pealer (Rotbringers)`,
+    effects: [
+      {
+        name: `Flesh Pealer (Rotbringers)`,
+        desc: `Roll a dice for each enemy unit that is within 6" of the bearer.  On a 5+ the unit being rolled for suffers D3 mortal wounds.`,
+        when: [HERO_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `The Bileheart (Rotbringers)`,
+    effects: [
+      {
+        name: `The Bileheart (Rotbringers)`,
+        desc: `Roll a dice each time you allocate a wound or mortal wound to the bearer in the combat phase (and it is not negated).  On a 4+ the attacking unit suffers 1 mortal wound after all its attacks have been made.`,
+        when: [COMBAT_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `The Fecund Flask (Rotbringers)`,
+    effects: [
+      {
+        name: `The Fecund Flask (Rotbringers)`,
+        desc: `Once per battle roll a dice.  On a 2+ any wounds suffered by the are healed.  On a 1 the bearer is slain.  If the bearer is slain by the flask, you can add 1 Beast of Nurgle within 1" of the bearer.`,
+        when: [START_OF_HERO_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `Noxious Nexus (Daemon)`,
+    effects: [
+      {
+        name: `Noxious Nexus (Daemon)`,
+        desc: `Roll a dice for each enemy unit within 7" of the bearer.  If the roll is equal to or less than the number of the current battle round, the unit being rolled for suffers 1 mortal wound.`,
+        when: [START_OF_HERO_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `Nurgle's Nail (Daemon)`,
+    effects: [
+      {
+        name: `Nurgle's Nail (Daemon)`,
+        desc: `Pick one of the bearer's melee weapons to be Nurgle's Nail.  Roll 2D6 for each enemy model that was allocated any wounds caused by Nurgle's Nail and not slain.  If the result is exactly 7 the model being rolled for is slain.`,
+        when: [END_OF_COMBAT_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `The Bountiful Swarm (Daemon)`,
+    effects: [
+      {
+        name: `The Bountiful Swarm (Daemon)`,
+        desc: `Pick an enemy model within 3" of the bearer and roll a dice.  If the roll is greater then that model's Wounds characteristic, then it is slain.  If a model with a wounds characteristic of 4+ is slain, you can set up a Beast of Nurgle within 1" of the slain model.`,
+        when: [START_OF_HERO_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `The Witherstave (Daemon)`,
+    effects: [
+      {
+        name: `The Witherstave (Daemon)`,
+        desc: `Re-roll hits of 6 for enemy units while they are within 12" of the bearer.`,
+        when: [SHOOTING_PHASE, COMBAT_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `Tome of a Thousand Poxes (Daemon)`,
+    effects: [
+      {
+        name: `Tome of a Thousand Poxes (Daemon)`,
+        desc: `If the bearer is a wizard, add 1 to the casting rolls for any spells from the Lore of Nurgle that they attempt to cast.  If the bearer is not a wizard they can attempt to cast the Sumptuous Pestilence spell.  This does not provide the unit with an unbind.`,
+        when: [HERO_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `The Endless Gift (Daemon)`,
+    effects: [
+      {
+        name: `The Endless Gift (Daemon)`,
+        desc: `Roll a dice for each wound that was allocated to this model in that turn and not negated.  On a 4+ the wound is healed.`,
+        when: [START_OF_BATTLESHOCK_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `The Virulent Blade (Mortal)`,
+    effects: [
+      {
+        name: `The Virulent Blade (Mortal)`,
+        desc: `Pick one of the bearer's melee weapons to be the Virulent Blade.  Add 1 to the damage characteristic for attacks made by this weapon for wound rolls of 5+.`,
+        when: [COMBAT_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `The Foetid Shroud (Mortal)`,
+    effects: [
+      {
+        name: `The Foetid Shroud (Mortal)`,
+        desc: `Re-roll hit rolls of 6+ or more for attacks that target the bearer in the combat phase.`,
+        when: [COMBAT_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `Sublucus' Stenchplate (Mortal)`,
+    effects: [
+      {
+        name: `Sublucus' Stenchplate (Mortal)`,
+        desc: `Enemy units that are within 3" of the bearer at the end of their movement phase suffer D3 mortal wounds.`,
+        when: [MOVEMENT_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `The Eye of Nurgle (Mortal)`,
+    effects: [
+      {
+        name: `The Eye of Nurgle (Mortal)`,
+        desc: `Once per battle, you can roll 2D6 if there are any enemy models within 12" of the bearer.  If the result is exactly 7, the the closest enemy model to the bearer is slain.`,
+        when: [START_OF_HERO_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `The Carrion Dirge (Mortal)`,
+    effects: [
+      {
+        name: `The Carrion Dirge (Mortal)`,
+        desc: `Subtract 2 from the Bravery characteristic of enemy unit while they are within 12" of the bearer.`,
+        when: [HERO_PHASE, SHOOTING_PHASE, COMBAT_PHASE, BATTLESHOCK_PHASE],
+        artifact: true,
+      },
+    ],
+  },
+  {
+    name: `The Shield of Growths (Mortal)`,
+    effects: [
+      {
+        name: `The Shield of Growths (Mortal)`,
+        desc: `You can re-roll failed save rolls for the bearer if the roll is equal to or less than the number of wounds currently allocated to the bearer.`,
+        when: [HERO_PHASE, SHOOTING_PHASE, COMBAT_PHASE],
+        artifact: true,
       },
     ],
   },
--- src/army/nurgle/traits.ts
+++ src/army/nurgle/traits.ts
@@ -1,5 +1,5 @@
 import { TCommandTraits } from 'types/army'
-import { 
+import {
   BATTLESHOCK_PHASE,
   CHARGE_PHASE,
   COMBAT_PHASE,
@@ -17,30 +17,33 @@ const CommandTraits: TCommandTraits = [
     effects: [
       {
         name: `Grandfather's Blessing`,
-        desc: `Once per battle, at the start of your hero phase, you can move the Cycle of Corruption one stage forward or backward if your general has not been slain.`,
+        desc: `Once per battle, you can move the Cycle of Corruption one stage forward or backward if your general has not been slain.`,
         when: [START_OF_HERO_PHASE],
+        command_trait: true,
       },
-    ],    
+    ],
   },
   {
     name: `Living Plague`,
     effects: [
       {
         name: `Living Plague`,
-        desc: `At the start of your hero phase, roll a dice for each enemy unit within 1" of your general.  On a 4+ the unit being rolled for suffers 1 mortal wound and your receive 1 contagion point.`,
+        desc: `Roll a dice for each enemy unit within 1" of your general.  On a 4+ the unit being rolled for suffers 1 mortal wound and your receive 1 contagion point.`,
         when: [START_OF_HERO_PHASE],
+        command_trait: true,
       },
-    ],    
+    ],
   },
-  {    
+  {
     name: `Hulking Physique`,
     effects: [
       {
         name: `Hulking Physique`,
         desc: `Add 1 to the wound rolls for your general in the combat phase.`,
         when: [COMBAT_PHASE],
+        command_trait: true,
       },
-    ],    
+    ],
   },
   {
     name: `Bloated with Corruption (Rotbringer)`,
@@ -49,6 +52,7 @@ const CommandTraits: TCommandTraits = [
         name: `Bloated with Corruption (Rotbringer)`,
         desc: `Roll a dice each time you allocate a wound to your general in the combat phase (and it is not negated).  On a 4+ the attacking unit suffers 1 mortal wound after all its attacks have been made.`,
         when: [COMBAT_PHASE],
+        command_trait: true,
       },
     ],
   },
@@ -59,8 +63,9 @@ const CommandTraits: TCommandTraits = [
         name: `Avalanche of Rotten Flesh (Rotbringer)`,
         desc: `Add 2 to the run and charge rolls for your general.`,
         when: [MOVEMENT_PHASE, CHARGE_PHASE],
+        command_trait: true,
       },
-    ],    
+    ],
   },
   {
     name: `Resilient (Rotbringer)`,
@@ -69,16 +74,18 @@ const CommandTraits: TCommandTraits = [
         name: `Resilient (Rotbringer)`,
         desc: `Roll a dice each time you allocate a wound or mortal wound to your general.  On a 6+ the wound is negated.`,
         when: [HERO_PHASE, SHOOTING_PHASE, COMBAT_PHASE],
+        command_trait: true,
       },
-    ],    
+    ],
   },
   {
     name: `Tainted Corruptor (Daemon)`,
     effects: [
       {
         name: `Tainted Corruptor (Daemon)`,
-        desc: `At the start of each of your hero phases, you can pick one terrain feature that is within 3" of your general.  For the rest of the battle that terrain feature has the Sickness Blossoms scenery rule from the Feculent Gnarlmaw warscroll in addtion to any other rules it already had.`,
+        desc: `You can pick one terrain feature that is within 3" of your general.  For the rest of the battle that terrain feature has the Sickness Blossoms scenery rule from the Feculent Gnarlmaw warscroll in addtion to any other rules it already had.`,
         when: [START_OF_HERO_PHASE],
+        command_trait: true,
       },
     ],
   },
@@ -87,8 +94,9 @@ const CommandTraits: TCommandTraits = [
     effects: [
       {
         name: `Nurgling Infestation (Daemon)`,
-        desc: `Once per battle, at the start of a combat phase, you can inflict D3 mortal wounds on an enemy unit that is within 3" of your general.`,
+        desc: `Once per battle, you can inflict D3 mortal wounds on an enemy unit that is within 3" of your general.`,
         when: [START_OF_COMBAT_PHASE],
+        command_trait: true,
       },
     ],
   },
@@ -97,8 +105,9 @@ const CommandTraits: TCommandTraits = [
     effects: [
       {
         name: `Pestilent Breath (Daemon)`,
-        desc: `At the start of your shooting phase, pick one enemy unit within 6" of your general.  Roll a dice for each model in that unit that is within 6" of your general.  The unit suffers 1 mortal wound for each roll of 5+.`,
+        desc: `Pick one enemy unit within 6" of your general.  Roll a dice for each model in that unit that is within 6" of your general.  The unit suffers 1 mortal wound for each roll of 5+.`,
         when: [START_OF_SHOOTING_PHASE],
+        command_trait: true,
       },
     ],
   },
@@ -109,6 +118,7 @@ const CommandTraits: TCommandTraits = [
         name: `Hideous Visage (Mortal)`,
         desc: `Subtract 2 from the Bravery characterisic of enemy units while they are within 3" of your general.`,
         when: [HERO_PHASE, SHOOTING_PHASE, COMBAT_PHASE, BATTLESHOCK_PHASE],
+        command_trait: true,
       },
     ],
   },
@@ -119,6 +129,7 @@ const CommandTraits: TCommandTraits = [
         name: `Overpowering Stench (Mortal)`,
         desc: `Re-roll hit rolls of 6+ for attacks that target your general in the combat phase.`,
         when: [COMBAT_PHASE],
+        command_trait: true,
       },
     ],
   },
@@ -129,6 +140,7 @@ const CommandTraits: TCommandTraits = [
         name: `Virulent Contagion (Mortal)`,
         desc: `Improve the Rend characterisic by 1 for attacks made by your general in the combat phase.`,
         when: [COMBAT_PHASE],
+        command_trait: true,
       },
     ],
   },
