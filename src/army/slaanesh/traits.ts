a5be273d84c4c62c26790def221e9b6d07287804
John Schramm <jas1535@hotmail.com>
2019-07-29 16:50:35 -0600

    * Add Slaanesh (Fixes #42)
    * Added template files.

---
+++ src/army/slaanesh/traits.ts
@@ -0,0 +1,18 @@
+import { TCommandTraits } from 'types/army'
+import { HERO_PHASE } from 'types/phases'
+
+const CommandTraits: TCommandTraits = [
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [HERO_PHASE],
+        command_trait: true,
+      },
+    ],
+  },
+]
+
+export default CommandTraits
