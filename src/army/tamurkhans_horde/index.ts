5d53146b88841dbe184fa66f246b652e0be4955e
John Schramm <jas1535@hotmail.com>
2019-07-31 14:31:33 -0600

    * Updated Tamurkhan's Horde to utilize imports from Nurgle.
    * Updated Nurgle to use imports for Tamurkhan's Horde.

--- src/army/tamurkhans_horde/index.ts
+++ src/army/tamurkhans_horde/index.ts
@@ -2,5 +2,7 @@ import { Battalions, Units } from './units'
 import Abilities from './abilities'
 import Artifacts from './artifacts'
 import Traits from './traits'
+//import Spells from 'army/nurgle'
 
 export default { Abilities, Artifacts, Battalions, Traits, Units }
+//export default { Abilities, Artifacts, Spells, Battalions, Traits, Units }
