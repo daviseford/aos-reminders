51e5e9e3fc0013f665219b1bc614883189bfe2fa
Davis Ford <davis.e.ford@gmail.com>
2019-07-30 22:37:29 -0400

    Adding Spells/EndlessSpells to indexes

--- src/army/nurgle/index.ts
+++ src/army/nurgle/index.ts
@@ -1,14 +1,15 @@
 import { Battalions, Units } from './units'
 import Abilities from './abilities'
 import Artifacts from './artifacts'
+import Spells from './spells'
 import Traits from './traits'
-//import Spells from './spells'
 import { Battalions as TamurkhansBattalions, Units as TamurkhansUnits } from 'army/tamurkhans_horde/units'
 
 export default {
   Abilities,
   Artifacts,
   Battalions: Battalions.concat(TamurkhansBattalions),
+  Spells,
   Traits,
   Units: Units.concat(TamurkhansUnits),
 }
