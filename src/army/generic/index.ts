308f554c5bdcd59cc1690507707b8efe0243129c
Davis Ford <davis.e.ford@gmail.com>
2019-08-01 17:01:48 -0400

    Move endless spells to Generic

--- src/army/generic/index.ts
+++ src/army/generic/index.ts
@@ -1,4 +1,6 @@
-import Spells from './spells'
+import GenericEndlessSpells from './endless_spells'
+import GenericSpells from './spells'
+
 // This file is used for data re-used across any army
 // For example, Arcane Bolt and Mystic Shield are spells that any army can use
-export default { Spells }
+export { GenericEndlessSpells, GenericSpells }
--- src/army/generic/spells.ts
+++ src/army/generic/spells.ts
@@ -1,4 +1,4 @@
-import { HERO_PHASE } from 'types/phases'
+import { HERO_PHASE, COMBAT_PHASE, SHOOTING_PHASE } from 'types/phases'
 import { TSpells } from 'types/army'
 
 // Spells, Prayers, etc. go here
@@ -8,7 +8,7 @@ const Spells: TSpells = [
     effects: [
       {
         name: `Arcane Bolt`,
-        desc: ``,
+        desc: `Casting value of 5. If successfully cast, pick an enemy unit within 18" of the caster that is visible to them. That unit suffers 1 mortal wound. If the casting roll was 10 or more, the unit suffers D3 mortal wounds instead.`,
         when: [HERO_PHASE],
       },
     ],
@@ -18,8 +18,8 @@ const Spells: TSpells = [
     effects: [
       {
         name: `Mystic Shield`,
-        desc: ``,
-        when: [HERO_PHASE],
+        desc: `Casting value of 6. If successfully cast, pick a friendly unit within 18" of the caster that is visible to them. Re-roll save rolls of 1 for that unit until your next hero phase.`,
+        when: [HERO_PHASE, COMBAT_PHASE, SHOOTING_PHASE],
       },
     ],
   },
