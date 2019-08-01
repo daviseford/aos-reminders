3eba3aa2c266a99e8b85efd2818d5115b098564a
Davis Ford <davis.e.ford@gmail.com>
2019-07-31 21:58:51 -0400

    Add Spells

--- src/army/legions_of_azgorh/index.ts
+++ src/army/legions_of_azgorh/index.ts
@@ -1,8 +1,7 @@
 import { Battalions, Units } from './units'
 import Abilities from './abilities'
 import Artifacts from './artifacts'
-//import Spells from './spells'
+import Spells from './spells'
 import Traits from './traits'
 
-//export default { Abilities, Artifacts, Battalions, Spells, Traits, Units }
-export default { Abilities, Artifacts, Battalions, Traits, Units }
+export default { Abilities, Artifacts, Battalions, Spells, Traits, Units }
--- src/army/malign_sorcery/index.ts
+++ src/army/malign_sorcery/index.ts
@@ -1,5 +1,6 @@
 import RealmArtifacts from './artifacts'
 import RealmscapeFeatures from './realmscape_features'
 import EndlessSpells from './endless_spells'
+import Spells from './spells'
 
-export { EndlessSpells, RealmArtifacts, RealmscapeFeatures }
+export { EndlessSpells, RealmArtifacts, RealmscapeFeatures, Spells }
