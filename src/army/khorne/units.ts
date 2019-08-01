0fc240bd47a880c0c8fcf7d6825772ef7c6e739e
John Schramm <jas1535@hotmail.com>
2019-08-01 15:30:13 -0600

    * Unify Chaos (Fixes #166)
    * Added links for valid subsets of Slaves/Everchosen units to god specific armies.
    
    * Removed small warning from SCE spells file found while compiling.

--- src/army/khorne/units.ts
+++ src/army/khorne/units.ts
@@ -17,9 +17,41 @@ import {
   START_OF_MOVEMENT_PHASE,
   START_OF_TURN,
 } from 'types/phases'
+import SlavesToDarkness from 'army/slaves_to_darkness'
+import Everchosen from 'army/everchosen'
+
+// Importing Nurgle markable Slaves to Darkness units.
+const getSlavesUnits = () => {
+  const listOfUnits = [
+    `Daemon Prince`,
+    `Chaos Lord on Manticore`,
+    `Chaos Lord on Daemonic Mount`,
+    `Lord of Chaos`,
+    `Exalted Hero of Chaos`,
+    `Chaos Marauders`,
+    `Chaos Chariot`,
+    `Gorebeast Chariot`,
+    `Chaos Chosen`,
+    `Chaos Warriors`,
+    `Chaos Warshrine`,
+    `Chaos Knights`,
+    `Chaos Marauder Horsemen`,
+    `Chaos War Mammoth`,
+  ]
+  return SlavesToDarkness.Units.filter(unit => listOfUnits.includes(unit.name))
+}
+
+// Importing god aligned Everchosen units.
+const getEverchosenUnits = () => {
+  const listOfUnits = [`Archaon`]
+  return Everchosen.Units.filter(unit => listOfUnits.includes(unit.name))
+}
 
 // Unit Names
 export const Units: TUnits = [
+  // Import Everchosen/Slaves to Darkness Units
+  ...getSlavesUnits(),
+  ...getEverchosenUnits(),
   {
     name: `Korghos Khul`,
     effects: [
