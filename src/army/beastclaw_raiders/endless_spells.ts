30165f5ce1fcd7e7f91d6851483490c56b919413
JohnSchramm <53314953+JohnSchramm@users.noreply.github.com>
2019-08-03 12:39:37 -0600

    * Added placeholder files for factions with no current spell lore or endless spells. (#160). (#177)

---
+++ src/army/beastclaw_raiders/endless_spells.ts
@@ -0,0 +1,6 @@
+import { TEndlessSpells } from 'types/army'
+//import { HERO_PHASE } from 'types/phases'
+
+export const EndlessSpells: TEndlessSpells = []
+
+export default EndlessSpells
---
+++ src/army/kharadron_overlords/spells.ts
@@ -0,0 +1,6 @@
+import { TSpells } from 'types/army'
+//import { HERO_PHASE } from 'types/phases'
+
+const Spells: TSpells = []
+
+export default Spells
