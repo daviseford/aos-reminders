ce5630d7e09c91e1493151a103b80df02dd4ef0a
Davis Ford <daviseford@users.noreply.github.com>
2019-08-08 22:10:48 -0400

    Add Skaven traits and abilities (fixes #193) (#202)
    * Add Skaven traits and abilities (fixes #193)
    
    * Update allegiances.ts

--- src/army/skaven/abilities.ts
+++ src/army/skaven/abilities.ts
@@ -1,9 +1,12 @@
 import {
-  HERO_PHASE,
-  START_OF_SETUP,
-  START_OF_MOVEMENT_PHASE,
-  MOVEMENT_PHASE,
+  BATTLESHOCK_PHASE,
   CHARGE_PHASE,
+  COMBAT_PHASE,
+  DURING_GAME,
+  HERO_PHASE,
+  MOVEMENT_PHASE,
+  START_OF_MOVEMENT_PHASE,
+  START_OF_SETUP,
 } from 'types/phases'
 import { IEffects } from 'types/data'
 
@@ -28,6 +31,26 @@ const Abilities: IEffects[] = [
     name: `Gnawhole: Aura of the Horned Rat`,
     desc: `Non-SKAVENTIDE units treat this terrain feature as having the Deadly scenery rule.`,
     when: [MOVEMENT_PHASE, CHARGE_PHASE],
+  },
+  {
+    name: `Lead From The Back`,
+    desc: `The Look Out, Sir! rule applies to an attack made with a melee weapon as well as an attack made with a missile weapon if the target of the attack is a SKAVENTIDE HERO that is not a MONSTER.`,
+    when: [DURING_GAME],
+  },
+  {
+    name: `Scurry Away`,
+    desc: `In the combat phase, when you pick a friendly SKAVENTIDE HERO to fight with, you can say it is going to scurry away instead of making a pile-in move and then attacking. If you do so, that HERO must make a normal move, and must retreat.`,
+    when: [COMBAT_PHASE],
+  },
+  {
+    name: `Overwhelming Mass`,
+    desc: `Add 1 to hit rolls for attacks made with melee weapons by SKAVENTIDE units while they have 20 or more models. In addition, add 1 to wound rolls for attacks made with melee weapons by SKAVENTIDE units while they have 30 or more models.`,
+    when: [COMBAT_PHASE],
+  },
+  {
+    name: `Strength in Numbers`,
+    desc: `When a SKAVENTIDE unit takes a battleshock test, add 2 to its Bravery characteristic instead of 1 for every 10 models in the unit.`,
+    when: [BATTLESHOCK_PHASE],
   },
 ]
 
