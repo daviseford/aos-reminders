d0de71fb6f239541f765bab87120a6e0876663ed
thundergore <52839060+thundergore@users.noreply.github.com>
2019-08-10 19:25:26 +0200

    Removed duplicate traits/artifacts
    Already present in UI from destruction over-faction

--- src/army/gutbusters/artifacts.ts
+++ src/army/gutbusters/artifacts.ts
@@ -1,65 +1,15 @@
 import { TArtifacts } from 'types/army'
-import { HERO_PHASE, COMBAT_PHASE, START_OF_MOVEMENT_PHASE, DURING_GAME } from 'types/phases'
+import { HERO_PHASE } from 'types/phases'
 
 // These Are The General Allegiance Artifacts From Destruction
 const Artifacts: TArtifacts = [
   {
-    name: `Hammerblade`,
+    name: ``,
     effects: [
       {
-        name: `Hammerblade`,
-        desc: `Pick a melee weapon. Instead of attacking with it, roll a dice for each model within 3" of the bearer (apart from the bearer). On a 5+ 1 mortal wound is inflicted on that model's unit.`,
-        when: [COMBAT_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Battered Talisman`,
-    effects: [
-      {
-        name: `Battered Talisman`,
-        desc: `5+ to ignore mortal wounds allocated to bearer.`,
-        when: [DURING_GAME],
-      },
-    ],
-  },
-  {
-    name: `Rockeye`,
-    effects: [
-      {
-        name: `Rockeye`,
-        desc: `Pick an enemy unit within 12". Until your next hero phase, add 1 to hit rolls made for the bearer when they target that unit.`,
+        name: ``,
+        desc: ``,
         when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Bellowing Blade`,
-    effects: [
-      {
-        name: `Bellowing Blade`,
-        desc: `When the bearer is picked to fight, you can re-roll one wound roll for the bearer's attacks in that fight for each enemy HERO within 12" of the bearer when they were picked.`,
-        when: [COMBAT_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Collar of Domination`,
-    effects: [
-      {
-        name: `Collar of Domination`,
-        desc: `At the start of your opponent's movement phase, pick an enemy MONSTER within 3" of the bearer and roll 2D6. If the roll is equal to or greater than their bravery it must retreat in that movement phase.`,
-        when: [START_OF_MOVEMENT_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Battle Brew`,
-    effects: [
-      {
-        name: `Battle Brew`,
-        desc: `Once per battle, take either 1 or 2 swigs. 1 swig = +1 to hit and wound until your next hero phase. 2 swigs = +2 to hit and wound rolls until your next hero phase AND you take D6 mortal wounds at the end of the turn they drank.`,
-        when: [DURING_GAME],
       },
     ],
   },
--- src/army/gutbusters/traits.ts
+++ src/army/gutbusters/traits.ts
@@ -1,64 +1,14 @@
 import { TTraits } from 'types/army'
-import { HERO_PHASE, COMBAT_PHASE, DURING_GAME } from 'types/phases'
+import { HERO_PHASE } from 'types/phases'
 
 // These Are The General Allegiance Traits From Destruction
 const CommandTraits: TTraits = [
   {
-    name: `Nothing Left Standing`,
+    name: ``,
     effects: [
       {
-        name: `Nothing Left Standing`,
-        desc: `Pick a terrain feature within 6" of this general and at least 5 other DESTRUCTION models. For the rest of the battle, that feature no longer grants cover.`,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Might is Right`,
-    effects: [
-      {
-        name: `Might is Right`,
-        desc: `Add 1 to wound rolls made for this general's melee weapons.`,
-        when: [COMBAT_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Wild Fury`,
-    effects: [
-      {
-        name: `Wild Fury`,
-        desc: `When you pick this general to fight, pick a melee weapon to add 1 attack to. If the general has 3 wounds or more allocated, add 2 attacks instead.`,
-        when: [COMBAT_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Bellowing Tyrant`,
-    effects: [
-      {
-        name: `Bellowing Tyrant`,
-        desc: `Pick a friendly DESTRUCTION unit within 6" of this general. Until your next hero phase, add 1 to charge and run rolls for the unit. Use the general's bravery characteristic instead of the unit's own.`,
-        when: [HERO_PHASE],
-      },
-    ],
-  },
-  {
-    name: `Big and Brutish`,
-    effects: [
-      {
-        name: `Big and Brutish`,
-        desc: `+1 wound characteristic.`,
-        when: [DURING_GAME],
-      },
-    ],
-  },
-  {
-    name: `Ravager`,
-    effects: [
-      {
-        name: `Ravager`,
-        desc: `Add 3 to the Rampaging Destroyers dice roll for this general.`,
+        name: ``,
+        desc: ``,
         when: [HERO_PHASE],
       },
     ],
