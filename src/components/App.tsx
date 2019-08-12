92481b0e87014aa3d921f00393d2427b4c07decc
Davis Ford <davis.e.ford@gmail.com>
2019-08-11 20:15:52 -0400

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
 import { logPageView } from 'utils/analytics'
+import { AlliedArmies } from 'components/input/ally_armies'
 import { ArmyBuilder } from 'components/input/army_builder'
 import { FooterComponent } from 'components/page/footer'
 import { Header } from 'components/page/header'
 import { PrintFooterComponent, PrintArmy } from 'components/print/print'
 import { Reminders } from 'components/info/reminders'
 import { Toolbar } from 'components/input/toolbar'
-import { AlliedArmies } from './input/ally_armies'
 
 const App = () => {
   useEffect(() => {
