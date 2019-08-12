7c3fac4f7d0700211f2bb87f59176051da3c24dd
Davis Ford <davis.e.ford@gmail.com>
2019-08-11 20:03:10 -0400

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,14 @@
 import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
+import { logPageView } from 'utils/analytics'
 import { ArmyBuilder } from 'components/input/army_builder'
 import { FooterComponent } from 'components/page/footer'
 import { Header } from 'components/page/header'
 import { PrintFooterComponent, PrintArmy } from 'components/print/print'
 import { Reminders } from 'components/info/reminders'
 import { Toolbar } from 'components/input/toolbar'
-import { factionNames, selections } from 'ducks'
-import { logPageView } from 'utils/analytics'
-import { TSupportedFaction } from 'meta/factions'
 import { AlliedArmies } from './input/ally_armies'
-import { IStore } from 'types/store'
 
-interface IAppProps {
-  factionName: TSupportedFaction
-  resetSelections: () => void
-}
-
-const App = (props: IAppProps) => {
+const App = () => {
   useEffect(() => {
     logPageView()
   }, [])
@@ -43,16 +34,4 @@ const App = (props: IAppProps) => {
   )
 }
 
-const mapStateToProps = (state: IStore, ownProps) => ({
-  ...ownProps,
-  factionName: factionNames.selectors.getFactionName(state),
-})
-
-const mapDispatchToProps = {
-  resetSelections: selections.actions.resetSelections,
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
+export default App
