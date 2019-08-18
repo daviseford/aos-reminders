6196d100a9aae1d99b711efa023e13682bec48a4
Davis Ford <davis.e.ford@gmail.com>
2019-08-18 10:22:20 -0400

    Update index.ts

--- src/army/generic/index.ts
+++ src/army/generic/index.ts
@@ -9,7 +9,6 @@ import RealmscapeFeatures from './realmscape_features'
 import RealmscapeSpells from './realmscape_spells'
 
 // This file is used for data re-used across any army.
-// For example, Arcane Bolt and Mystic Shield are spells that any army can use.
 export {
   GenericCommands,
   GenericEndlessSpells,
