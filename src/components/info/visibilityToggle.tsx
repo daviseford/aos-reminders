18cffc710167cfe04c1e019bbd8d41d4e4f68d92
Davis Ford <daviseford@users.noreply.github.com>
2019-08-19 13:30:22 -0400

    Replace eye with fold/unfold (#268)

--- src/components/info/visibilityToggle.tsx
+++ src/components/info/visibilityToggle.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
-import { IconContext } from 'react-icons'
-import { MdVisibility, MdVisibilityOff } from 'react-icons/md'
+import { IconContext, IconType } from 'react-icons'
+import { MdVisibility, MdVisibilityOff, MdUnfoldLess, MdUnfoldMore } from 'react-icons/md'
+
+type TIconType = 'eye' | 'fold'
 
 interface IVisibilityToggleProps {
   isVisible: boolean
   setVisibility: (e) => void
   size?: number
+  type?: TIconType
+}
+
+const icons: { [key in TIconType]: { visible: IconType; hidden: IconType } } = {
+  eye: {
+    visible: MdVisibility,
+    hidden: MdVisibilityOff,
+  },
+  fold: {
+    visible: MdUnfoldLess,
+    hidden: MdUnfoldMore,
+  },
 }
 
 export const VisibilityToggle: React.FC<IVisibilityToggleProps> = props => {
-  const { isVisible, setVisibility, size = 1.4 } = props
-  const VisibilityComponent = isVisible ? MdVisibility : MdVisibilityOff
+  const { isVisible, setVisibility, size = 1.4, type = 'eye' } = props
+  const icon = icons[type]
+  const VisibilityComponent = isVisible ? icon.visible : icon.hidden
+
   return (
     <>
       <IconContext.Provider value={{ size: `${size}em` }}>
