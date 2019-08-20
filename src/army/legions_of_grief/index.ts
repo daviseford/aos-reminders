2129b7da18e3df5fac26dc99205878754aa4faea
Zirhark <chris.thiel987@gmail.com>
2019-08-20 15:03:22 -0400

    Legion of Grief (fixes #273) (#278)
    * General cleanup for Fyreslayers
    
    - updated some formats in endless spells
    - Clarified runes in abilities
    - Added the rest of the rules for the magmic forge
    
    * Creating Legion of Grief
    
    Created the outline of LoG.
    
    * Added Units and abilities
    
    * Finished Legions of Grief
    
    Also cleaned up some LoN units.
    
    * Cleaning up comments
    
    * Remove Battalions

---
+++ src/army/legions_of_grief/index.ts
@@ -0,0 +1,13 @@
+import { Units } from './units'
+import Abilities from './abilities'
+import Artifacts from './artifacts'
+import Spells from './spells'
+import Traits from './traits'
+
+export default {
+  Abilities,
+  Artifacts,
+  Spells,
+  Traits,
+  Units,
+}
---
+++ src/army/legions_of_grief/traits.ts
@@ -0,0 +1,37 @@
+import { TTraits } from 'types/army'
+import { BATTLESHOCK_PHASE, DURING_GAME, HERO_PHASE } from 'types/phases'
+
+const CommandTraits: TTraits = [
+  {
+    name: `Amathyst Glow`,
+    effects: [
+      {
+        name: `Amathyst Glow`,
+        desc: `This general is a WIZARD. They can attempt to cast one spell in you hero phase, and attempt to unbind one spell in the enemy hero phase. They know the arcane bolt and mystic shield spells.\n\nIf this general is already a WIZARD, they know one extra spell from the Lore of Sorrows.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Vassal of the Craven King`,
+    effects: [
+      {
+        name: `Vassal of the Craven King`,
+        desc: `If this general is on the battlefield, each time you spend a command point, roll a dice. On a 5+ you receive 1 extra command point.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Tragic Emanations`,
+    effects: [
+      {
+        name: `Tragic Emanations`,
+        desc: `Subtract 2 from the Bravery characteristic of enemy units while they are within 12" of this general.`,
+        when: [BATTLESHOCK_PHASE],
+      },
+    ],
+  },
+]
+
+export default CommandTraits
