ee021d3002e37340fc1dfd431de556ed40b6c1ff
Davis Ford <davis.e.ford@gmail.com>
2019-08-26 22:40:26 -0400

    Updates

--- src/components/user/profile.tsx
+++ src/components/user/profile.tsx
@@ -1,10 +1,10 @@
-// src/components/Profile.js
-
 import React from 'react'
 import { useAuth0 } from 'react-auth0-wrapper'
+import { IUser } from 'types/user'
+import { NavBar } from 'components/page/navbar'
 
 export const Profile = () => {
-  const { loading, user } = useAuth0()
+  const { loading, user }: { loading: boolean; user: IUser } = useAuth0()
 
   if (loading || !user) {
     // TODO make this more fancy
@@ -12,12 +12,15 @@ export const Profile = () => {
   }
 
   return (
-    <>
-      <img src={user.picture} alt="Profile" />
+    <div className="ThemeDarkBg">
+      <NavBar />
+      <>
+        <img src={user.picture} alt="Profile" />
 
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
-      <code>{JSON.stringify(user, null, 2)}</code>
-    </>
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <code>{JSON.stringify(user, null, 2)}</code>
+      </>
+    </div>
   )
 }
---
+++ src/types/user.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+  email: string
+  email_verified: string
+  name: string
+  nickname: string
+  picture: string
+  sub: string
+  updated_at: string
+}
