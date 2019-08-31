a47c6b38fd34f5c9b6670af85228c111a5a6936a
Davis Ford <davis.e.ford@gmail.com>
2019-08-31 14:00:25 -0400

    Update navbar.tsx

--- src/components/page/navbar.tsx
+++ src/components/page/navbar.tsx
@@ -15,45 +15,47 @@ const NavBarComponent: React.FC<INavBarProps> = props => {
   const { pathname } = window.location
 
   const styles = {
-    btn: `btn btn-block btn-outline-light btn-sm`,
-    btnCol: `col-4`,
-    flex: `d-flex justify-content-between align-items-center`,
+    btn: `btn btn btn-outline-light btn-sm`,
+    btnCol: `col`,
+    flex: `d-flex w-100 align-items-center`,
     header: `ThemeDarkBg pt-2 d-print-none`,
     link: `font-weight-bold text-light mx-3`,
-    linkCol: `col ml-3`,
+    linkCol: `col`,
+    row: `row`,
   }
   const btnText = !isAuthenticated ? `Log in` : `Log out`
   const handleClick = !isAuthenticated ? loginWithRedirect : logout
 
   return (
     <header className={styles.header}>
-      <div className={styles.flex}>
-        <div className="col"></div>
-        <div className={styles.linkCol}>
-          {isAuthenticated && (
-            <>
-              {pathname !== '/' && (
-                <Link to="/" className={styles.link}>
-                  Home
-                </Link>
-              )}
-              {pathname !== '/profile' && (
-                <Link to="/profile" className={styles.link}>
-                  Profile
-                </Link>
-              )}
-              {!isSubscribed && pathname !== '/subscribe' && (
-                <Link to="/subscribe" className={styles.link}>
-                  Subscribe
-                </Link>
-              )}
-            </>
-          )}
-        </div>
-        <div className={styles.btnCol}>
-          <button className={styles.btn} onClick={() => handleClick({})}>
-            {btnText}
-          </button>
+      <div className={styles.row}>
+        <div className={styles.flex}>
+          <div className={styles.linkCol}>
+            {isAuthenticated && (
+              <>
+                {pathname !== '/' && (
+                  <Link to="/" className={styles.link}>
+                    Home
+                  </Link>
+                )}
+                {pathname !== '/profile' && (
+                  <Link to="/profile" className={styles.link}>
+                    Profile
+                  </Link>
+                )}
+                {!isSubscribed && pathname !== '/subscribe' && (
+                  <Link to="/subscribe" className={styles.link}>
+                    Subscribe
+                  </Link>
+                )}
+              </>
+            )}
+          </div>
+          <div className={styles.btnCol}>
+            <button className={styles.btn} onClick={() => handleClick({})}>
+              {btnText}
+            </button>
+          </div>
         </div>
       </div>
     </header>
