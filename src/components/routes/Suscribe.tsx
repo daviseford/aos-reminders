0fada80b4fec1b3291542e03cffd512c6416014e
Davis Ford <davis.e.ford@gmail.com>
2019-08-31 12:54:49 -0400

    Update Suscribe.tsx

--- src/components/routes/Suscribe.tsx
+++ src/components/routes/Suscribe.tsx
@@ -25,6 +25,35 @@ const SubscribeComponent = () => {
       <div className="ThemeDarkBg py-2">
         <NavBar />
       </div>
+
+      <div className="py-5 text-center col-8 mx-auto">
+        <img
+          className="d-block mx-auto mb-4 img-fluid"
+          src="/img/logo_noURL.png"
+          width="100px"
+          alt="Subscribe to support AoS Reminders"
+        />
+        <h2>Support AoS Reminders</h2>
+        <p className="lead">
+          It takes a lot of time, effort, and money to keep this project going. While the core product will{' '}
+          <i>always</i> be free, I do offer this subscription service to those who wish to support AoS
+          Reminders.
+        </p>
+        <p className="lead">
+          Currently, purchasing a subscription will allow you to save and load your army rosters from anywhere
+          in the world. In the future, we aim to offer:
+        </p>
+        <p className="lead">
+          Importing army lists from Warscroll Builder
+          <br />
+          Importing army lists from Battlescribe
+          <br />
+          Adding custom reminders
+          <br />
+          And much more...
+        </p>
+      </div>
+
       <div>
         <PricingPlans />
       </div>
