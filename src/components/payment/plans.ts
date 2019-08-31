e17921b593cf9264c993400b29de53af728aeb9c
Davis Ford <davis.e.ford@gmail.com>
2019-08-31 11:58:49 -0400

    Create plans.ts

---
+++ src/components/payment/plans.ts
@@ -0,0 +1,30 @@
+export const SupportPlans = {
+  '1Month': {
+    dev: 'plan_Fj0pS8B1S5BCbT',
+    prod: '',
+    cost: '4.79',
+    interval: 'month',
+    quantity: 1,
+  },
+  '3Month': {
+    dev: 'plan_Fj0yqd54eh9j5M',
+    prod: '',
+    cost: '11.97',
+    interval: 'month',
+    quantity: 3,
+  },
+  '6Month': {
+    dev: 'plan_Fj0zcqykYVZlDw',
+    prod: '',
+    cost: '11.97',
+    interval: 'month',
+    quantity: 6,
+  },
+  '1Year': {
+    dev: 'plan_Fj0vu1PrU562gd',
+    prod: '',
+    cost: '30.00',
+    interval: 'year',
+    quantity: 1,
+  },
+}
