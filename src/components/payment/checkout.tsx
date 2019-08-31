25af66b0e86a4f0a86eb87a0d198acae78b0ad19
Davis Ford <davis.e.ford@gmail.com>
2019-08-30 22:13:53 -0400

    env

--- src/components/payment/checkout.tsx
+++ src/components/payment/checkout.tsx
@@ -3,6 +3,8 @@ import { injectStripe, Elements } from 'react-stripe-elements'
 import { useAuth0 } from 'react-auth0-wrapper'
 import { IUser } from 'types/user'
 
+const isDev = process.env.REACT_APP_AOS_ENV === 'dev'
+
 const CheckoutComponent = props => {
   const { user }: { user: IUser } = useAuth0()
   console.log(user)
@@ -11,12 +13,14 @@ const CheckoutComponent = props => {
     // When the customer clicks on the button, redirect
     // them to Checkout.
 
-    // If we want to use the Auth0 customerId
-    // Research this: https://stripe.com/docs/payments/checkout/server#using-existing-customers
+    if (isDev) console.log('Redirecting to DEV Checkout')
+
+    const plan = isDev ? 'plan_Fi2fEmmQwMgSGR' : 'plan_Fi2OZlDqziP4bY'
+    const url = isDev ? 'localhost:3000' : 'aosreminders.com'
+
     props.stripe
       .redirectToCheckout({
-        // items: [{ plan: 'plan_Fi2OZlDqziP4bY', quantity: 1 }], // PROD
-        items: [{ plan: 'plan_Fi2fEmmQwMgSGR', quantity: 1 }], // TEST
+        items: [{ plan, quantity: 1 }],
 
         // Meta
         customerEmail: user.email, // Used to prefill checkout
@@ -27,11 +31,10 @@ const CheckoutComponent = props => {
         // a successful payment.
         // Instead use one of the strategies described in
         // https://stripe.com/docs/payments/checkout/fulfillment
-        successUrl: window.location.protocol + '//aosreminders.com/?session_id={CHECKOUT_SESSION_ID}',
-        cancelUrl: window.location.protocol + '//aosreminders.com/',
+        successUrl: `${window.location.protocol}//${url}/`,
+        cancelUrl: `${window.location.protocol}//${url}/`,
       })
       .then(function(result) {
-        console.log('what up')
         console.log(result)
         if (result.error) {
           // If `redirectToCheckout` fails due to a browser or network
