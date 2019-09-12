fce53f7f5c8b514c80c3a895fe639bd656006258
Davis Ford <davis.e.ford@gmail.com>
2019-09-11 20:28:53 -0400

    Update getWarscrollArmy.ts

--- src/utils/warscroll/getWarscrollArmy.ts
+++ src/utils/warscroll/getWarscrollArmy.ts
@@ -55,10 +55,12 @@ const getInitialWarscrollArmyPdf = (pdfText: string[]): IWarscrollArmy => {
   const cleanedText = pdfText
     .map(txt =>
       txt
+        .replace(/\\\(/g, '(') // Fix parentheses i.e. "\(value\)"
+        .replace(/\\\)/g, ')') // Fix parentheses i.e. "\(value\)"
         .replace(/^[0-9]{1,2}"$/g, '') // Remove '12"' entries
         .replace(/^[0-9]{1,2}"\*$/g, '') // Remove '10"*' entries
         .replace(/^[0-9]{1,2}D6"/g, '') // Remove '2D6"' entries
-        .replace(/\\\([0-9]+\\\)/g, '') // Remove point values e.g. "Slann Starmaster \(360\)"
+        .replace(/ \([0-9]+\)/g, '') // Remove point values e.g. "Slann Starmaster (360)"
         .replace(/[0-9]+ x /g, '') // Remove quantity from units e.g. "3 x Razordons"
         .trim()
     )
@@ -192,6 +194,8 @@ const getInitialWarscrollArmyTxt = (fileText: string): IWarscrollArmy => {
     .split('\n')
     .map(txt =>
       txt
+        .replace(/\\\(/g, '(') // Fix parentheses i.e. "\(value\)"
+        .replace(/\\\)/g, ')') // Fix parentheses i.e. "\(value\)"
         .replace(/^[0-9]{1,2}"$/g, '') // Remove '12"' entries
         .replace(/^[0-9]{1,2}"\*$/g, '') // Remove '10"*' entries
         .replace(/^[0-9]{1,2}D6"/g, '') // Remove '2D6"' entries
@@ -351,7 +355,8 @@ const warscrollPdfErrorChecker = (army: IWarscrollArmy): IWarscrollArmyWithError
     units: lookup('units'),
   }
 
-  console.log('Could not find: ', difference(unknownSelections, foundSelections))
+  const couldNotFind = difference(unknownSelections, foundSelections)
+  if (couldNotFind.length > 0) console.log('Could not find: ', couldNotFind)
 
   return {
     ...army,
@@ -397,9 +402,6 @@ const selectionLookup = (
 
   const errorFree = selections[type]
     .map((val: string) => {
-      // Fix parentheses
-      val = val.replace(/\\\(/g, '(').replace(/\\\)/g, ')')
-
       // Check for typos
       if (warscrollTypoMap[val]) val = warscrollTypoMap[val]
 
@@ -429,8 +431,6 @@ const selectionLookup = (
   const found = unknownSelections
     .map(val => {
       const orig = `${val}`
-      // Fix parentheses
-      val = val.replace(/\\\(/g, '(').replace(/\\\)/g, ')')
 
       // Check for typos
       if (warscrollTypoMap[val]) val = warscrollTypoMap[val]
