4cf92d59d8d844d581432d987dfd04cf6f4ba219
Davis Ford <davis.e.ford.alt+github@gmail.com>
2019-09-13 10:28:34 -0400

    Update getWarscrollArmy.ts

--- src/utils/warscroll/getWarscrollArmy.ts
+++ src/utils/warscroll/getWarscrollArmy.ts
@@ -53,8 +53,8 @@ const unitIndicatorsTxt = [
 ]
 const unitIndicatorsPdf = unitIndicatorsTxt.map(x => x.toUpperCase())
 
-const getInitialWarscrollArmyPdf = (pdfText: string[]): IWarscrollArmy => {
-  const cleanedText = pdfText
+const cleanText = (pdfText: string[]) => {
+  return pdfText
     .map(txt =>
       txt
         .replace(/\\\(/g, '(') // Fix parentheses i.e. "\(value\)"
@@ -73,6 +73,10 @@ const getInitialWarscrollArmyPdf = (pdfText: string[]): IWarscrollArmy => {
         txt !== 'Warscroll Builder on www.warhammer-community.com' &&
         txt !== '* See Warscroll'
     )
+}
+
+const getInitialWarscrollArmyPdf = (pdfText: string[]): IWarscrollArmy => {
+  const cleanedText = cleanText(pdfText)
 
   let hasAllies = false
   let unknownSelections: string[] = []
@@ -198,26 +202,7 @@ const getInitialWarscrollArmyPdf = (pdfText: string[]): IWarscrollArmy => {
 }
 
 const getInitialWarscrollArmyTxt = (fileText: string): IWarscrollArmy => {
-  const cleanedText = fileText
-    .split('\n')
-    .map(txt =>
-      txt
-        .replace(/\\\(/g, '(') // Fix parentheses i.e. "\(value\)"
-        .replace(/\\\)/g, ')') // Fix parentheses i.e. "\(value\)"
-        .replace(/^[0-9]{1,2}"$/g, '') // Remove '12"' entries
-        .replace(/^[0-9]{1,2}"\*$/g, '') // Remove '10"*' entries
-        .replace(/^[0-9]{1,2}D6"/g, '') // Remove '2D6"' entries
-        .replace(/ \([0-9]+\)/g, '') // Remove point values e.g. "Slann Starmaster (360)"
-        .replace(/[0-9]+ x /g, '') // Remove quantity from units e.g. "3 x Razordons"
-        .trim()
-    )
-    .filter(
-      txt =>
-        !!txt &&
-        txt.length > 2 &&
-        txt !== 'Warscroll Builder on www.warhammer-community.com' &&
-        txt !== '* See Warscroll'
-    )
+  const cleanedText = cleanText(fileText.split('\n'))
 
   let hasAllies = false
   let unknownSelections: string[] = []
