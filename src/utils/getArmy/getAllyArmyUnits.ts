c433c53db2f7284183a62622adba4c3c4ebdfb27
Davis Ford <davis.e.ford.alt+github@gmail.com>
2019-09-13 11:17:31 -0400

    Update getAllyArmyUnits.ts

--- src/utils/getArmy/getAllyArmyUnits.ts
+++ src/utils/getArmy/getAllyArmyUnits.ts
@@ -1,8 +1,15 @@
-import { IArmy } from 'types/army'
-import { isValidFactionName } from 'utils/armyUtils'
-import { ArmyList } from 'meta/army_list'
-import { MERCENARY_COMPANIES, TSupportedFaction } from 'meta/factions'
 import { uniq, without } from 'lodash'
+import { isValidFactionName } from 'utils/armyUtils'
+import {
+  MERCENARY_COMPANIES,
+  TSupportedFaction,
+  CHAOS_GRAND_ALLIANCE,
+  ORDER_GRAND_ALLIANCE,
+  DESTRUCTION_GRAND_ALLIANCE,
+  DEATH_GRAND_ALLIANCE,
+} from 'meta/factions'
+import { IArmy } from 'types/army'
+import { ArmyList } from 'meta/army_list'
 
 type TAllyArmies = { [key in TSupportedFaction]: IArmy['Units'] }
 type TGetAllyArmies = (factionName: string) => TAllyArmies | null
@@ -17,7 +24,13 @@ export const getAllyArmyUnits: TGetAllyArmies = factionName => {
       Object.keys(ArmyList)
         .filter(x => ArmyList[x].GrandAlliance === GrandAlliance)
         .concat(MERCENARY_COMPANIES),
-      factionName
+      ...[
+        factionName,
+        CHAOS_GRAND_ALLIANCE,
+        DEATH_GRAND_ALLIANCE,
+        DESTRUCTION_GRAND_ALLIANCE,
+        ORDER_GRAND_ALLIANCE,
+      ]
     )
   )
 
