a8bf55101630776fdb9e9f18ec0d6673e9d7a9b8
Davis Ford <davis.e.ford.alt+github@gmail.com>
2019-09-15 15:46:31 -0400

    Update azyrPdf.ts

--- src/components/input/azyr/azyrPdf.ts
+++ src/components/input/azyr/azyrPdf.ts
@@ -2,6 +2,7 @@ import pdfjsLib from 'pdfjs-dist'
 import { uniq, without } from 'lodash'
 
 const sep = ', '
+const commaAlt = `&&`
 
 export const getAzyrPdfText = async typedarray => {
   try {
@@ -30,11 +31,13 @@ export const getAzyrPdfText = async typedarray => {
 
     const joinedPages = uniq(cleanedPages.join(sep).split(sep))
 
-    const splitText = joinedPages.filter(x => {
-      if (prefixTypes.some(pre => x.startsWith(`${pre}:`))) return true
-      console.log('Missing a prefix: ' + x)
-      return !joinedPages.some(s => s.includes(x) && s !== x)
-    })
+    const splitText = joinedPages
+      .filter(x => {
+        if (prefixTypes.some(pre => x.startsWith(`${pre}:`))) return true
+        console.log('Missing a prefix: ' + x)
+        return !joinedPages.some(s => s.includes(x) && s !== x)
+      })
+      .map(x => x.replace(/&&/g, ','))
 
     console.table(splitText)
 
@@ -48,75 +51,84 @@ export const getAzyrPdfText = async typedarray => {
 // Battleline Behemoth Rearguard Leader Behemoth 260pts Auric Runesmiter Gener al Role: Leader , Behemoth Quantity: 1 Command T rait: Warrior Indominate Artefact: Tyrant Sla yer Prayer: Prayer of Ash Mount T rait: Fire-claw Adult 160pts Vulkite Berzerkers Role: Battleline Quantity: 10 Auric Runesmiter See the "Leader " occurr ence of this unit 240pts Auric Runeson Role: Leader , Behemoth Quantity: 1
 // Total: 1000/1000pts 0pts/0pts Allies Army deemed valid by Azyr Roster Builder Other Magmic Inv ocations Auric Runeson See the "Leader " occurr ence of this unit 100pts Doomseeker Role: Other Quantity: 1 40pts Runic Fyrewall Role: Magmic Inv ocation
 const cleanAzyrText = (text: string) => {
-  return (
-    text
-      .replace(/([A-Z]) ([a-z])/g, `$1$2`)
-      .replace(/Gener al/g, '')
-      .replace(/T ype/g, 'Type')
-      .replace(/Pur chased/g, 'Purchased')
-      .replace(/Extra Command [\w]+ Purchased \(.+\)/g, '')
-      .replace(/Mercenar y Company/g, 'Mercenary Company')
-      .replace(/.+Play Type: {2}.+ {2}\| {2}/g, '') // Removes "[army name] Play Type:  Open  |  Grand Alliance:  Order  |  "
-      .replace(/(Allegiance: {2}.+) Leader Battleline/g, `$1, `)
-      .replace(
-        /Realm of Battle:.+(AQSHY|CHAMON|GHUR|GHYRAN|HYSH|SHYISH|STYGXX|ULGU), [\w- ]+,/g,
-        ', REALMSCAPE: $1, '
-      )
-      .replace(/(Allegiance: [\w- ]+) ([\w]+:)/g, '$1, $2')
-      .replace(/(Mercenary Company: ([\w- ]+)),/g, ', MERCENARY COMPANY: $2, ')
-      .replace(/Quantity: {2}[0-9]{1,2}/g, '') // Removes "Quantity:  1"
-      .replace(/ See the .+ of this unit/g, sep)
-      .replace(/Army deemed .+ by Azyr Roster Builder/g, '')
-      .replace(/Total: [0-9]{1,4}[/][0-9]{1,4}pts [0-9]{1,4}pts[/][0-9]{1,4}pts Allies/g, '')
-      .replace(/ {2,4}/g, ' ')
-      .replace(
-        /(Artefact|Spell|Weapon|Command Trait|Mount Trait|Upgrade): ([\w- ]+) (Artefact|Spell|Weapon|Command Trait|Mount Trait|Upgrade| {1,3})/g,
-        '$1: $2, $3'
-      )
-      .replace(/[0-9]{1,4}pts/g, '')
-      .replace(/Total: /g, '')
-      .replace(/Allegiance: /g, 'FACTION: ')
-      .split(',')
-      .map(x => x.trim())
-      .filter(x => !!x)
-      .join(sep)
-      // Have to run this twice, really :(
-      .replace(
-        /(Artefact|Spell|Weapon|Command Trait|Mount Trait|Upgrade): ([\w- ]+) (Artefact|Spell|Weapon|Command Trait|Mount Trait|Upgrade| {1,3})/g,
-        '$1: $2, $3'
-      )
-      .replace(
-        /(,| {2})([\w- ]+) Role:[ ]+(Leader|Battleline|Artillery|Behemoth|Battalion|Endless Spell|Other)/g,
-        ', $3: $2, '
-      )
-      .replace(/ {2,4}/g, ' ')
-      .replace(
-        /(,| {2})?([\w- ]+) Role:[ ]+(Leader|Battleline|Artillery|Behemoth|Battalion|Endless Spell|Other)/g,
-        ', $3: $2, '
-      )
-      .replace(/(Leader|Battleline|Artillery|Behemoth|Other):/g, 'UNIT:')
-      .replace(/Battalion:/g, 'BATTALION:')
-      .replace(/Endless Spell:/g, 'ENDLESS SPELL:')
-      .replace(/Command Trait:/g, 'COMMAND TRAIT:')
-      .replace(/Mount Trait:/g, 'MOUNT TRAIT:')
-      .replace(/Spell:/g, 'SPELL:')
-      .replace(/Weapon:/g, 'WEAPON:')
-      .replace(/Artefact:/g, 'ARTIFACT:')
-      .replace(/Upgrade:/g, 'UPGRADE:')
-      .replace(/(UNIT:|,) ([\w- ]+) Ally/g, 'ALLY: $2')
-      .replace(/(Artillery|Battalions|Endless Spells)/g, '')
-      .split(',')
-      .map(x => {
-        x = x.trim()
-        x = x.replace(/^(Behemoth|Other) /g, '')
-        const allegianceMatch = allegianceTypes.find(a => x.startsWith(`${a}:`))
-        if (allegianceMatch) x = x.replace(allegianceMatch, 'ALLEGIANCE')
-        x = x.trim()
-        return x
-      })
-      .filter(x => !!x && x !== 'Behemoth' && x !== 'Other')
-      .join(sep)
-  )
+  const firstRun = text
+    .replace(/([A-Z]) ([a-z])/g, `$1$2`)
+    .replace(/Gener al/g, '')
+    .replace(/T ype/g, 'Type')
+    .replace(/Pur chased/g, 'Purchased')
+    .replace(/Extra Command [\w]+ Purchased \(.+\)/g, '')
+    .replace(/Mercenar y Company/g, 'Mercenary Company')
+    .replace(/.+Play Type: {2}.+ {2}\| {2}/g, '') // Removes "[army name] Play Type:  Open  |  Grand Alliance:  Order  |  "
+    .replace(/(Allegiance: {2}.+) Leader Battleline/g, `$1, `)
+    .replace(
+      /Realm of Battle:.+(AQSHY|CHAMON|GHUR|GHYRAN|HYSH|SHYISH|STYGXX|ULGU), [\w- ]+,/g,
+      ', REALMSCAPE: $1, '
+    )
+    .replace(/(Allegiance: [\w- ]+) ([\w]+:)/g, '$1, $2')
+    .replace(/(Mercenary Company: ([\w- ]+)),/g, ', MERCENARY COMPANY: $2, ')
+    .replace(/Quantity: {2}[0-9]{1,2}/g, '') // Removes "Quantity:  1"
+    .replace(/ See the .+ of this unit/g, sep)
+    .replace(/Army deemed .+ by Azyr Roster Builder/g, '')
+    .replace(/Total: [0-9]{1,4}[/][0-9]{1,4}pts [0-9]{1,4}pts[/][0-9]{1,4}pts Allies/g, '')
+    .replace(/ {2,4}/g, ' ')
+    .replace(
+      /(Artefact|Spell|Weapon|Command Trait|Mount Trait|Upgrade): ([\w- ]+) (Artefact|Spell|Weapon|Command Trait|Mount Trait|Upgrade| {1,3})/g,
+      '$1: $2, $3'
+    )
+    .replace(/[0-9]{1,4}pts/g, '')
+    .replace(/Total: /g, '')
+    .replace(/Allegiance: /g, 'FACTION: ')
+    .split(',')
+    .map(x => x.trim())
+    .filter(x => !!x)
+    .join(sep)
+
+  console.log('f', firstRun)
+
+  const secondRun = firstRun
+    // Have to run this twice, really :(
+    .replace(
+      /(Artefact|Spell|Weapon|Command Trait|Mount Trait|Upgrade): ([\w- ]+) (Artefact|Spell|Weapon|Command Trait|Mount Trait|Upgrade| {1,3})/g,
+      '$1: $2, $3'
+    )
+    // These next two lines handle Nagash, Supreme Lord of the Undead
+    .replace(/  ([\w]+(,| {2})[\w- ]+) Role:[ ]+(Leader)/g, `${commaAlt} $3: $1 ${commaAlt}`)
+    .replace(/&& (.+)(, )(.+) &&/g, `, $1${commaAlt} $3, `)
+    // Now handle normal units
+    .replace(
+      /(,| {2})([\w- ]+) Role:[ ]+(Leader|Battleline|Artillery|Behemoth|Battalion|Endless Spell|Other)/g,
+      ', $3: $2, '
+    )
+    .replace(/ {2,4}/g, ' ')
+    .replace(
+      /(,| {2})?([\w- ]+) Role:[ ]+(Leader|Battleline|Artillery|Behemoth|Battalion|Endless Spell|Other)/g,
+      ', $3: $2, '
+    )
+    .replace(/(Leader|Battleline|Artillery|Behemoth|Other):/g, 'UNIT:')
+    .replace(/Battalion:/g, 'BATTALION:')
+    .replace(/Endless Spell:/g, 'ENDLESS SPELL:')
+    .replace(/Command Trait:/g, 'COMMAND TRAIT:')
+    .replace(/Mount Trait:/g, 'MOUNT TRAIT:')
+    .replace(/Spell:/g, 'SPELL:')
+    .replace(/Weapon:/g, 'WEAPON:')
+    .replace(/Artefact:/g, 'ARTIFACT:')
+    .replace(/Upgrade:/g, 'UPGRADE:')
+    .replace(/(UNIT:|,) ([\w- ]+) Ally/g, 'ALLY: $2')
+    .replace(/(Artillery|Battalions|Endless Spells)/g, '')
+    .split(',')
+    .map(x => {
+      x = x.trim()
+      x = x.replace(/Behemoth /g, '')
+      x = x.replace(/^Other /g, '')
+      const allegianceMatch = allegianceTypes.find(a => x.startsWith(`${a}:`))
+      if (allegianceMatch) x = x.replace(allegianceMatch, 'ALLEGIANCE')
+      x = x.trim()
+      return x
+    })
+    .filter(x => !!x && x !== 'Behemoth' && x !== 'Other')
+    .join(sep)
+
+  return secondRun
 }
 
 const prefixTypes = [
