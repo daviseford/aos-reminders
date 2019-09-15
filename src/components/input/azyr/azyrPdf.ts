599599728843083387474f5b1c370e2400cca97e
Davis Ford <davis.e.ford.alt+github@gmail.com>
2019-09-15 10:46:37 -0400

    Update azyrPdf.ts

--- src/components/input/azyr/azyrPdf.ts
+++ src/components/input/azyr/azyrPdf.ts
@@ -1,4 +1,7 @@
 import pdfjsLib from 'pdfjs-dist'
+import { uniq } from 'lodash'
+
+const sep = ', '
 
 export const getAzyrPdfText = async typedarray => {
   try {
@@ -22,7 +25,7 @@ export const getAzyrPdfText = async typedarray => {
     )
 
     const cleanedPages = pages.map(cleanAzyrText)
-    const text = cleanedPages.join(', ')
+    const text = uniq(cleanedPages.join(sep).split(sep)).join(sep)
     console.log(text)
     return text
   } catch (err) {}
@@ -32,27 +35,24 @@ export const getAzyrPdfText = async typedarray => {
 // Battleline Behemoth Rearguard Leader Behemoth 260pts Auric Runesmiter Gener al Role: Leader , Behemoth Quantity: 1 Command T rait: Warrior Indominate Artefact: Tyrant Sla yer Prayer: Prayer of Ash Mount T rait: Fire-claw Adult 160pts Vulkite Berzerkers Role: Battleline Quantity: 10 Auric Runesmiter See the "Leader " occurr ence of this unit 240pts Auric Runeson Role: Leader , Behemoth Quantity: 1
 // Total: 1000/1000pts 0pts/0pts Allies Army deemed valid by Azyr Roster Builder Other Magmic Inv ocations Auric Runeson See the "Leader " occurr ence of this unit 100pts Doomseeker Role: Other Quantity: 1 40pts Runic Fyrewall Role: Magmic Inv ocation
 const cleanAzyrText = (text: string) => {
-  return (
-    text
-      .replace(/([A-Z]) ([a-z])/g, `$1$2`)
-      // .replace(/ {2,5}/g, ' ')
-      .replace(/Gener al/g, '')
-      .replace(/T ype/g, 'Type')
-      .replace(/.+Play Type:  .+  \|  /g, '') // Removes "[army name] Play Type:  Open  |  Grand Alliance:  Order  |  "
-      .replace(/(Allegiance:  .+) Leader Battleline/g, `$1, `)
-      .replace(/Quantity:  [0-9]{1,2}/g, '') // Removes "Quantity:  1"
-      .replace(/Artillery Behemoth Battalions Endless Spells/g, '')
-      .replace(/Role:  (Leader|Battleline|Artillery|Behemoth|Battalion|Endless Spell)/g, ',')
-      .replace(/ Behemoth /g, '')
-      .replace(/ See the .+ of this unit/g, ',')
-      .replace(/Army deemed .+ by Azyr Roster Builder/g, '')
-      .replace(/Total: [0-9]{1,4}[/][0-9]{1,4}pts [0-9]{1,4}pts[/][0-9]{1,4}pts Allies/g, '')
-      .replace(/[0-9]{1,4}pts/g, '')
-      .replace(/Total: /g, '')
-      .replace(/Allegiance:  /g, 'Allegiance: ')
-      .split(',')
-      .map(x => x.trim())
-      .filter(x => !!x)
-      .join(', ')
-  )
+  return text
+    .replace(/([A-Z]) ([a-z])/g, `$1$2`)
+    .replace(/Gener al/g, '')
+    .replace(/T ype/g, 'Type')
+    .replace(/.+Play Type:  .+  \|  /g, '') // Removes "[army name] Play Type:  Open  |  Grand Alliance:  Order  |  "
+    .replace(/(Allegiance:  .+) Leader Battleline/g, `$1, `)
+    .replace(/Quantity:  [0-9]{1,2}/g, '') // Removes "Quantity:  1"
+    .replace(/Role:  (Leader|Battleline|Artillery|Behemoth|Battalion|Endless Spell)/g, ',')
+    .replace(/(Artillery|Behemoth|Battalions|Endless Spells)/g, '')
+    .replace(/Behemoth /g, '')
+    .replace(/ See the .+ of this unit/g, ',')
+    .replace(/Army deemed .+ by Azyr Roster Builder/g, '')
+    .replace(/Total: [0-9]{1,4}[/][0-9]{1,4}pts [0-9]{1,4}pts[/][0-9]{1,4}pts Allies/g, '')
+    .replace(/[0-9]{1,4}pts/g, '')
+    .replace(/Total: /g, '')
+    .replace(/Allegiance:  /g, 'Allegiance: ')
+    .split(',')
+    .map(x => x.trim())
+    .filter(x => !!x)
+    .join(sep)
 }
