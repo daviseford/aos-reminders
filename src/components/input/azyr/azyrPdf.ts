3ea92f798823bc12fce35bb37bba4be9935a1ae2
Davis Ford <davis.e.ford.alt+github@gmail.com>
2019-09-15 12:08:31 -0400

    Update azyrPdf.ts

--- src/components/input/azyr/azyrPdf.ts
+++ src/components/input/azyr/azyrPdf.ts
@@ -25,51 +25,74 @@ export const getAzyrPdfText = async typedarray => {
     )
 
     const cleanedPages = pages.map(cleanAzyrText)
-    const text = uniq(cleanedPages.join(sep).split(sep)).join(sep)
-    console.log(text)
-    return text
-  } catch (err) {}
+    console.log(cleanedPages)
+    const splitText = uniq(cleanedPages.join(sep).split(sep))
+    console.log(splitText)
+    return splitText
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 // Fyreslayers Play Type: Matched | Game T ype: Meeting Engagement | Grand Alliance: Order | Allegiance: Fyreslayers Lodge: Hermdar Realm of Battle: AQSHY, The Realm of FIRE Spearhead Leader Other Main Body Leader 200pts Fjul-Grimnir Role: Leader Quantity: 1 200pts The Chosen Axes Role: Other Quantity: 3
 // Battleline Behemoth Rearguard Leader Behemoth 260pts Auric Runesmiter Gener al Role: Leader , Behemoth Quantity: 1 Command T rait: Warrior Indominate Artefact: Tyrant Sla yer Prayer: Prayer of Ash Mount T rait: Fire-claw Adult 160pts Vulkite Berzerkers Role: Battleline Quantity: 10 Auric Runesmiter See the "Leader " occurr ence of this unit 240pts Auric Runeson Role: Leader , Behemoth Quantity: 1
 // Total: 1000/1000pts 0pts/0pts Allies Army deemed valid by Azyr Roster Builder Other Magmic Inv ocations Auric Runeson See the "Leader " occurr ence of this unit 100pts Doomseeker Role: Other Quantity: 1 40pts Runic Fyrewall Role: Magmic Inv ocation
 const cleanAzyrText = (text: string) => {
-  return text
-    .replace(/([A-Z]) ([a-z])/g, `$1$2`)
-    .replace(/Gener al/g, '')
-    .replace(/T ype/g, 'Type')
-    .replace(/.+Play Type:  .+  \|  /g, '') // Removes "[army name] Play Type:  Open  |  Grand Alliance:  Order  |  "
-    .replace(/(Allegiance:  .+) Leader Battleline/g, `$1, `)
-    .replace(/Quantity:  [0-9]{1,2}/g, '') // Removes "Quantity:  1"
-    .replace(/Role:  (Leader|Battleline|Artillery|Behemoth|Battalion|Endless Spell|Other)/g, sep)
-    .replace(/(Artillery|Behemoth|Battalions|Endless Spells|Other)/g, '')
-    .replace(/Behemoth /g, '')
-    .replace(/ See the .+ of this unit/g, sep)
-    .replace(/Army deemed .+ by Azyr Roster Builder/g, '')
-    .replace(/Total: [0-9]{1,4}[/][0-9]{1,4}pts [0-9]{1,4}pts[/][0-9]{1,4}pts Allies/g, '')
-    .replace(
-      /Realm of Battle:.+(AQSHY|CHAMON|GHUR|GHYRAN|HYSH|SHYISH|STYGXX|ULGU), [\w ]+,/g,
-      ', REALMSCAPE: $1, '
-    )
-    .replace(/ {2,4}/g, ' ')
-    .replace(
-      /(Artefact|Spell|Weapon|Command Trait|Mount Trait): ([\w- ]+) (Artefact|Spell|Weapon|Command Trait|Mount Trait| {1,3})/g,
-      '$1: $2, $3'
-    )
-    .replace(/[0-9]{1,4}pts/g, '')
-    .replace(/Total: /g, '')
-    .replace(/Allegiance: /g, 'ALLEGIANCE: ')
-    .split(',')
-    .map(x => x.trim())
-    .filter(x => !!x)
-    .join(sep)
-    .replace(
-      /(Artefact|Spell|Weapon|Command Trait|Mount Trait): ([\w- ]+) (Artefact|Spell|Weapon|Command Trait|Mount Trait| {1,3})/g,
-      '$1: $2, $3'
-    )
-    .split(',')
-    .map(x => x.trim())
-    .filter(x => !!x)
-    .join(sep)
+  return (
+    text
+      .replace(/([A-Z]) ([a-z])/g, `$1$2`)
+      .replace(/Gener al/g, '')
+      .replace(/T ype/g, 'Type')
+      .replace(/.+Play Type:  .+  \|  /g, '') // Removes "[army name] Play Type:  Open  |  Grand Alliance:  Order  |  "
+      .replace(/(Allegiance:  .+) Leader Battleline/g, `$1, `)
+      .replace(/Quantity:  [0-9]{1,2}/g, '') // Removes "Quantity:  1"
+      .replace(/Behemoth /g, '')
+      .replace(/ See the .+ of this unit/g, sep)
+      .replace(/Army deemed .+ by Azyr Roster Builder/g, '')
+      .replace(/Total: [0-9]{1,4}[/][0-9]{1,4}pts [0-9]{1,4}pts[/][0-9]{1,4}pts Allies/g, '')
+      .replace(
+        /Realm of Battle:.+(AQSHY|CHAMON|GHUR|GHYRAN|HYSH|SHYISH|STYGXX|ULGU), [\w- ]+,/g,
+        ', REALMSCAPE: $1, '
+      )
+      .replace(/ {2,4}/g, ' ')
+      .replace(
+        /(Artefact|Spell|Weapon|Command Trait|Mount Trait): ([\w- ]+) (Artefact|Spell|Weapon|Command Trait|Mount Trait| {1,3})/g,
+        '$1: $2, $3'
+      )
+      .replace(/[0-9]{1,4}pts/g, '')
+      .replace(/Total: /g, '')
+      .replace(/Allegiance: /g, 'ALLEGIANCE: ')
+      .replace(/Other Endless Spells/g, '')
+      .split(',')
+      .map(x => x.trim())
+      .filter(x => !!x)
+      .join(sep)
+      // Have to run this twice, really :(
+      .replace(
+        /(Artefact|Spell|Weapon|Command Trait|Mount Trait): ([\w- ]+) (Artefact|Spell|Weapon|Command Trait|Mount Trait| {1,3})/g,
+        '$1: $2, $3'
+      )
+      .replace(
+        /(,| {2})([\w- ]+) Role:[ ]+(Leader|Battleline|Artillery|Behemoth|Battalion|Endless Spell|Other)/g,
+        ', $3: $2, '
+      )
+      .replace(/ {2,4}/g, ' ')
+      .replace(
+        /(,| {2})?([\w- ]+) Role:[ ]+(Leader|Battleline|Artillery|Behemoth|Battalion|Endless Spell|Other)/g,
+        ', $3: $2, '
+      )
+      .replace(/(Leader|Battleline|Artillery|Behemoth|Other):/g, 'UNIT:')
+      .replace(/Battalion:/g, 'BATTALION:')
+      .replace(/Endless Spell:/g, 'ENDLESS SPELL:')
+      .replace(/Command Trait:/g, 'COMMAND TRAIT:')
+      .replace(/Mount Trait:/g, 'MOUNT TRAIT:')
+      .replace(/Spell:/g, 'SPELL:')
+      .replace(/Weapon:/g, 'WEAPON:')
+      .replace(/Artefact:/g, 'ARTIFACT:')
+      .replace(/(UNIT:|,) ([\w- ]+) Ally/g, 'ALLY: $2')
+      .split(',')
+      .map(x => x.trim())
+      .filter(x => !!x)
+      .join(sep)
+  )
 }
