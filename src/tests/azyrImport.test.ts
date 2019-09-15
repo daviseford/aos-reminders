eb9c2aaf4a90b919eb6a36c76b96c78d01dd444e
Davis Ford <davis.e.ford.alt+github@gmail.com>
2019-09-15 15:10:35 -0400

    Blah

--- src/tests/azyrImport.test.ts
+++ src/tests/azyrImport.test.ts
@@ -1,9 +1,14 @@
-import { getAzyrArmy } from 'utils/azyr/getAzyrArmy'
 import { readFileSync } from 'fs'
+import { getAzyrPdfText } from 'components/input/azyr/azyrPdf'
 
+const pdfText = readFileSync(__dirname + '/fixtures/azyr/pdf/Cats.and.Judicators.2000.pdf').buffer
+
+// TODO: Get this to work :(
+// https://github.com/mozilla/pdf.js/issues/7612
 xdescribe('getAzyrArmy', () => {
-  it('does it', () => {
-    const pdfText = readFileSync(__dirname + '/fixtures/azyr/pdf/Cats.and.Judicators.2000.pdf', 'utf8')
-    getAzyrArmy()
-  })
+  it('does it', async () => {
+    const typedArray = new Uint8Array(pdfText as any)
+    const res = await getAzyrPdfText(typedArray)
+    console.log(res)
+  }, 300000)
 })
