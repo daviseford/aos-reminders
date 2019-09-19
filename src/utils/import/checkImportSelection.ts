99e4fbc4d3f77a57ec067203d176c8832aa95873
Davis Ford <davis.e.ford.alt+github@gmail.com>
2019-09-19 17:28:45 -0400

    Use validators  in checkImportSeelction

--- src/utils/import/checkImportSelection.ts
+++ src/utils/import/checkImportSelection.ts
@@ -1,9 +1,6 @@
-import { last } from 'lodash'
-import { TNameMap } from 'utils/import/options'
-import { replaceOf } from 'utils/import/replaceOf'
-import { stripPunctuation } from 'utils/textUtils'
-import { isPoorlySpacedMatch } from 'utils/import/isPoorlySpacedMatch'
 import { createWarning } from 'utils/import/warnings'
+import { TNameMap } from 'utils/import/options'
+import { Validators } from 'utils/import/validators'
 import { TImportError } from 'types/import'
 
 export const checkImportSelection = (
@@ -13,50 +10,47 @@ export const checkImportSelection = (
   logError: boolean = true,
   checkPoorSpacing: boolean,
   typoMap: TNameMap
-) => (val: string) => {
-  // Check for typos
-  if (typoMap[val]) val = typoMap[val]
+) => {
+  const validators = Validators(Names)
 
-  if (NameMap[val]) return val
+  return (val: string) => {
+    // Check for typos
+    if (typoMap[val]) val = typoMap[val]
 
-  // See if we have something like it...
-  const valUpper = val.toUpperCase()
-  const match = Names.find(x => x.toUpperCase().includes(valUpper))
-  if (match) return match
+    if (NameMap[val]) return val
 
-  // Maybe we have a trailing '... of Slaanesh'?
-  const valShortened = replaceOf(valUpper)
-  const match2 = Names.find(x => x.toUpperCase().includes(valShortened))
-  if (match2) return match2
+    // Going to check against uppercased values
+    const valUpper = val.toUpperCase()
 
-  // Maybe punctuation is in our way?
-  const valNoPunc = stripPunctuation(valUpper)
-  const match3 = Names.find(x => stripPunctuation(x.toUpperCase()).includes(valNoPunc))
-  if (match3) return match3
+    // Simple check if maybe some capitalization is off
+    const match = validators.matchUpper(valUpper)
+    if (match) return match
 
-  // Sometimes parentheses get in our way
-  const valNoParens = valUpper.replace(/\(.+\)/g, '').trim()
-  const match4 = Names.find(x =>
-    x
-      .toUpperCase()
-      .replace(/\(.+\)/g, '')
-      .includes(valNoParens)
-  )
-  if (match4) return match4
+    // Maybe we have a trailing '... of Slaanesh'?
+    const match2 = validators.matchNoOf(valUpper)
+    if (match2) return match2
 
-  // Maybe semicolons?
-  const valNoSemi = last(valUpper.split(':')) as string
-  const match5 = Names.find(x => x.toUpperCase().includes(valNoSemi))
-  if (match5) return match5
+    // Maybe punctuation is in our way?
+    const match3 = validators.matchNoPunc(valUpper)
+    if (match3) return match3
 
-  if (checkPoorSpacing) {
-    // Last chance - check for bad spacing
-    const match6 = Names.find(x => isPoorlySpacedMatch(val, x))
-    if (match6) return match6
+    // Sometimes parentheses get in our way
+    const match4 = validators.matchNoParens(valUpper)
+    if (match4) return match4
+
+    // Maybe semicolons?
+    const match5 = validators.matchNoSemicolons(valUpper)
+    if (match5) return match5
+
+    // Last chance - check for bad spacing (if enabled)
+    if (checkPoorSpacing) {
+      const match6 = validators.matchPoorSpacing(valUpper)
+      if (match6) return match6
+    }
+
+    if (logError) {
+      errors.push(createWarning(val))
+    }
+    return ''
   }
-
-  if (logError) {
-    errors.push(createWarning(val))
-  }
-  return ''
 }
