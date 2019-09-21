67110eb280da4271cb398f2d795a04f47d4697de
Davis Ford <davis.e.ford@gmail.com>
2019-09-21 10:29:40 -0400

    Update pdf.tsx

--- src/components/print/pdf.tsx
+++ src/components/print/pdf.tsx
@@ -2,8 +2,8 @@ import jsPDF from 'jspdf'
 import { TSupportedFaction } from 'meta/factions'
 import { IAllySelections, ISelections } from 'types/selections'
 import { TRealms } from 'types/realmscapes'
-import { IReminder } from 'types/data'
-import { connect } from 'react-redux'
+import { IReminder, TTurnAction } from 'types/data'
+import { titleCase, getActionTitle } from 'utils/textUtils'
 
 interface IPrintPdf {
   allyFactionNames: TSupportedFaction[]
@@ -28,7 +28,47 @@ export const savePdf = (data: IPrintPdf) => {
     reminders,
   } = data
 
-  const visibleReminders = Object.keys(reminders).reduce(
+  const visibleReminders = getVisibleReminders(reminders, hiddenReminders)
+
+  console.log(visibleReminders)
+
+  const doc = new jsPDF()
+  let [x, y] = [20, 20]
+
+  Object.keys(visibleReminders).forEach(key => {
+    const title = titleCase(key)
+    doc.text(title, x, y)
+    y = y + 10
+    visibleReminders[key].forEach(action => {
+      const actionTitle = getTitle(action)
+      doc.text(actionTitle, x, y)
+      y = y + 10
+      const desc = getActionDesc(action)
+      desc.forEach(d => {
+        doc.text(d, x, y)
+        y = y + 10
+      })
+    })
+  })
+
+  // doc.text('Hello world!', 20, 20);
+  // doc.text('This is client-side Javascript, pumping out a PDF.', 20, 30);
+  doc.save('two-by-four.pdf')
+}
+
+const getActionDesc = (action: TTurnAction) => {
+  return action.desc
+    .split('\n')
+    .map(t => t.trim())
+    .filter(t => !!t)
+}
+
+const getTitle = (action: TTurnAction) => {
+  return `${getActionTitle(action)} - ${action.name}${action.tag ? ` (${action.tag})` : ``}`
+}
+
+const getVisibleReminders = (reminders: IReminder, hiddenReminders: string[]): IReminder => {
+  return Object.keys(reminders).reduce(
     (a, key) => {
       const actions = reminders[key].filter(action => {
         const name = `${key}_${action.name}_`
@@ -39,11 +79,4 @@ export const savePdf = (data: IPrintPdf) => {
     },
     {} as IReminder
   )
-
-  console.log(visibleReminders)
-
-  // const doc = new jsPDF();
-  // doc.text('Hello world!', 20, 20);
-  // doc.text('This is client-side Javascript, pumping out a PDF.', 20, 30);
-  // doc.save('two-by-four.pdf')
 }
