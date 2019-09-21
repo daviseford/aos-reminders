390e547db783b96016d7faa78b5d28d83bbedf59
Davis Ford <davis.e.ford@gmail.com>
2019-09-21 10:03:26 -0400

    Update react-select

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -1,4 +1,5 @@
 import { startCase, camelCase } from 'lodash'
+import { TTurnAction } from 'types/data'
 
 export const titleCase = (val: string): string => startCase(camelCase(val))
 
@@ -13,4 +14,26 @@ export const stripPunctuation = (text: string): string => text.replace(/[.,/#!$%
 
 export const generateUUID = () => {
   return [...Array(10)].map(i => (~~(Math.random() * 36)).toString(36)).join('')
+}
+
+export const getActionTitle = ({
+  artifact,
+  command_ability,
+  command_trait,
+  condition,
+  endless_spell,
+  name,
+  scenery,
+  spell,
+  triumph,
+}: TTurnAction): string => {
+  const suffix = name === condition ? `` : `: ${condition}`
+  if (artifact) return `Artifact${suffix}`
+  if (command_ability) return `Command Ability${suffix}`
+  if (command_trait) return `Command Trait${suffix}`
+  if (endless_spell) return `Endless Spell${suffix}`
+  if (scenery) return `Scenery${suffix}`
+  if (spell) return `Spell${suffix}`
+  if (triumph) return `Triumph${suffix}`
+  return condition
 }
