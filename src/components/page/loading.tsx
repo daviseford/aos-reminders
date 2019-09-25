7d00a5d85eb23c321a6d5df0345c1f3f8592a869
Davis Ford <davis.e.ford.alt+github@gmail.com>
2019-09-25 16:46:14 -0400

    Adjust Loading screen and deps

--- src/components/page/loading.tsx
+++ src/components/page/loading.tsx
@@ -11,17 +11,19 @@ export const EmptyHeader = () => {
   )
 }
 
-export const Loading = () => {
-  return (
-    <div className="d-block">
-      <EmptyHeader />
-      <div className="row align-items-center justify-content-center mt-5 pt-5">
-        <div className="my-5 py-5"> </div>
-        <div className="col my-5 py-5 px-5 text-center">
-          <h4>AoS Reminders</h4>
-          <p className="lead">Loading...</p>
-        </div>
-      </div>
+export const Loading = () => (
+  <div className="container d-flex flex-column align-items-center justify-content-center LoadingContainer">
+    {/* <div className="col text-center">
+    <img
+          className="d-block mx-auto mb-3 img-fluid"
+          src="/img/logo_noURL.png"
+          width="100px"
+          alt="AoS Reminders"
+        />
+    </div> */}
+    <div className="col text-center">
+      <h3>AoS Reminders</h3>
+      <p className="lead text-muted">Loading...</p>
     </div>
-  )
-}
+  </div>
+)
