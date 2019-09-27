91edc60dd2b1f08f7143e11d82bf3e4f4fd8c740
Davis Ford <davis.e.ford.alt+github@gmail.com>
2019-09-27 16:57:56 -0400

    Add Suspense

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -4,10 +4,12 @@ import { useSubscription } from 'context/useSubscription'
 import ArmyBuilder from 'components/input/army_builder'
 import { Header } from 'components/page/homeHeader'
 import { Toolbar } from 'components/input/toolbar'
-import LoadedArmyHeader from 'components/input/savedArmies/loaded_army_header'
 
 const AlliedArmies = lazy(() => import(/* webpackChunkName: 'ally_armies' */ 'components/input/ally_armies'))
 const FooterComponent = lazy(() => import(/* webpackChunkName: 'footer' */ 'components/page/footer'))
+const LoadedArmyHeader = lazy(() =>
+  import(/* webpackChunkName: 'loaded_army_header' */ 'components/input/savedArmies/loaded_army_header')
+)
 const PrintArmy = lazy(() => import(/* webpackChunkName: 'printArmy' */ 'components/print/printArmy'))
 const PrintFooter = lazy(() => import(/* webpackChunkName: 'printFooter' */ 'components/print/printFooter'))
 const Reminders = lazy(() => import(/* webpackChunkName: 'reminders' */ 'components/info/reminders'))
@@ -27,7 +29,9 @@ const Home: React.FC = () => {
     <>
       <Header />
 
-      <LoadedArmyHeader />
+      <Suspense fallback={<></>}>
+        <LoadedArmyHeader />
+      </Suspense>
 
       <ArmyBuilder />
 
