fe305559b839107a55cc69e54158a6520a2dd5ce
Unknown <chris.thiel987@gmail.com>
2019-09-27 09:53:42 -0400

    Fixed Typos

--- src/army/kharadron_overlords/abilities.ts
+++ src/army/kharadron_overlords/abilities.ts
@@ -4,7 +4,7 @@ import { DURING_SETUP } from 'types/phases'
 const Abilities: TAbilities = [
   {
     name: `Stick to the Code`,
-    desc: `When you are not in a defined skyport, choose an artycle, amendment, and footnote. (Located in the traits drop down)`,
+    desc: `When you are not in a defined skyport, choose an ARTYCLE, AMENDMENT, and FOOTNOTE. (Located in the Traits drop down)`,
     when: [DURING_SETUP],
   },
 ]
