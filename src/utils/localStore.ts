fbf826059077350f3d7471e8c31cd64b3e26dd18
Davis Ford <davis.e.ford@gmail.com>
2019-10-04 12:51:15 -0400

    Update localStore.ts

--- src/utils/localStore.ts
+++ src/utils/localStore.ts
@@ -1,4 +1,5 @@
 import { TSupportedFaction } from 'meta/factions'
+import { isDev } from './env'
 
 const LOCAL_FAVORITE_KEY = 'favoriteFaction'
 
@@ -10,14 +11,14 @@ const LOCAL_FAVORITE_KEY = 'favoriteFaction'
 export const setLocalFavorite = (factionName: TSupportedFaction | null) => {
   if (factionName) {
     localStorage.setItem(LOCAL_FAVORITE_KEY, factionName)
-    console.log(`Set ${LOCAL_FAVORITE_KEY} in localStorage to ${factionName}`)
   } else {
     localStorage.removeItem(LOCAL_FAVORITE_KEY)
   }
+  if (isDev) console.log(`Set ${LOCAL_FAVORITE_KEY} in localStorage to ${factionName}`)
 }
 
 export const getLocalFavorite = () => {
   const localFavorite = localStorage.getItem(LOCAL_FAVORITE_KEY) as TSupportedFaction | null
-  console.log(`Got local favorite: ${localFavorite}`)
+  if (isDev) console.log(`Got local favorite: ${localFavorite}`)
   return localFavorite
 }
