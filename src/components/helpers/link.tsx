f015afd2c78f370750a3ecddca0da7773ce02c81
Davis Ford <davis.e.ford@gmail.com>
2019-10-08 22:29:17 -0400

    Refactor LinkButton

--- src/components/helpers/link.tsx
+++ src/components/helpers/link.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { IconType } from 'react-icons'
+import { logClick } from 'utils/analytics'
+import { btnContentWrapper } from 'theme/helperClasses'
+import { componentWithSize } from 'utils/mapSizesToProps'
 
 interface ILinkProps {
   href: string
@@ -11,3 +15,26 @@ export const LinkNewTab: React.FC<ILinkProps> = ({ href, children, ...props }) =
     {children}
   </a>
 )
+
+interface ILinkBtnProps {
+  isMobile: boolean
+  href: string
+  btnClass: string
+  Icon: IconType
+  text: string
+}
+
+const LinkBtnComponent: React.FC<ILinkBtnProps> = props => {
+  const { Icon, href, btnClass, isMobile, text } = props
+
+  return (
+    <LinkNewTab href={href} className={`${btnClass} mb-1`} onClick={e => logClick(`Contact-${text}`)}>
+      <div className={btnContentWrapper}>
+        <Icon className={isMobile ? `mx-2 my-1` : `mr-2`} />
+        {isMobile ? `` : ` ${text}`}
+      </div>
+    </LinkNewTab>
+  )
+}
+
+export const LinkButton = componentWithSize(LinkBtnComponent)
--- src/components/page/contact.tsx
+++ src/components/page/contact.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
-import { btnContentWrapper } from 'theme/helperClasses'
 import { FaGithub, FaEnvelopeOpenText, FaReddit, FaTwitter, FaDiscord } from 'react-icons/fa'
-import { IconType } from 'react-icons'
-import { componentWithSize } from 'utils/mapSizesToProps'
-import { logClick } from 'utils/analytics'
-import { LinkNewTab } from 'components/helpers/link'
 import { GITHUB_URL } from 'utils/env'
+import { LinkButton } from 'components/helpers/link'
 
 interface IContactProps {
   size?: 'normal' | 'small' | 'large'
@@ -18,39 +14,16 @@ export const ContactComponent: React.FC<IContactProps> = props => {
 
   return (
     <>
-      <Link href={GITHUB_URL} btnClass={btnClass} Icon={FaGithub} text={'Github'} />
-      <Link
+      <LinkButton href={GITHUB_URL} btnClass={btnClass} Icon={FaGithub} text={'Github'} />
+      <LinkButton
         href="mailto:aosreminders@gmail.com"
         btnClass={btnClass}
         Icon={FaEnvelopeOpenText}
         text={'Email'}
       />
-      <Link href="//reddit.com/r/AoSReminders/" btnClass={btnClass} Icon={FaReddit} text={'Reddit'} />
-      <Link href="//discord.gg/2nt9Fxp" btnClass={btnClass} Icon={FaDiscord} text={'Discord'} />
-      <Link href="//twitter.com/daviseford" btnClass={btnClass} Icon={FaTwitter} text={'Twitter'} />
+      <LinkButton href="//reddit.com/r/AoSReminders/" btnClass={btnClass} Icon={FaReddit} text={'Reddit'} />
+      <LinkButton href="//discord.gg/2nt9Fxp" btnClass={btnClass} Icon={FaDiscord} text={'Discord'} />
+      <LinkButton href="//twitter.com/daviseford" btnClass={btnClass} Icon={FaTwitter} text={'Twitter'} />
     </>
   )
 }
-
-interface ILinkProps {
-  isMobile: boolean
-  href: string
-  btnClass: string
-  Icon: IconType
-  text: string
-}
-
-const LinkComponent: React.FC<ILinkProps> = componentWithSize(props => {
-  const { Icon, href, btnClass, isMobile, text } = props
-
-  return (
-    <LinkNewTab href={href} className={`${btnClass} mb-1`} onClick={e => logClick(`Contact-${text}`)}>
-      <div className={btnContentWrapper}>
-        <Icon className={isMobile ? `mx-2 my-1` : `mr-2`} />
-        {isMobile ? `` : ` ${text}`}
-      </div>
-    </LinkNewTab>
-  )
-})
-
-const Link = componentWithSize(LinkComponent)
