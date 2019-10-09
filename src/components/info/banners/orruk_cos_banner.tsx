6096d6e96f7a37ec2f1efade94a1e9e577a82437
Davis Ford <davis.e.ford@gmail.com>
2019-10-09 15:39:39 -0400

    Update orruk_cos_banner.tsx

--- src/components/info/banners/orruk_cos_banner.tsx
+++ src/components/info/banners/orruk_cos_banner.tsx
@@ -4,10 +4,11 @@ import { LinkNewTab } from 'components/helpers/link'
 import { GITHUB_URL } from 'utils/env'
 
 const OrrukCoSBanner = () => (
-  <NotificationBanner name="Orruk_CoS_WaitingForBooks" persistClose={true} variant={'primary'}>
-    Looking for <LinkNewTab href={`${GITHUB_URL}/pull/535`}>Cities of Sigmar</LinkNewTab> or{' '}
-    <LinkNewTab href={`${GITHUB_URL}/pull/534`}>Orruk Warclans</LinkNewTab>? Don't fret, our goblins are hard
-    at work getting this information updated.
+  <NotificationBanner name="CoS_WaitingForBook" persistClose={true} variant={'primary'}>
+    <strong>Orruk Warclans are updated!</strong>
+    <br />
+    Looking for <LinkNewTab href={`${GITHUB_URL}/pull/535`}>Cities of Sigmar</LinkNewTab>? Don't fret, our
+    goblins are hard at work getting this information updated.
   </NotificationBanner>
 )
 
