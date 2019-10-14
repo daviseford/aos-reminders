379437dd569b18e50eb81dafe5b82b2536ec6032
Davis Ford <davis.e.ford@gmail.com>
2019-10-13 20:42:31 -0400

    Update preferenceApi.ts

--- src/api/preferenceApi.ts
+++ src/api/preferenceApi.ts
@@ -1,13 +1,20 @@
 import request from 'superagent'
 import { ISavedArmy } from 'types/savedArmy'
 import { isDev } from 'utils/env'
+import { TImportFileTypes, TImportParsers } from 'types/import'
 
 const devEndpoint = `https://bgj1fpqcj6.execute-api.us-east-1.amazonaws.com/dev`
 const prodEndpoint = `https://mzrv8apqrd.execute-api.us-east-1.amazonaws.com/prod`
 
-const endpoint = isDev ? devEndpoint : prodEndpoint
+const api = isDev ? devEndpoint : prodEndpoint
 
-interface ICreateItems {
+interface ICreateError {
+  fileTxt: string | string[]
+  fileType: TImportFileTypes
+  parser: TImportParsers
+}
+
+interface ICreateItem {
   userName: string
   [key: string]: any
 }
@@ -16,19 +23,20 @@ interface ICreateSavedArmy extends ISavedArmy {
   userName: string
 }
 
-interface IUpdateItems {
+interface IUpdateItem {
   [key: string]: any
 }
 
-const createItem = (data: ICreateItems) => request.post(`${endpoint}/items`).send(data)
-const createSavedArmy = (data: ICreateSavedArmy) => request.post(`${endpoint}/items`).send(data)
-const deleteItem = (id: string, userName: string) => request.delete(`${endpoint}/items/${id}/${userName}`)
-const getItem = (id: string) => request.get(`${endpoint}/items/${id}`)
-const getUserItems = (userName: string) => request.get(`${endpoint}/user/${userName}`)
-const updateItem = (id: string, data: IUpdateItems) =>
-  request.post(`${endpoint}/update/`).send({ ...data, id })
+const createError = (data: ICreateError) => request.post(`${api}/errors`).send(data)
+const createItem = (data: ICreateItem) => request.post(`${api}/items`).send(data)
+const createSavedArmy = (data: ICreateSavedArmy) => request.post(`${api}/items`).send(data)
+const deleteItem = (id: string, userName: string) => request.delete(`${api}/items/${id}/${userName}`)
+const getItem = (id: string) => request.get(`${api}/items/${id}`)
+const getUserItems = (userName: string) => request.get(`${api}/user/${userName}`)
+const updateItem = (id: string, data: IUpdateItem) => request.post(`${api}/update/`).send({ ...data, id })
 
 export const PreferenceApi = {
+  createError,
   createItem,
   createSavedArmy,
   deleteItem,
