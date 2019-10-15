7e50f57973daec209dc8361f49404820c8f34e61
Davis Ford <davis.e.ford@gmail.com>
2019-10-15 09:29:16 -0400

    Update app_banner.tsx

--- src/components/info/banners/app_banner.tsx
+++ src/components/info/banners/app_banner.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import { NotificationBanner } from 'components/info/banners/notification_banner'
-import { Link } from 'react-router-dom'
-import { useSubscription } from 'context/useSubscription'
 
 const AppBanner = () => {
-  const { isSubscribed } = useSubscription()
-  if (isSubscribed) return null
   return (
-    <NotificationBanner name="Subscribe_Nudge_01" persistClose={false} variant={'info'}>
-      <p>
-        We work hard to maintain our rules library and constantly add premium features for the community. You
-        can help support us by subscribing!
-      </p>
-
-      <Link to="/subscribe">
-        <span>Subscribe Today</span>
-      </Link>
+    <NotificationBanner name="App_Notification" persistClose={false} variant={'info'}>
+      Application Notification Goes Here
     </NotificationBanner>
   )
 }
