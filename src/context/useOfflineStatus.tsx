42425168c5108dd94701dd83f5e08995e51d0b8d
Davis Ford <daviseford@users.noreply.github.com>
2019-10-17 17:03:02 -0400

    Add fallback ping for offline status (#605)
    * Update useOfflineStatus.tsx
    
    * Update useOfflineStatus.tsx

--- src/context/useOfflineStatus.tsx
+++ src/context/useOfflineStatus.tsx
@@ -5,6 +5,15 @@ interface IOfflineStatusProvider {
   isOnline: boolean
 }
 
+const timeout = (ms: number, promise) => {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      reject(new Error('timeout'))
+    }, ms)
+    promise.then(resolve, reject)
+  })
+}
+
 const OfflineStatusContext = React.createContext<IOfflineStatusProvider | void>(undefined)
 
 const OfflineStatusProvider: React.FC = ({ children }) => {
@@ -14,6 +23,24 @@ const OfflineStatusProvider: React.FC = ({ children }) => {
     console.log('No internet connection found. App is running in offline mode.')
     setIsOffline(true)
   }
+
+  useEffect(() => {
+    const poll = async () => {
+      try {
+        await timeout(
+          10000,
+          fetch('https://google.com', {
+            method: 'GET',
+            mode: 'no-cors',
+            headers: { 'Access-Control-Allow-Origin': '*' },
+          })
+        )
+      } catch (error) {
+        if (!isOffline) setOffline()
+      }
+    }
+    poll()
+  })
 
   useEffect(() => {
     window.addEventListener('isOffline', setOffline)
