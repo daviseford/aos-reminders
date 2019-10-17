180fc95b448914f3329f030639f714e4a267b887
Davis Ford <davis.e.ford@gmail.com>
2019-10-17 18:44:53 -0400

    Add display event

--- src/components/info/banners/update_banner.tsx
+++ src/components/info/banners/update_banner.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { NotificationBanner } from 'components/info/banners/notification_banner'
 import { useAppStatus } from 'context/useAppStatus'
+import { logDisplay } from 'utils/analytics'
 
 const UpdateBanner = () => {
   const { hasNewContent } = useAppStatus()
+  const name = 'Content_Update_Notification'
 
   if (!hasNewContent) return <></>
 
+  logDisplay(name)
+
   return (
-    <NotificationBanner name="Content_Update_Notification" persistClose={false} variant={'warning'}>
+    <NotificationBanner name={name} persistClose={false} variant={'warning'}>
       New content is available and will be used when all tabs for this page are closed.
     </NotificationBanner>
   )
