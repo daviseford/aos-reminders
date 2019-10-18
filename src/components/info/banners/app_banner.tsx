e8847a4d39fc6ac64a06e4ca7b5e3d1337b3f90a
Davis Ford <davis.e.ford@gmail.com>
2019-10-18 12:44:54 -0400

    Update app_banner.tsx

--- src/components/info/banners/app_banner.tsx
+++ src/components/info/banners/app_banner.tsx
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { NotificationBanner } from 'components/info/banners/notification_banner'
 import { useSubscription } from 'context/useSubscription'
+import { logClick, logDisplay } from 'utils/analytics'
 import { ROUTES } from 'utils/env'
-import { Link } from 'react-router-dom'
-import { logClick } from 'utils/analytics'
 import { componentWithSize } from 'utils/mapSizesToProps'
 
 const AppBanner = componentWithSize(props => {
   const { isTinyMobile } = props
   const { isNotSubscribed } = useSubscription()
+  const name = 'Offline_Sale_Notification'
+
   const subscribeTxt = (
     <>
       To celebrate, subscriptions are <strong>50% off!</strong>
     </>
   )
 
+  useEffect(() => {
+    logDisplay(name)
+  }, [])
+
   return (
-    <NotificationBanner name="Offline_Sale_Notification" persistClose={true} variant={'info'}>
+    <NotificationBanner name={name} persistClose={true} variant={'info'}>
       You can now use this site <strong>offline!</strong>
       <br />
       {isNotSubscribed && (
-        <Link to={ROUTES.SUBSCRIBE} onClick={() => logClick('Offline_Sale_Subscribe')}>
+        <Link to={ROUTES.SUBSCRIBE} onClick={() => logClick(name)}>
           {isTinyMobile ? <small>{subscribeTxt}</small> : subscribeTxt}
         </Link>
       )}
