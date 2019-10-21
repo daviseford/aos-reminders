875746909471f8023b3a2f62ef2924f04a6a1449
Davis Ford <davis.e.ford@gmail.com>
2019-10-21 10:25:51 -0400

    Working it

--- src/utils/battlescribe/getBattlescribeArmy.ts
+++ src/utils/battlescribe/getBattlescribeArmy.ts
@@ -1,26 +1,41 @@
+//@ts-nocheck
 import parse5 from 'parse5'
 
 export const getBattleScribeArmy = (html_string: string) => {
   const document = parse5.parse(html_string)
 
+  const nonCircular = stripParentNode(document)
+
   // @ts-ignore
-  const body = document.childNodes[2]
-  console.log(document)
+  const body = nonCircular.childNodes[2]
+  console.log(nonCircular)
 }
 
-const stripParentNode = (document: parse5.Document) => {
-  const strip = (a: Object) => {
-    //@ts-ignore
-    delete a.parentNode
-    return a
+// Remove empty entries
+const cleanHtml = (a: { [key: string]: any }) => {}
+
+/**
+ * Helps us get the JSON string (removes circular references)
+ */
+const stripParentNode = (docObj: Object) => {
+  //@ts-ignore
+  delete docObj.parentNode
+  if (!docObj.childNodes) return docObj
+
+  if (docObj.childNodes.length > 0) {
+    docObj.childNodes = docObj.childNodes.map(stripParentNode).filter(x => {
+      if (x.value && (!x.childNodes || !x.childNodes.length)) {
+        const trimmedVal = x.value.trim()
+        if (trimmedVal === '') return false
+        if (trimmedVal === '\n') return false
+        if (trimmedVal === '\n\n') return false
+      }
+
+      return true
+    })
   }
 
-  return Object.keys(document).reduce((a, k) => {
-    const obj = strip(document[k])
-    if (obj.childNodes) {
-      a[k]
-    }
-  }, {})
+  return docObj
 }
 
 const getFaction = (document: parse5.Document) => {}
