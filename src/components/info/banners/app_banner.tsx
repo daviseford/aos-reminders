97d81d242d5736eede79c271c2286cba6df59583
Davis Ford <davis.e.ford@gmail.com>
2019-10-29 09:23:14 -0400

    Update sale, add notification banner about battlescribe

--- src/components/info/banners/app_banner.tsx
+++ src/components/info/banners/app_banner.tsx
@@ -1,21 +1,9 @@
 import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
 import { NotificationBanner } from 'components/info/banners/notification_banner'
-import { useSubscription } from 'context/useSubscription'
-import { logClick, logDisplay } from 'utils/analytics'
-import { ROUTES } from 'utils/env'
-import { componentWithSize } from 'utils/mapSizesToProps'
+import { logDisplay } from 'utils/analytics'
 
-const AppBanner = componentWithSize(props => {
-  const { isTinyMobile } = props
-  const { isNotSubscribed } = useSubscription()
-  const name = 'Offline_Sale_Notification'
-
-  const subscribeTxt = (
-    <>
-      To celebrate, subscriptions are <strong>50% off!</strong>
-    </>
-  )
+const AppBanner = () => {
+  const name = 'Battlescribe_Release_Notification'
 
   useEffect(() => {
     logDisplay(name)
@@ -23,15 +11,38 @@ const AppBanner = componentWithSize(props => {
 
   return (
     <NotificationBanner name={name} persistClose={true} variant={'info'}>
-      You can now use this site <strong>offline!</strong>
-      <br />
-      {isNotSubscribed && (
-        <Link to={ROUTES.SUBSCRIBE} onClick={() => logClick(name)}>
-          {isTinyMobile ? <small>{subscribeTxt}</small> : subscribeTxt}
-        </Link>
-      )}
+      Great news! You can now import Battlescribe HTML files!
     </NotificationBanner>
   )
-})
+}
 
 export default AppBanner
+
+// Sale Banner
+// const AppBanner = componentWithSize(props => {
+//   const { isTinyMobile } = props
+//   const { isNotSubscribed } = useSubscription()
+//   const name = 'Offline_Sale_Notification'
+
+//   const subscribeTxt = (
+//     <>
+//       To celebrate, subscriptions are <strong>50% off!</strong>
+//     </>
+//   )
+
+//   useEffect(() => {
+//     logDisplay(name)
+//   }, [])
+
+//   return (
+//     <NotificationBanner name={name} persistClose={true} variant={'info'}>
+//       You can now use this site <strong>offline!</strong>
+//       <br />
+//       {isNotSubscribed && (
+//         <Link to={ROUTES.SUBSCRIBE} onClick={() => logClick(name)}>
+//           {isTinyMobile ? <small>{subscribeTxt}</small> : subscribeTxt}
+//         </Link>
+//       )}
+//     </NotificationBanner>
+//   )
+// })
