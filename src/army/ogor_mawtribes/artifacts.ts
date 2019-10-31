17a97d2e5048c43e27f48c6f9b42d9c07cff93d5
Davis Ford <davis.e.ford@gmail.com>
2019-10-31 15:26:35 -0400

    Beginning to add traits and artifacts

--- src/army/ogor_mawtribes/artifacts.ts
+++ src/army/ogor_mawtribes/artifacts.ts
@@ -1,16 +1,285 @@
 import { TArtifacts } from 'types/army'
+import {
+  COMBAT_PHASE,
+  MOVEMENT_PHASE,
+  SHOOTING_PHASE,
+  DURING_GAME,
+  START_OF_HERO_PHASE,
+  HERO_PHASE,
+  CHARGE_PHASE,
+} from 'types/phases'
 
 const Artifacts: TArtifacts = [
-  // {
-  //   name: ``,
-  //   effects: [
-  //     {
-  //       name: ``,
-  //       desc: ``,
-  //       when: [],
-  //     },
-  //   ],
-  // },
+  {
+    name: `Headmasher`,
+    effects: [
+      {
+        name: `Headmasher`,
+        desc: `The bearer's Thundermace has a Damage characteristic of 4 instead of 3.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Grawl's Gut-plate`,
+    effects: [
+      {
+        name: `Grawl's Gut-plate`,
+        desc: `Add 4" to the bearer's Move characteristic while they are hungry instead of 2".`,
+        when: [MOVEMENT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Gruesome Trophy Rack`,
+    effects: [
+      {
+        name: `Gruesome Trophy Rack`,
+        desc: `Add 1 to hit rolls for attacks made by friendly GUTBUSTERS units wholly within 12" of the bearer that target a MONSTER or HERO.`,
+        when: [SHOOTING_PHASE, COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Flask of Stonehorn Blood`,
+    effects: [
+      {
+        name: `Flask of Stonehorn Blood`,
+        desc: `Once per battle, at the start of any phase, the bearer can use this artefact. If they do so, until the end of that turn, roll a dice each time you allocate a wound or mortal wound to the bearer. On a 4+, that wound or mortal wound is negated.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Sky-Titan Scatter Pistols`,
+    effects: [
+      {
+        name: `Sky-Titan Scatter Pistols`,
+        desc: `The bearer's Ogor Pistols have an Attacks characteristic of 6 instead of 2.`,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `The Fang of Ghur`,
+    effects: [
+      {
+        name: `The Fang of Ghur`,
+        desc: `The bearer's Beastskewer Glaive has a Rend characteristic of -3 instead of -l.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Dracoline Heart`,
+    effects: [
+      {
+        name: `Dracoline Heart`,
+        desc: `Once per battle, at the start of your hero phase, the bearer can use this artefact. If they do so, pick 1 GREAT MAWPOT terrain feature that is part of your army, within 6" of the bearer and empty. Tlat GREAT MAWPOT is now full.`,
+        when: [START_OF_HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Shrunken Priest Head`,
+    effects: [
+      {
+        name: `Shrunken Priest Head`,
+        desc: `Roll a dice each time you allocate a wound or mortal wound to the bearer. On a 5+, that wound or mortal wound is negated.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Wizardflesh Apron`,
+    effects: [
+      {
+        name: `Wizardflesh Apron`,
+        desc: `The bearer can cast 1 additional spell in each of your hero phases.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Bloodrock Talisman`,
+    effects: [
+      {
+        name: `Bloodrock Talisman`,
+        desc: `Add 2 to the roll when the bearer attempts to unbind or dispel an endless spell.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Grease-smeared Tusks`,
+    effects: [
+      {
+        name: `Grease-smeared Tusks`,
+        desc: `Add 1 to charge rolls for friendly MONSTERS while they are within 9" of the bearer.`,
+        when: [CHARGE_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Rotting Dankhold Spores`,
+    effects: [
+      {
+        name: `Rotting Dankhold Spores`,
+        desc: `Once per battle, at the start of your hero phase, the bearer can use this artefact. If they do so, pick 1 enemy unit within 6" of the bearer and roll a number of dice equal to the number of models in that unit. For each 4+, that unit suffers 1 mortal wound.`,
+        when: [START_OF_HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
 ]
 
 export default Artifacts
--- src/army/ogor_mawtribes/traits.ts
+++ src/army/ogor_mawtribes/traits.ts
@@ -1,16 +1,266 @@
 import { TTraits } from 'types/army'
+import {
+  START_OF_HERO_PHASE,
+  BATTLESHOCK_PHASE,
+  COMBAT_PHASE,
+  START_OF_SETUP,
+  DURING_GAME,
+  CHARGE_PHASE,
+  HERO_PHASE,
+  SHOOTING_PHASE,
+} from 'types/phases'
 
 const CommandTraits: TTraits = [
-  // {
-  //   name: ``,
-  //   effects: [
-  //     {
-  //       name: ``,
-  //       desc: ``,
-  //       when: [HERO_PHASE],
-  //     },
-  //   ],
-  // },
+  {
+    name: `Furious Guzzler`,
+    effects: [
+      {
+        name: `Furious Guzzler`,
+        desc: `At the start of your hero phase, if this general is eating, you can heal up to D3 wounds allocated to them.`,
+        when: [START_OF_HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Prodigious Girth`,
+    effects: [
+      {
+        name: `Prodigious Girth`,
+        desc: `Add 2 to this general's Wounds characteristic.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Killer Reputation`,
+    effects: [
+      {
+        name: `Killer Reputation`,
+        desc: `Before this general is set up for the first time, you can pick a second big name for them. Their second big name cannot be the same as their first big name.`,
+        when: [START_OF_SETUP],
+      },
+    ],
+  },
+  {
+    name: `Mighty Bellower`,
+    effects: [
+      {
+        name: `Mighty Bellower`,
+        desc: `If an enemy unit fails a battleshock test within 6 of this general, add D3 to the number of models that flee.`,
+        when: [BATTLESHOCK_PHASE],
+      },
+    ],
+  },
+  {
+    name: `An Eye for Loot`,
+    effects: [
+      {
+        name: `An Eye for Loot`,
+        desc: `You can re-roll hit and wound rolls for attacks made with melee weapons by this general that target an enemy HERO with an artefact of power.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Crushing Bulk`,
+    effects: [
+      {
+        name: `Crushing Bulk`,
+        desc: `This general is treated as a MONSTER for the purposes of the Trampling Charge battle trait.`,
+        when: [CHARGE_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Questionable Hygiene`,
+    effects: [
+      {
+        name: `Questionable Hygiene`,
+        desc: `Subtract 1 from hit rolls for attacks made by enemy units while they are within 6" of this general.`,
+        when: [SHOOTING_PHASE, COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Herald of the Gulping God`,
+    effects: [
+      {
+        name: `Herald of the Gulping God`,
+        desc: `Friendly units do not take battleshock tests while they are wholly within 15" of this general and this general is eating.`,
+        when: [BATTLESHOCK_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Growling Stomach`,
+    effects: [
+      {
+        name: `Growling Stomach`,
+        desc: `Subtract 2 from the Bravery characteristic of enemy units while they are within 12" of this general and this general is hungry.`,
+        when: [BATTLESHOCK_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Gastromancer`,
+    effects: [
+      {
+        name: `Gastromancer`,
+        desc: `This general knows all spells from the Lore of Gutmagic.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Rolls of Fat`,
+    effects: [
+      {
+        name: `Rolls of Fat`,
+        desc: `Add 2 to this general's Wounds characteristic.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  {
+    name: `Spell-eater`,
+    effects: [
+      {
+        name: `Spell-eater`,
+        desc: `Each time this general dispels an endless spell, you can heal any wounds allocated to them. If this general has no wounds allocated to them, you can instead add 1 to this general's Wounds characteristic until the end of the battle. In addition, each time this general dispels an endless spell, they can cast 1 additional spell in that phase.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
 ]
 
 export default CommandTraits
