ac6860806aef295db39d1327e729257ffda19bf1
Davis Ford <davis.e.ford@gmail.com>
2019-11-01 16:02:57 -0400

    Update offline.tsx

--- src/components/info/offline.tsx
+++ src/components/info/offline.tsx
@@ -8,7 +8,7 @@ const OfflineComponent = () => {
   const { isOnline } = useAppStatus()
   const { theme } = useTheme()
 
-  if (isOnline) return null
+  // if (isOnline) return null
 
   const userName = LocalUserName.get()
 
@@ -16,23 +16,23 @@ const OfflineComponent = () => {
     <div className="container pt-4">
       <div className="row justify-content-center">
         <div
-          className={`col-12 col-sm-8 col-md-6 col-lg-6 col-xl-6 col-xxl-4 card ${theme.bgColor} py-3 text-center`}
+          className={`col-12 col-sm-8 col-md-6 col-lg-6 col-xl-6 col-xxl-4 ${theme.card} ${theme.bgColor} ${theme.text} py-3 text-center`}
         >
           <p className="text-danger">
             <FiWifiOff className="mr-2" />
             You are in <strong>Offline</strong> mode.
             <FiWifiOff className="ml-2" />
           </p>
-          <p className={theme.text}>Your capabilites are limited in this mode.</p>
-          You cannot save an army.
+          <p>Your capabilites are limited in this mode.</p>
+          You cannot save a new army.
           {!!userName && (
             <>
               <br />
               You cannot update or delete armies.
               <br />
+              You cannot access your profile.
             </>
           )}
-          {!!userName && <>You cannot access your profile.</>}
         </div>
       </div>
     </div>
