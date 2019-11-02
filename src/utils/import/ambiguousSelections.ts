f4cc30a3c09a575494d5f2d7e62c087026547d62
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2019-11-02 21:02:44 +0000

    Check all selection types for ambiguous imports

--- src/utils/import/ambiguousSelections.ts
+++ src/utils/import/ambiguousSelections.ts
@@ -1,4 +1,6 @@
+import { TSupportedFaction } from 'meta/factions'
 import { TImportError } from 'types/import'
+import { TAllySelectionStore } from 'types/store'
 import { TNameMap } from 'utils/import/options'
 import { createAmbiguityWarning } from 'utils/import/warnings'
 
@@ -6,21 +8,25 @@ import { createAmbiguityWarning } from 'utils/import/warnings'
  * Adds an error for each selection we've identified could be wrong based on ambiguous naming in the source
  * @param errors
  * @param selections
+ * @param allyData
  * @param ambiguousNamesMap
  */
 export const addAmbiguousSelectionErrors = (
   errors: TImportError[],
-  selections: string[],
+  selections: { [key: string]: string[] },
+  allyData: { allyFactionNames: TSupportedFaction[]; allySelections: TAllySelectionStore },
   ambiguousNamesMap: TNameMap
 ) => {
-  selections.forEach(selection => {
-    const match = ambiguousNamesMap[selection]
-    if (match) {
-      errors.push(
-        createAmbiguityWarning(
-          `Azyr lists more than one unit as '${match}'. Please check it has imported the correct one.`
+  for (let key in selections) {
+    selections[key].forEach(selection => {
+      const match = ambiguousNamesMap[selection]
+      if (match) {
+        errors.push(
+          createAmbiguityWarning(
+            `Azyr lists more than one unit as '${match}'. Please check it has imported the correct one.`
+          )
         )
-      )
-    }
-  })
+      }
+    })
+  }
 }
