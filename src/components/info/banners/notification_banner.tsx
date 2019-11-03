abbc0d2b8bb95484f0b31fd01f29f267501691f6
Davis Ford <davis.e.ford@gmail.com>
2019-11-03 16:32:42 -0500

    Update banners

--- src/components/info/banners/notification_banner.tsx
+++ src/components/info/banners/notification_banner.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { hideNotificationBanner, getNotificationBanner } from 'utils/localStore'
+import { logDisplay, logClick } from 'utils/analytics'
+import { centerContentClass } from 'theme/helperClasses'
 
 interface IBannerProps {
-  name: string
-  variant?: TAlertTypes
-  persistClose?: boolean
   displayOnce?: boolean
+  enableLog?: boolean
+  name: string
+  persistClose?: boolean
+  variant?: TAlertTypes
 }
 
 /**
@@ -14,18 +17,27 @@ interface IBannerProps {
  * @param props
  */
 export const NotificationBanner: React.FC<IBannerProps> = props => {
-  const { name, variant = 'primary', persistClose = true, displayOnce = false, children } = props
+  const {
+    children,
+    displayOnce = false,
+    enableLog = false,
+    name,
+    persistClose = true,
+    variant = 'primary',
+  } = props
   const isHidden = persistClose || displayOnce ? getNotificationBanner(name) === 'hidden' : false
   const [isOn, setIsOn] = useState(!isHidden)
 
   const handleClose = () => {
     setIsOn(false)
-    persistClose && hideNotificationBanner(name)
+    if (persistClose) hideNotificationBanner(name)
+    if (enableLog) logClick(`Close-${name}`)
   }
 
   useEffect(() => {
+    if (enableLog && isOn) logDisplay(name)
     return () => {
-      if (displayOnce) hideNotificationBanner(name)
+      if (displayOnce && isOn) hideNotificationBanner(name)
     }
   })
 
@@ -34,7 +46,7 @@ export const NotificationBanner: React.FC<IBannerProps> = props => {
   return (
     <div className="mb-2">
       <div className={`alert alert-${variant} text-center fade show d-flex`} role="alert">
-        <div className={`flex-grow-1`}>{children}</div>
+        <div className={`flex-grow-1 ${centerContentClass}`}>{children}</div>
         <div className={`align-self-start ml-2`}>
           <button type="button" className="close" aria-label="Close" onClick={handleClose}>
             <span aria-hidden="true">&times;</span>
--- src/components/info/banners/update_banner.tsx
+++ src/components/info/banners/update_banner.tsx
@@ -1,21 +1,16 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { NotificationBanner } from 'components/info/banners/notification_banner'
 import { useAppStatus } from 'context/useAppStatus'
-import { logDisplay } from 'utils/analytics'
 
 const UpdateBanner = () => {
   const { hasNewContent } = useAppStatus()
   const name = 'Content_Update_Notification'
 
-  useEffect(() => {
-    if (hasNewContent) logDisplay(name)
-  }, [hasNewContent])
-
   if (!hasNewContent) return <></>
 
   return (
-    <NotificationBanner name={name} persistClose={false} variant={'warning'}>
-      New content is available and will be used when all tabs for this page are closed.
+    <NotificationBanner name={name} persistClose={false} variant={'warning'} enableLog={true}>
+      Updates are available and will be used once all tabs for this page are closed.
     </NotificationBanner>
   )
 }
