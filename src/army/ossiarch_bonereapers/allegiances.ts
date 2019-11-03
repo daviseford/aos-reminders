9ccf329fafd257e078648275d9266c096dd6c07b
Davis Ford <davis.e.ford@gmail.com>
2019-11-03 08:12:47 -0500

    Add abilities

--- src/army/ossiarch_bonereapers/allegiances.ts
+++ src/army/ossiarch_bonereapers/allegiances.ts
@@ -3,10 +3,66 @@ import { TAllegiances } from 'types/army'
 // This is where we store sub-allegiances such as
 // Grand Courts, Hosts, Clans, Glades, Lodges, etc
 const Allegiances: TAllegiances = [
-  // {
-  //   name: ``,
-  //   effects: [{ name: ``, desc: ``, when: [] }],
-  // },
+  {
+    name: `Mortis Praetorians`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Petrifex Elite`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Stalliarch Lords`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Ivory Host`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Null Myriad`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+  {
+    name: `Crematorians`,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
 ]
 
 export default Allegiances
