c0e3d68c80e9428ef0f9d1fc2430f051ac7efa8c
Davis Ford <daviseford@users.noreply.github.com>
2019-11-06 09:10:28 -0500

    Update Big Waagh ability text (#686)
    * Update Big Waagh abilities
    
    * Update Wyldwood rules (fixes #641)
    
    * Update units.ts

--- src/army/sylvaneth/scenery.ts
+++ src/army/sylvaneth/scenery.ts
@@ -1,14 +1,29 @@
 import { TScenery } from 'types/army'
-import { START_OF_SETUP } from 'types/phases'
+import { START_OF_SETUP, DURING_GAME, HERO_PHASE, END_OF_CHARGE_PHASE } from 'types/phases'
 
 const Scenery: TScenery = [
   {
-    name: `Wyldwood Groves`,
+    name: `Awakened Wyldwood`,
     effects: [
       {
-        name: `Wyldwood Groves`,
-        desc: `After all other pieces of scenery are set up, but before the battle begins and players choose territory or set up their armies, you can place one Sylvaneth Wyldwood anywhere on the battlefield that is more than 1" from any other piece of scenery.`,
+        name: `Setup`,
+        desc: `After territories have been chosen but before armies are set up, you can set up the AWAKENED WYLDWOOD anywhere on the battlefield that is more than 1" from any other terrain features, more than 1" from enemy territory and more than 6" away from any objectives.`,
         when: [START_OF_SETUP],
+      },
+      {
+        name: `Overgrown Wilderness`,
+        desc: `Models are not visible to each other if an imaginary straight line 1mm wide drawn between the closest points of the two models crosses over more than 1" of an AWAKENED WYLDWOOD. This scenery rule does not apply if either model can fly.`,
+        when: [DURING_GAME],
+      },
+      {
+        name: `Roused by Magic`,
+        desc: `In the hero phase, if a spell is successfully cast by a WIZARD wholly within 6" of an AWAKENED WYLDWOOD and not unbound, roll a D6 for each unit within 1" of that AWAKENED WYLDWOOD which does not have the SYLVANETH keyword. On a 5+ that unit suffers D3 mortal wounds after that spell's effects have been resolved.`,
+        when: [HERO_PHASE],
+      },
+      {
+        name: `Wyldwood`,
+        desc: `At the end of the charge phase, roll a D6 for each unit within 1" of an AWAKENED WYLDWOOD which does not have the SYLVANETH keyword. On a 6, that unit suffers D3 mortal wounds.`,
+        when: [END_OF_CHARGE_PHASE],
       },
     ],
   },
--- src/army/sylvaneth/units.ts
+++ src/army/sylvaneth/units.ts
@@ -5,7 +5,6 @@ import {
   COMBAT_PHASE,
   DURING_GAME,
   DURING_SETUP,
-  END_OF_CHARGE_PHASE,
   END_OF_COMBAT_PHASE,
   END_OF_MOVEMENT_PHASE,
   HERO_PHASE,
@@ -19,26 +18,6 @@ import {
 
 // Unit Names
 export const Units: TUnits = [
-  {
-    name: `Awakened Wyldwood`,
-    effects: [
-      {
-        name: `Overgrown Wilderness`,
-        desc: `Models are not visible to each other if an imaginary straight line 1mm wide drawn between the closest points of the two models crosses over more than 1" of an AWAKENED WYLDWOOD. This scenery rule does not apply if either model can fly.`,
-        when: [DURING_GAME],
-      },
-      {
-        name: `Roused by Magic`,
-        desc: `In the hero phase, if a spell is successfully cast by a WIZARD wholly within 6" of an AWAKENED WYLDWOOD and not unbound, roll a D6 for each unit within 1" of that AWAKENED WYLDWOOD which does not have the SYLVANETH keyword. On a 5+ that unit suffers D3 mortal wounds after that spell's effects have been resolved.`,
-        when: [HERO_PHASE],
-      },
-      {
-        name: `Wyldwood`,
-        desc: `At the end of the charge phase, roll a D6 for each unit within 1" of an AWAKENED WYLDWOOD which does not have the SYLVANETH keyword. On a 6, that unit suffers D3 mortal wounds.`,
-        when: [END_OF_CHARGE_PHASE],
-      },
-    ],
-  },
   {
     name: `Alarielle The Everqueen`,
     effects: [
