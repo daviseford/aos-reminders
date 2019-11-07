d4a2e57bcaff3191d2b6ed52b8371c388fb6b50a
Davis Ford <davis.e.ford@gmail.com>
2019-11-07 18:57:44 -0500

    Update loadArmyFromLink.ts

--- src/utils/loadArmyFromLink.ts
+++ src/utils/loadArmyFromLink.ts
@@ -7,33 +7,37 @@ import { PreferenceApi } from 'api/preferenceApi'
 import { ILinkedArmy } from 'types/savedArmy'
 
 export const loadArmyFromLink = async (id: string) => {
-  const res = await PreferenceApi.getLinkedArmy(id)
-  const linkedArmy = res.body as ILinkedArmy
+  try {
+    const res = await PreferenceApi.getLinkedArmy(id)
+    const linkedArmy = res.body as ILinkedArmy
 
-  if (!linkedArmy) return
+    if (!linkedArmy) return
 
-  store.dispatch(factionNames.actions.setFactionName(linkedArmy.factionName))
+    store.dispatch(factionNames.actions.setFactionName(linkedArmy.factionName))
 
-  // Add Ally Game data to the store
-  if (linkedArmy.allyFactionNames.length) {
-    const armies = linkedArmy.allyFactionNames.map(factionName => {
-      const Army = getArmy(factionName) as IArmy
-      return { factionName, Army }
-    })
-    store.dispatch(army.actions.updateAllyArmies(armies))
+    // Add Ally Game data to the store
+    if (linkedArmy.allyFactionNames.length) {
+      const armies = linkedArmy.allyFactionNames.map(factionName => {
+        const Army = getArmy(factionName) as IArmy
+        return { factionName, Army }
+      })
+      store.dispatch(army.actions.updateAllyArmies(armies))
+    }
+
+    store.dispatch(selections.actions.updateSelections(linkedArmy.selections))
+    store.dispatch(selections.actions.updateAllySelections(linkedArmy.allySelections))
+    store.dispatch(realmscape.actions.setRealmscape(linkedArmy.realmscape))
+    store.dispatch(realmscape.actions.setRealmscapeFeature(linkedArmy.realmscape_feature))
+
+    // Hide any reminders necessary
+    if (linkedArmy.hiddenReminders) {
+      store.dispatch(visibility.actions.clearReminder())
+      store.dispatch(visibility.actions.addReminders(linkedArmy.hiddenReminders))
+    }
+
+    logEvent(`LoadArmyLink-${id}`)
+    logEvent(`LoadArmyFromLink-${linkedArmy.factionName}`)
+  } catch (err) {
+    console.error(err)
   }
-
-  store.dispatch(selections.actions.updateSelections(linkedArmy.selections))
-  store.dispatch(selections.actions.updateAllySelections(linkedArmy.allySelections))
-  store.dispatch(realmscape.actions.setRealmscape(linkedArmy.realmscape))
-  store.dispatch(realmscape.actions.setRealmscapeFeature(linkedArmy.realmscape_feature))
-
-  // Hide any reminders necessary
-  if (linkedArmy.hiddenReminders) {
-    store.dispatch(visibility.actions.clearReminder())
-    store.dispatch(visibility.actions.addReminders(linkedArmy.hiddenReminders))
-  }
-
-  logEvent(`LoadArmyLink-${id}`)
-  logEvent(`LoadArmyFromLink-${linkedArmy.factionName}`)
 }
