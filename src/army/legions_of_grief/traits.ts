0a21e6f07ff0ef19b8e7dea735bd200fbadd2ce9
Davis Ford <davis.e.ford@gmail.com>
2019-11-07 14:31:47 -0500

    Amethyst Glow

--- src/army/legions_of_grief/traits.ts
+++ src/army/legions_of_grief/traits.ts
@@ -3,11 +3,11 @@ import { BATTLESHOCK_PHASE, DURING_GAME, HERO_PHASE } from 'types/phases'
 
 const CommandTraits: TTraits = [
   {
-    name: `Amathyst Glow`,
+    name: `Amethyst Glow`,
     effects: [
       {
-        name: `Amathyst Glow`,
-        desc: `This general is a WIZARD. They can attempt to cast one spell in you hero phase, and attempt to unbind one spell in the enemy hero phase. They know the arcane bolt and mystic shield spells.\n\nIf this general is already a WIZARD, they know one extra spell from the Lore of Sorrows.`,
+        name: `Amethyst Glow`,
+        desc: `This general is a WIZARD. They can attempt to cast one spell in your hero phase, and attempt to unbind one spell in the enemy hero phase. They know the arcane bolt and mystic shield spells.\n\nIf this general is already a WIZARD, they know one extra spell from the Lore of Sorrows.`,
         when: [HERO_PHASE],
       },
     ],
