7439f8e89ab7a457541b461804f1147381188372
Davis Ford <davis.e.ford@gmail.com>
2019-11-26 16:42:57 -0500

    Fixing update button

--- src/components/info/banners/update_banner.tsx
+++ src/components/info/banners/update_banner.tsx
@@ -3,7 +3,8 @@ import { useAppStatus } from 'context/useAppStatus'
 import { useTheme } from 'context/useTheme'
 import { MdRefresh } from 'react-icons/md'
 import { componentWithSize } from 'utils/mapSizesToProps'
-import { installNewWorker } from 'utils/installNewWorker'
+import { LocalStoredArmy } from 'utils/localStore'
+import { logClick } from 'utils/analytics'
 import { NotificationBanner } from 'components/info/banners/notification_banner'
 import GenericButton from 'components/input/generic_button'
 
@@ -18,12 +19,32 @@ const UpdateBanner = componentWithSize(({ isTinyMobile = false, isMobile = false
   const btnClass = `btn btn-${btnSize} btn-primary ml-3`
   const iconClass = isTinyMobile ? `` : `mr-2`
 
+  const handleAccept = async () => {
+    logClick('ReloadContentButton')
+    LocalStoredArmy.set() // Save anything we're working on
+    window.location.reload()
+    // try {
+    //   if (navigator && navigator.serviceWorker) {
+    //     const waitingServiceWorker = await navigator.serviceWorker.ready
+
+    //     if (waitingServiceWorker.waiting) {
+    //       logClick('ReloadContentButton')
+    //       LocalStoredArmy.set() // Save anything we're working on
+    //       window.location.reload()
+    //       // waitingServiceWorker.waiting.postMessage({ type: 'SKIP_WAITING' })
+    //     }
+    //   }
+    // } catch (e) {
+    //   console.error(e)
+    // }
+  }
+
   if (!hasNewContent) return <></>
 
   return (
     <NotificationBanner name={name} persistClose={false} variant={variant} enableLog={true}>
       Updates are available!
-      <GenericButton onClick={() => installNewWorker(true)} className={btnClass}>
+      <GenericButton onClick={handleAccept} className={btnClass}>
         <MdRefresh className={iconClass} />
         {btnText}
       </GenericButton>
