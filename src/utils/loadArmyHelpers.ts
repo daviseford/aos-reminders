fb309c4754ff1aefbeb9fe00afdc9edd15811f0b
Davis Ford <davis.e.ford@gmail.com>
2019-11-28 10:23:31 -0500

    Oberhaul

--- src/utils/loadArmyHelpers.ts
+++ src/utils/loadArmyHelpers.ts
@@ -1,12 +1,13 @@
 import { store } from 'index'
 import { PreferenceApi } from 'api/preferenceApi'
 import { factionNames, army, selections, realmscape, visibility } from 'ducks'
-import { logEvent } from 'utils/analytics'
+import { logEvent, logIndividualSelection, logAllyFaction, logFactionSwitch } from 'utils/analytics'
 import { getArmy } from 'utils/getArmy/getArmy'
 import { LocalStoredArmy } from 'utils/localStore'
 import { IArmy, ICurrentArmy } from 'types/army'
-import { ILinkedArmy } from 'types/savedArmy'
+import { ILinkedArmy, ISavedArmyFromApi } from 'types/savedArmy'
 import { IImportedArmy } from 'types/import'
+import { titleCase } from './textUtils'
 
 export const loadArmyFromLink = async (id: string) => {
   try {
@@ -36,35 +37,85 @@ export const loadArmyFromLocalStore = () => {
   logEvent(`LoadArmyFromStore-${storedArmy.factionName}`)
 }
 
-type TLoadedArmy = IImportedArmy | ILinkedArmy | (ICurrentArmy & { hiddenReminders: string[] })
+type TLoadedArmy =
+  | ISavedArmyFromApi
+  | IImportedArmy
+  | ILinkedArmy
+  | (ICurrentArmy & { hiddenReminders: string[] })
 
-export const addArmyToStore = (linkedArmy: TLoadedArmy) => {
+export const addArmyToStore = (loadedArmy: TLoadedArmy) => {
   try {
-    store.dispatch(factionNames.actions.setFactionName(linkedArmy.factionName))
+    store.dispatch(factionNames.actions.setFactionName(loadedArmy.factionName))
 
     // Add Ally Game data to the store
-    if (linkedArmy.allyFactionNames.length) {
-      const armies = linkedArmy.allyFactionNames.map(factionName => {
+    if (loadedArmy.allyFactionNames.length) {
+      const armies = loadedArmy.allyFactionNames.map(factionName => {
         const Army = getArmy(factionName) as IArmy
         return { factionName, Army }
       })
       store.dispatch(army.actions.updateAllyArmies(armies))
     }
 
-    store.dispatch(selections.actions.updateSelections(linkedArmy.selections))
-    store.dispatch(selections.actions.updateAllySelections(linkedArmy.allySelections))
-    store.dispatch(realmscape.actions.setOriginRealm(linkedArmy.origin_realm || null))
-    store.dispatch(realmscape.actions.setRealmscape(linkedArmy.realmscape))
-    store.dispatch(realmscape.actions.setRealmscapeFeature(linkedArmy.realmscape_feature))
+    // Add our unit selections to the store
+    store.dispatch(selections.actions.updateSelections(loadedArmy.selections))
+
+    // Add our allied unit selections to the store
+    store.dispatch(selections.actions.updateAllySelections(loadedArmy.allySelections))
+
+    // Add Realm info to the store
+    store.dispatch(realmscape.actions.setOriginRealm(loadedArmy.origin_realm || null))
+    store.dispatch(realmscape.actions.setRealmscape(loadedArmy.realmscape || null))
+    store.dispatch(realmscape.actions.setRealmscapeFeature(loadedArmy.realmscape_feature || null))
 
     // Hide any reminders necessary
-    if (linkedArmy.hiddenReminders) {
+    if (loadedArmy.hiddenReminders) {
       store.dispatch(visibility.actions.clearReminder())
-      store.dispatch(visibility.actions.addReminders(linkedArmy.hiddenReminders))
+      store.dispatch(visibility.actions.addReminders(loadedArmy.hiddenReminders))
     }
+
+    // Log our army to GA
+    logLoadedArmy(loadedArmy)
   } catch (err) {
     console.error(err)
   }
 }
 
-const logLoadedArmy = (army: TLoadedArmy) => {}
+/**
+ * Logs all the pertinent details of a loaded army to Google Analytics
+ * @param army
+ */
+export const logLoadedArmy = (army: TLoadedArmy) => {
+  try {
+    const {
+      selections = [],
+      allySelections = {},
+      origin_realm = null,
+      realmscape = null,
+      realmscape_feature = null,
+      factionName = null,
+    } = army
+
+    // Log the faction name
+    if (factionName) logFactionSwitch(factionName)
+
+    // Log each selection
+    Object.keys(selections).forEach(key => {
+      const trait = titleCase(key)
+      selections[key].forEach((name: string) => logIndividualSelection(trait, name))
+    })
+
+    // Log each allied faction + selection
+    Object.keys(allySelections).forEach(allyFactionName => {
+      logAllyFaction(allyFactionName)
+      const units: string[] = allySelections[allyFactionName].units || []
+      units.forEach(name => logIndividualSelection('AlliedUnits', name))
+    })
+
+    // Log Realm information
+    if (origin_realm) logIndividualSelection('Realm of Origin', origin_realm)
+    if (realmscape) logIndividualSelection('Realm of Battle', realmscape)
+    if (realmscape_feature) logIndividualSelection('Realm Feature', realmscape_feature)
+  } catch (err) {
+    console.error(err)
+  }
+}
