2362c3367b3452da688579ff1926ac3d6d926abe
Davis Ford <daviseford@users.noreply.github.com>
2019-11-30 13:04:47 -0500

    Rules cleanup (#777)
    * Cleaning up some more rules
    
    * Remove unused LoN traits
    
    * Update units.ts
    
    * Update units.ts
    
    * Update units.ts
    
    * Update units.ts

--- src/army/legions_of_nagash/index.ts
+++ src/army/legions_of_nagash/index.ts
@@ -1,16 +1,10 @@
 import { Battalions, Units } from './units'
 import Abilities from './abilities'
-import Artifacts from './artifacts'
-import EndlessSpells from './endless_spells'
 import Spells from './spells'
-import Traits from './traits'
 
 export default {
   Abilities,
-  Artifacts,
   Battalions,
-  EndlessSpells,
   Spells,
-  Traits,
   Units,
 }
