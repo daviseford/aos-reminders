aa37f873abdeebcd80cc43adf6458a2b9caaa5fc
John Schramm <jas1535@hotmail.com>
2019-12-08 22:46:20 -0700

    Updated StD units with new warscrolls - WIP. Added StD endless_spells.ts. Removed adopted StD units from GA Chaos. Cleaned up naming in Slaanesh/BoC spells. Removed redundant import from BoC into Slaanesh.

--- src/army/slaves_to_darkness/index.ts
+++ src/army/slaves_to_darkness/index.ts
@@ -1,12 +1,14 @@
 import { Battalions, Units } from './units'
 import Abilities from './abilities'
 import Artifacts from './artifacts'
+import EndlessSpells from './endless_spells'
 import Traits from './traits'
 
 export default {
   Abilities,
   Artifacts,
   Battalions,
+  EndlessSpells,
   Traits,
   Units,
 }
