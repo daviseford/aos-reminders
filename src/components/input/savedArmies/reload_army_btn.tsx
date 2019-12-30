b743b58de15227595932f2adc1895223823fe844
Davis Ford <davis.e.ford@gmail.com>
2019-12-30 14:33:16 -0500

    Update reload_army_btn.tsx

--- src/components/input/savedArmies/reload_army_btn.tsx
+++ src/components/input/savedArmies/reload_army_btn.tsx
@@ -1,26 +1,17 @@
 import React from 'react'
+import { FaSyncAlt } from 'react-icons/fa'
 import { useSavedArmies } from 'context/useSavedArmies'
 import { useTheme } from 'context/useTheme'
-import { FaSyncAlt } from 'react-icons/fa'
 import GenericButton from 'components/input/generic_button'
 
-interface IReloadArmyProps {}
-
-type TReloadArmyBtn = React.FC<IReloadArmyProps>
-
-const ReloadArmyBtn: TReloadArmyBtn = () => {
+const ReloadArmyBtn: React.FC = () => {
   const { isDark } = useTheme()
   const { reloadArmy } = useSavedArmies()
 
-  const handleClick = async e => {
-    e.preventDefault()
-    reloadArmy()
-  }
-
-  const btnClass = `btn${isDark ? ` btn-outline-light ` : ` `} btn-block`
+  const btnClass = `btn ${isDark ? `btn-outline-light` : ``} btn-block`
 
   return (
-    <GenericButton className={btnClass} onClick={handleClick}>
+    <GenericButton className={btnClass} onClick={reloadArmy}>
       <FaSyncAlt className="mr-2" />
       Reload Army
     </GenericButton>
