70e00a32cdf367d9d9f5b01769e9279420ce51c8
Davis Ford <davis.e.ford@gmail.com>
2020-03-17 09:12:52 -0400

    Endless spells

---
+++ src/army/seraphon/endless_spells.ts
@@ -0,0 +1,17 @@
+import { TEndlessSpells } from 'types/army'
+
+// Endless spells.
+const EndlessSpells: TEndlessSpells = [
+  {
+    name: ``,
+    effects: [
+      {
+        name: ``,
+        desc: ``,
+        when: [],
+      },
+    ],
+  },
+]
+
+export default EndlessSpells
--- src/army/seraphon/index.ts
+++ src/army/seraphon/index.ts
@@ -2,6 +2,7 @@ import { Battalions, Units } from './units'
 import Abilities from './abilities'
 import Allegiances from './allegiances'
 import Artifacts from './artifacts'
+import EndlessSpells from './endless_spells'
 import Scenery from './scenery'
 import Spells from './spells'
 import Traits from './traits'
@@ -11,6 +12,7 @@ export default {
   Allegiances,
   Artifacts,
   Battalions,
+  EndlessSpells,
   Scenery,
   Spells,
   Traits,
