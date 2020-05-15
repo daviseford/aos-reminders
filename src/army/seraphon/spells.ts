2163776625fb67df5e35d6bad39e85ee29121980
Davis Ford <davis.e.ford@gmail.com>
2020-05-15 11:20:34 -0400

    better spacing

--- src/army/seraphon/spells.ts
+++ src/army/seraphon/spells.ts
@@ -37,7 +37,7 @@ const Spells: TSpells = [
     effects: [
       {
         name: `Mystical Unforging`,
-        desc: `Casting value of 7. Pick 1 enemy HERO that bears an artefact of power within 12" of the caster and visible to them. That HERO suffers D3 mortal wounds and you must roll a dice. On a 5+, that HEROno longer bears that artefact of power (if a weapon was picked when it was selected, the weapon reverts to normal).`,
+        desc: `Casting value of 7. Pick 1 enemy HERO that bears an artefact of power within 12" of the caster and visible to them. That HERO suffers D3 mortal wounds and you must roll a dice. On a 5+, that HERO no longer bears that artefact of power (if a weapon was picked when it was selected, the weapon reverts to normal).`,
         when: [HERO_PHASE],
       },
     ],
