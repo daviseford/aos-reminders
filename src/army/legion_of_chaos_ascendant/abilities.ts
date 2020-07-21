0537c0f2bc65534ecdaba400da829921d135b5f7
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-07-21 14:36:43 +0100

    Fix a few little mistakes in the FAQ changes (#977)

--- src/army/legion_of_chaos_ascendant/abilities.ts
+++ src/army/legion_of_chaos_ascendant/abilities.ts
@@ -10,7 +10,7 @@ const Abilities: TAbilities = [
   },
   {
     name: `Unyielding Legions`,
-    desc: `Pick 1 friendly Chaos Ascendant Daemon hero that is on the battlefield and roll 3D6. On a 10+ you can summon a 10 man unit of either Bloodletters, Plaguebearers, Daemonettes, or Pink Horrors. - Tzeentch Hero: 5 Horrors of Tzeentch. The unit's god keyword must match a god keyword from the summoning hero. The summoned unit must be setup within 12" of the hero and more than 9" from enemy units.
+    desc: `Pick 1 friendly Chaos Ascendant Daemon hero that is on the battlefield and roll 3D6. On a 10+ you can summon either 10 Bloodletters, 10 Plaguebearers, 10 Daemonettes, or 5 Horrors of Tzeentch. The unit's god keyword must match a god keyword from the summoning hero. The summoned unit must be setup within 12" of the hero and more than 9" from enemy units.
            If the unmodified summoning roll contained any doubles, the hero suffers 1 mortal wound. If the unmodified summoning roll was a triple, the hero suffers D3 mortal wounds instead.`,
     when: [END_OF_MOVEMENT_PHASE],
   },
