f30420ce6675d6bcb8107affb862107f606f4a9d
Unknown <amcalvert@esteiro.co.uk>
2020-07-26 14:02:40 +0100

    Battalions

--- src/army/lumineth/units.ts
+++ src/army/lumineth/units.ts
@@ -237,15 +237,34 @@ export const Units: TUnits = [
 
 export const AlliedUnits: TUnits = idoneth.Units
 
-// Battalions
 export const Battalions: TBattalions = [
   {
-    name: ``,
+    name: `Alarith Temple`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Skin to Stone`,
+        desc: `Any friendly STONEGUARD units from this battalion that are wholly within 12" of a friendly HERO from the same battalion can turn their skin to stone until the end of the phase. Re-roll save rolls for attacks that target a unit that has turned its skin to stone, but models in the unit that has turned its skin to stone can only move 1" when they pile in.`,
+        when: [START_OF_COMBAT_PHASE],
+      },
+    ],
+  },
+  {
+    name: `Auralan Legion`,
+    effects: [
+      {
+        name: `Shield of Light`,
+        desc: `You can re-roll save rolls of 1 for attacks that target a friendly unit from this battalion whilst it is within 3" of any other friendly unit from the same battalion.`,
+        when: [WOUND_ALLOCATION],
+      },
+    ],
+  },
+  {
+    name: `Dawnrider Lance`,
+    effects: [
+      {
+        name: `Shafts of Light`,
+        desc: `Re-roll hit rolls of 1 for attacks made with melee weapons by friendly units from this battalion that made a charge move in the same turn.`,
+        when: [COMBAT_PHASE],
       },
     ],
   },
