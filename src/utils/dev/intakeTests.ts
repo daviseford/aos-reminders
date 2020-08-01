ae312235b582e699cb9d47a92583f2b5899708af
Davis Ford <davis.e.ford@gmail.com>
2020-08-01 14:08:45 -0400

    Update intakeTests

--- src/utils/dev/intakeTests.ts
+++ src/utils/dev/intakeTests.ts
@@ -1,33 +1,118 @@
-import { WARSCROLL_BUILDER, BATTLESCRIBE, AZYR } from 'types/import'
-
 const fs = require('fs')
+const path = require('path')
 
 const PLACEHOLDER_TXT = 'placeholder.txt'
-const WSB_OUTPUT = `${WARSCROLL_BUILDER}_tests.txt`
-const BS_OUTPUT = `${BATTLESCRIBE}_tests.txt`
-const AZYR_OUTPUT = `${AZYR}_tests.txt`
+const WSB_PDF_OUTPUT = `WSB_pdf_tests.txt`
+const WSB_JSON_OUTPUT = `WSB_json_tests.txt`
+const BS_OUTPUT = `BATTLESCRIBE_tests.txt`
+const AZYR_JSON_OUTPUT = `AZYR_json_tests.txt`
 
-const IGNORED_FILES = [PLACEHOLDER_TXT, WSB_OUTPUT, BS_OUTPUT, AZYR_OUTPUT]
+const IGNORED_FILES = [PLACEHOLDER_TXT, WSB_PDF_OUTPUT, WSB_JSON_OUTPUT, BS_OUTPUT, AZYR_JSON_OUTPUT]
 
-const INTAKE_DIR = 'src/tests/fixtures/intake/'
+const INTAKE_DIR = 'src/tests/fixtures/intake'
+const FAILED_DIR = 'src/tests/fixtures/intake/failed'
 const WSB_JSON_DIR = 'src/tests/fixtures/warscroll/json'
 const WSB_PDF_DIR = 'src/tests/fixtures/warscroll/pdf'
 const BS_DIR = 'src/tests/fixtures/battlescribe/html'
 const AZYR_JSON_DIR = 'src/tests/fixtures/azyr/json'
-const AZYR_PDF_DIR = 'src/tests/fixtures/azyr/pdf'
 
-const wsbTest = (file: string) => `
-it('should work with ${file}', () => {
-    const parsedText = getFile('${file}')
+const FIXTURE_DIRS = [WSB_JSON_DIR, WSB_PDF_DIR, BS_DIR, AZYR_JSON_DIR]
+
+const azyrJsonTest = (filename: string) => `
+it('should correctly read ${filename}', () => {
+    const fileTxt = getFile('${filename}')
+    const pages = handleAzyrPages(fileTxt)
+    const res = getAzyrArmyFromPdf(pages)
+    expect(res.errors).toEqual([])
+})`
+
+const bsHtmlTest = (filename: string) => `
+it('should correctly read ${filename}', () => {
+    const parsedText = getFile('${filename}')
+    const res = getBattlescribeArmy(parsedText)
+    expect(res.selections.units).toContain('Orpheon Katakros')
+    expect(res.errors).toEqual([])
+})`
+
+const wsbJsonTest = (filename: string) => `
+it('should correctly read ${filename}', () => {
+    const parsedText = getFile('${filename}')
     const res = getWarscrollArmyFromPdf(parsedText)
     expect(res.errors).toEqual([])
-})
-`
+})`
 
-fs.readdirSync('src/tests/fixtures/intake/').forEach(file => {
-  if (IGNORED_FILES.includes(file)) return
+const wsbPdfTest = (filename: string) => `
+it('should correctly read ${filename}', () => {
+    const pdfText = getFile('${filename}')
+    const parsedText = parsePdf(pdfText)
+    const res = getWarscrollArmyFromPdf(parsedText)
 
-  console.log(file)
-})
+    expect(res.errors).toEqual([])
+})`
 
+let WSB_JSON_OUTPUT_TXT = ''
+let WSB_PDF_OUTPUT_TXT = ''
+let AZYR_PDF_OUTPUT_TXT = ''
+let AZYR_JSON_OUTPUT_TXT = ''
+let BS_OUTPUT_TXT = ''
+
+const run = () => {
+  const intake_files: string[] = fs.readdirSync(INTAKE_DIR).filter(x => !IGNORED_FILES.includes(x))
+  const existing_files: string[] = FIXTURE_DIRS.map(x => fs.readdirSync(x)).flat()
+
+  console.log(intake_files)
+  //  console.log(existing_files)
+
+  intake_files.forEach(filename => {
+    const src = `${INTAKE_DIR}/${filename}`
+
+    // if (existing_files.includes(filename)) {
+    //   console.error('Exists already: ' + filename)
+    //   return
+    // }
+
+    // WSB
+    if (filename.includes('Warscroll_Builder')) {
+      if (filename.endsWith('.json')) {
+        fs.copyFileSync(src, `${WSB_JSON_DIR}/${filename}`)
+        WSB_JSON_OUTPUT_TXT = `${WSB_JSON_OUTPUT_TXT}\n${wsbJsonTest(filename.replace('.json', ''))}`
+      } else {
+        fs.copyFileSync(src, `${WSB_PDF_DIR}/${filename}`)
+        WSB_PDF_OUTPUT_TXT = `${WSB_PDF_OUTPUT_TXT}\n${wsbPdfTest(filename.replace('.pdf', ''))}`
+      }
+      return
+    }
+
+    // Battlescribe
+    if (filename.endsWith('.html')) {
+      fs.copyFileSync(src, `${BS_DIR}/${filename}`)
+      BS_OUTPUT_TXT = `${BS_OUTPUT_TXT}\n${bsHtmlTest(filename.replace('.html', ''))}`
+      return
+    }
+
+    // Azyr
+    if (filename.includes('Azyr') && filename.endsWith('.json')) {
+      fs.copyFileSync(src, `${AZYR_JSON_DIR}/${filename}`)
+      AZYR_JSON_OUTPUT_TXT = `${AZYR_JSON_OUTPUT_TXT}\n${azyrJsonTest(filename.replace('.json', ''))}`
+      return
+    }
+  })
+}
+
+const moveToFailed = filename => {
+  const src = `${INTAKE_DIR}/${filename}`
+  fs.copyFileSync(src, `${FAILED_DIR}/${filename}`)
+}
+
+const print = () => {
+  if (WSB_PDF_OUTPUT_TXT) fs.writeFileSync(`${INTAKE_DIR}/${WSB_PDF_OUTPUT}`, WSB_PDF_OUTPUT_TXT)
+  if (WSB_JSON_OUTPUT_TXT) fs.writeFileSync(`${INTAKE_DIR}/${WSB_JSON_OUTPUT}`, WSB_JSON_OUTPUT_TXT)
+  if (BS_OUTPUT_TXT) fs.writeFileSync(`${INTAKE_DIR}/${BS_OUTPUT}`, BS_OUTPUT_TXT)
+  if (AZYR_JSON_OUTPUT_TXT) fs.writeFileSync(`${INTAKE_DIR}/${AZYR_JSON_OUTPUT}`, AZYR_JSON_OUTPUT_TXT)
+}
+
+run()
+print()
+
+// So node doesn't complain
 export {}
