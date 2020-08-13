aab7b6480a35f10b30f9cbd2dd019cd36a7d6f48
Davis Ford <davis.e.ford@gmail.com>
2020-08-13 10:27:53 -0400

    Back off on the jspdf update

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,5 @@
 const { isEqual } = require('lodash')
-const FilterWarningsPlugin = require('webpack-filter-warnings-plugin')
+// const FilterWarningsPlugin = require('webpack-filter-warnings-plugin')
 
 module.exports = function override(config, env) {
   // This prevents `pdf.js` from throwing useless errors during the build process
@@ -9,11 +9,11 @@ module.exports = function override(config, env) {
   config.module.rules = updatedRules
 
   // This prevents `jspdf` from throwing useless errors during the build process
-  config.plugins.push(
-    new FilterWarningsPlugin({
-      exclude: /Critical dependency: the request of a dependency is an expression/,
-    })
-  )
+  // config.plugins.push(
+  //   new FilterWarningsPlugin({
+  //     exclude: /Critical dependency: the request of a dependency is an expression/,
+  //   })
+  // )
 
   // Why do both pdf libraries make Webpack unhappy?
   // That is a good question :)
