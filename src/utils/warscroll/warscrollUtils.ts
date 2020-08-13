2898dc0775a0a22c83889bb8bb3c8447f29fb283
Davis Ford <davis.e.ford@gmail.com>
2020-08-13 11:11:11 -0400

    Update warscrollUtils.ts

--- src/utils/warscroll/warscrollUtils.ts
+++ src/utils/warscroll/warscrollUtils.ts
@@ -27,6 +27,7 @@ export const cleanWarscrollText = (pdfText: string[]) => {
       txt
         .replace(/ \([0-9]+\)/g, '') // Remove point values e.g. "Slann Starmaster (360)"
         .replace(/[0-9]+ x /g, '') // Remove quantity from units e.g. "3 x Razordons"
+        .replace(/ x [0-9]{1,3}( \([0-9]{1,4}\))?/g, '') // Remove quantity from units e.g. "Knights of the Empty Throne Varanguard x 6 (560)"
         .trim()
     )
 }
