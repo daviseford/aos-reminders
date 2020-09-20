fbbf5fb1df18b27e1feb14a3fb4b918e9a8390c6
Davis Ford <davis.e.ford@gmail.com>
2020-09-20 17:42:19 -0400

    Update subscriptionUtils.ts

--- src/utils/subscriptionUtils.ts
+++ src/utils/subscriptionUtils.ts
@@ -1,19 +1,32 @@
 import { ISubscription } from 'types/subscription'
 
 export const isSubscriber = (subscription: ISubscription) => subscription.subscribed
+
 export const isActiveSubscriber = (subscription: ISubscription) => {
-  if (subscription.has_grant) return true
-  return (
-    isSubscriber(subscription) &&
-    !subscription.expired &&
-    subscription.subscriptionStatus !== 'pending_activation'
-  )
+  if (isPaypal(subscription)) {
+    if (subscription.subscriptionStatus === 'pending_activation') return false
+    if (hasGrant(subscription)) return true
+    return isSubscriber(subscription) && subscription.subscriptionStatus === 'active'
+  }
+
+  return isSubscriber(subscription) && !subscription.expired
 }
-export const isCanceledSubscriber = (subscription: ISubscription) =>
-  isSubscriber(subscription) && !subscription.active
-export const isGiftedSubscriber = (subscription: ISubscription) =>
-  isActiveSubscriber(subscription) && subscription.planId === 'gifted'
-export const isPendingSubscriber = (subscription: ISubscription) =>
-  subscription.subscriptionStatus === 'pending_activation'
+
+export const isCanceledSubscriber = (subscription: ISubscription) => {
+  return isSubscriber(subscription) && !subscription.active
+}
+
+export const isGiftedSubscriber = (subscription: ISubscription) => {
+  return isActiveSubscriber(subscription) && subscription.planId === 'gifted'
+}
+
+export const isPendingSubscriber = (subscription: ISubscription) => {
+  return subscription.subscriptionStatus === 'pending_activation' || hasGrant(subscription)
+}
+
+const hasGrant = (subscription: ISubscription) => {
+  return subscription.has_grant === true && subscription.subscriptionStatus === 'temporary_grant'
+}
+
 export const isStripe = (subscription: ISubscription) => subscription.createdBy === 'stripe'
 export const isPaypal = (subscription: ISubscription) => subscription.createdBy === 'paypal'
