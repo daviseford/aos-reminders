89935d80f1a9815d392f1a4e4cd565b474a88c06
Davis Ford <davis.e.ford@gmail.com>
2020-09-28 12:24:50 -0400

    Update useClickHandler.tsx

--- src/utils/hooks/useClickHandler.tsx
+++ src/utils/hooks/useClickHandler.tsx
@@ -1,7 +1,7 @@
-import { useCallback } from 'react'
-
 type TGenericFn = (e?: React.MouseEvent) => any
-type TClickHandler = <Method extends TGenericFn>(method: Method, dependencies?: any[]) => TGenericFn
+type TClickHandler = <Method extends TGenericFn>(
+  method: Method
+) => (e?: React.MouseEvent) => ReturnType<Method>
 
 /**
  * This handler automatically calls preventDefault() and stopPropagation() for you
@@ -10,17 +10,13 @@ type TClickHandler = <Method extends TGenericFn>(method: Method, dependencies?: 
  * Use this instead of writing e.preventDefault() everywhere
  * @param method
  */
-const useClickHandler: TClickHandler = (method, dependencies = []) => {
-  console.log('asadasdsad')
-  return useCallback(
-    e => {
-      e?.preventDefault?.()
-      e?.stopPropagation?.()
-      console.log('What up, clicked!')
-      return method(e)
-    },
-    [method]
-  )
+const clickEvtHandler: TClickHandler = method => {
+  return e => {
+    e?.preventDefault?.()
+    e?.stopPropagation?.()
+    console.log('What up, clicked!')
+    return method(e)
+  }
 }
 
-export default useClickHandler
+export default clickEvtHandler
