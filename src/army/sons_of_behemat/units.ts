4a1f580931c2375d360999e3e3aa06b52a5e566e
Davis Ford <daviseford@users.noreply.github.com>
2020-10-27 10:23:24 -0400

    Add Gargants

--- src/army/sons_of_behemat/units.ts
+++ src/army/sons_of_behemat/units.ts
@@ -7,6 +7,7 @@ import {
   HERO_PHASE,
   MOVEMENT_PHASE,
   SHOOTING_PHASE,
+  START_OF_CHARGE_PHASE,
   WOUND_ALLOCATION_PHASE,
 } from 'types/phases'
 
@@ -101,22 +102,22 @@ export const Units: TUnits = [
     ],
   },
   {
-    name: ``,
+    name: `Mancrusher Gargants`,
     effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Keep Up!`,
+        desc: `If this unit is within 12" of a friendly MEGA-GARGANT at the start of the charge phase, it can attempt to charge in that charge phase even if it ran in the same turn.`,
+        when: [START_OF_CHARGE_PHASE],
       },
-    ],
-  },
-  {
-    name: ``,
-    effects: [
       {
-        name: ``,
-        desc: ``,
-        when: [HERO_PHASE],
+        name: `Stomping Charge`,
+        desc: `After a model from this unit makes a charge move, pick 1 enemy unit within 1" of it and roll a dice. If the roll is equal to or greater than the Stomping Charge value for the charging model shown on the damage table above, that unit suffers D3 mortal wounds. If this unit has more than 1 model, do not allocate the mortal wounds until all of the models in this unit have made their charge moves.`,
+        when: [CHARGE_PHASE],
+      },
+      {
+        name: `Stuff 'Em In Me Bag`,
+        desc: `After a model from this unit piles in, you can pick 1 enemy model within 3" of it and roll a dice. If the roll is at least double that model's Wounds characteristic, it is slain.`,
+        when: [COMBAT_PHASE],
       },
     ],
   },
