925bd2964b107ec0fb2e27050e264f90f5ef811a
Davis Ford <davis.e.ford@gmail.com>
2020-11-28 10:35:45 -0500

    comment out

--- src/factions/seraphon/metatagger.ts
+++ src/factions/seraphon/metatagger.ts
@@ -3,13 +3,13 @@ import { omit, pick } from 'lodash'
 /**
  * Take in some data and add metadata
  */
-export const metadataTagger = obj => {
-  return Object.keys(obj).reduce((a, name) => {
-    const _copy = { ...obj[name], name }
-    a[name] = _copy
-    return a
-  }, {})
-}
+// export const metadataTagger = obj => {
+//   return Object.keys(obj).reduce((a, name) => {
+//     const _copy = { ...obj[name], name }
+//     a[name] = _copy
+//     return a
+//   }, {})
+// }
 
 export const keyOmitter = <T extends object, R extends keyof T>(obj: T, ...remove: R[]) => {
   if (!remove.length) return obj
