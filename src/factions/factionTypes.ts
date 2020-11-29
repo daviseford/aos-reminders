aaabb885a6bed9d8a8f6ed93e3c022532c5cd81e
Davis Ford <davis.e.ford@gmail.com>
2020-11-29 11:00:52 -0500

    working on adapter

--- src/factions/factionTypes.ts
+++ src/factions/factionTypes.ts
@@ -1,17 +1,16 @@
 export type TObjWithEffects = Record<string, object & { effects: unknown[] }>
 
-export type TSubFaction = Record<
-  string,
-  {
-    units?: TObjWithEffects[]
-    battalions?: TObjWithEffects[]
-    traits?: TObjWithEffects[]
-    spells?: TObjWithEffects[]
-    battle_traits?: TObjWithEffects[]
-    command_traits?: TObjWithEffects[]
-    flavors?: TObjWithEffects[]
-    artifacts?: TObjWithEffects[]
-    endless_spells?: TObjWithEffects[]
-    scenery?: TObjWithEffects[]
-  }
->
+export type TSubFaction = {
+  units?: TObjWithEffects[]
+  battalions?: TObjWithEffects[]
+  traits?: TObjWithEffects[]
+  spells?: TObjWithEffects[]
+  battle_traits?: TObjWithEffects[]
+  command_traits?: TObjWithEffects[]
+  flavors?: TObjWithEffects[]
+  artifacts?: TObjWithEffects[]
+  endless_spells?: TObjWithEffects[]
+  scenery?: TObjWithEffects[]
+}
+
+export type TSubFactions = Record<string, TSubFaction>
