74692b2bc26682591e8330d63161ec21b8cbe5c0
Davis Ford <davis.e.ford@gmail.com>
2020-11-29 13:12:40 -0500

    Blah

--- src/factions/slaanesh/index.ts
+++ src/factions/slaanesh/index.ts
@@ -4,10 +4,12 @@ import subFactions from './subfactions'
 
 export const SlaaneshFaction = {
   factionName: SLAANESH,
-  subFactions,
-  grandAlliance: CHAOS, // Should a Faction declare its grandalliance, or should it not know about that?
-  subFactionLabel: 'Hosts', // similar to AllegianceType in the old structure
 
+  subFactions,
+
+  grandAlliance: CHAOS, // Should a Faction declare its grandalliance, or should it not know about that?
+
+  subFactionLabel: 'Hosts', // similar to AllegianceType in the old structure
   flavorLabel: 'Flavor', // This will be used in place of "Flavor" in the UI eventually
 
   // Export everything (to be used for imports/ally stuff) ? Maybe... or maybe just get it through subfactions
