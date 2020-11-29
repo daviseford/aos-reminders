b43794e6813b69a5f014f0911dd6d1ddfbd71dd4
Davis Ford <davis.e.ford@gmail.com>
2020-11-29 12:00:26 -0500

    testing new slaanesh in the UI

--- src/factions/temporaryAdapter.ts
+++ src/factions/temporaryAdapter.ts
@@ -5,16 +5,18 @@ import { SlaaneshFaction } from './slaanesh'
 /**
  * To see how a new data-structure army might feel in the UI as-is
  */
-export const temporaryAdapter = (
-  newFaction: typeof SlaaneshFaction,
-  whichSubFaction: keyof typeof SlaaneshFaction['subFactions']
+export const temporaryAdapter = <F extends typeof SlaaneshFaction>(
+  newFaction: F,
+  whichSubFaction: Extract<keyof F['subFactions'], string>
 ): IInitialArmy => {
   const thisSubFactionsData = newFaction.subFactions[whichSubFaction]
 
   const Units = mergeAll(thisSubFactionsData.units || [])
+  const Allegiances = mergeAll(thisSubFactionsData.flavors || [])
 
   const initialArmy: IInitialArmy = {
     AllegianceType: newFaction.flavorLabel,
+    Allegiances,
     Units,
   }
 
@@ -26,7 +28,7 @@ type TObjWithName = TObjWithEffects & { name: string }
 const mergeAll = (objs: TParentEffectsObjWithEffects[]): TObjWithName[] => {
   return objs.reduce((a, obj) => {
     const arr = toArr(obj)
-    a.concat(arr)
+    a = a.concat(arr)
     return a
   }, [] as TObjWithName[])
 }
