1473dbc9dc78c8b40d8a7e360f41e8273148bd93
Davis Ford <davis.e.ford@gmail.com>
2020-11-29 15:58:38 -0500

    hmm

--- src/factions/seraphon/index.ts
+++ src/factions/seraphon/index.ts
@@ -1,14 +1,19 @@
-import { ORDER_GRAND_ALLIANCE } from 'meta/factions'
+import { ORDER } from 'meta/alliances'
+import { SERAPHON } from 'meta/factions'
 import subFactions from './subfactions'
 // import AllSeraphonUnits from './units'
 // import AllSeraphonBattalions from './units'
 // import AllEtc from './AllEtc' /// traits spells etc
 
 export const SeraphonFaction = {
-  factionName: 'SERAPHON',
-  grandAlliance: ORDER_GRAND_ALLIANCE,
-
+  factionName: SERAPHON,
   subFactions,
+
+  grandAlliance: ORDER,
+
+  subFactionLabel: 'Constellations', // similar to AllegianceType in the old structure
+  flavorLabel: 'Flavor', // This will be used in place of "Flavor" in the UI eventually
+
   // Export everything (to be used for imports/ally stuff)
   // units,
   // battalions,
