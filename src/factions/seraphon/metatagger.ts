f2827807570ef44c0a318d896fae33a6201f2893
Davis Ford <davis.e.ford@gmail.com>
2020-11-28 21:05:13 -0500

    Update metatagger.ts

--- src/factions/seraphon/metatagger.ts
+++ src/factions/seraphon/metatagger.ts
@@ -11,14 +11,41 @@ import { omit, pick } from 'lodash'
 //   }, {})
 // }
 
-export const keyOmitter = <T extends object, R extends Extract<keyof T, string>>(obj: T, remove: R[]) => {
-  if (!remove.length) return obj
-  return omit(obj, ...remove)
+/**
+ * Returns the `obj` with the `keys` removed.
+ *
+ * @param obj
+ * @param keys_to_omit - The keys to remove from the returned object
+ *
+ * @example
+ *  * keyOmitter({ a: 1, b: 2, c: 3 }, ['a', 'c'])
+ * // Returns { b: 2 }
+ *
+ */
+export const keyOmitter = <T extends object, R extends Extract<keyof T, string>>(
+  obj: T,
+  keys_to_omit: R[]
+) => {
+  if (!keys_to_omit.length) return obj
+  return omit(obj, ...keys_to_omit)
 }
 
-export const keyPicker = <T extends object, R extends Extract<keyof T, string>>(obj: T, select: R[]) => {
-  if (!select.length) return obj
-  return pick(obj, ...select)
+/**
+ * Returns the `obj` with **ONLY** the `keys` that you choose.
+ *
+ * @param obj
+ * @param keys_to_pick - The keys you'd like to be returned
+ *
+ * @example
+ * keyPicker({ a: 1, b: 2, c: 3 }, ['a', 'c'])
+ * // Returns { a: 1, c: 3 }
+ */
+export const keyPicker = <T extends object, R extends Extract<keyof T, string>>(
+  obj: T,
+  keys_to_pick: R[]
+) => {
+  if (!keys_to_pick.length) return obj
+  return pick(obj, ...keys_to_pick)
 }
 
 // type TObjWithEffects = Record<string, object & { effects: unknown[] }>
