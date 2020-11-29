682297d1f6587c136c643c801d6bbc9c6530058e
Davis Ford <davis.e.ford@gmail.com>
2020-11-28 20:58:51 -0500

    move to array lookup

--- src/factions/slaanesh/index.ts
+++ src/factions/slaanesh/index.ts
@@ -3,6 +3,7 @@ import subFactions from './subfactions'
 export const SlaaneshFaction = {
   factionName: 'SLAANESH',
   subFactions,
+  flavorLabel: 'Hosts?', // This will be used in place of "Flavor" in the UI eventually, similar to AllegianceType in the old data
   // Export everything (to be used for imports/ally stuff)
   // units,
   // battalions,
--- src/factions/slaanesh/subfactions.ts
+++ src/factions/slaanesh/subfactions.ts
@@ -1,6 +1,5 @@
-import BeastsofChaosFaction from 'faction/beasts_of_chaos/'
-import SlavestoDarknessFaction from 'faction/slaves_to_darkness/'
-import {} from 'types/phases'
+import { keyOmitter, keyPicker } from 'factions/seraphon/metatagger'
+import { MARK_SLAANESH } from 'meta/alliances'
 import { getChaosSlaves } from 'utils/chaosUtils'
 import { Artifacts } from './artifacts'
 import { Battalions } from './battalions'
@@ -8,12 +7,13 @@ import { BattleTraits } from './battle_traits'
 import { CommandTraits } from './command_traits'
 import { EndlessSpells } from './endless_spells'
 import Flavors from './flavors'
-import { keyOmitter, keyPicker } from './metatagger'
 import { Scenery } from './scenery'
 import { Spells } from './spells'
 import { Units } from './units'
 
-const SlaaneshS2DUnits = getChaosSlaves(MARK_SLAANESH) // Change this to return a list of string names.
+// import BeastsofChaosFaction from 'army/beasts_of_chaos/'
+// import SlavestoDarknessFaction from 'army/slaves_to_darkness/'
+const SlaaneshS2DUnits = getChaosSlaves(MARK_SLAANESH).map(x => x.name) // Change this to return a list of string names.
 const SlaaneshBoCUnits = [
   'Beastlord',
   'Bestigors',
@@ -34,29 +34,27 @@ const SlaaneshBoCUnits = [
 const subFactions = {
   INVADERS: {
     // Battle Traits
-    battletraits: keyPicker(BattleTraits, 'Thrilling Compulsions', 'Invaders Host'),
+    battletraits: keyPicker(BattleTraits, ['Thrilling Compulsions', 'Invaders Host']),
 
     // Command Traits
-    commandtraits: keyPicker(
-      CommandTraits,
+    commandtraits: keyPicker(CommandTraits, [
       'Best of the Best',
       'Glory Hog',
       'Hurler of Obscenities',
       'Territorial',
       'Skin-taker',
-      'Delusions of Infallibility'
-    ),
+      'Delusions of Infallibility',
+    ]),
 
     // Artifacts
-    artifacts: keyPicker(
-      Artifacts,
+    artifacts: keyPicker(Artifacts, [
       'The Rod of Misrule',
       'Rapier of Ecstatic Conquest',
       'Whip of Subversion',
       'Icon of Infinite Excess',
       'Fallacious Gift',
-      'The Beguiling Gem'
-    ),
+      'The Beguiling Gem',
+    ]),
 
     // Spells
     spells: Spells,
@@ -70,52 +68,49 @@ const subFactions = {
     // Units
     units: [
       Units,
-      SlaaneshS2DUnits.forEach(element => keyPicker(SlavestoDarknessFaction.Units, element)),
-      SlaaneshBoCUnits.forEach(element => keyPicker(BeastsofChaosFaction.Units, element)),
+      // keyPicker(SlavestoDarknessFaction.Units, SlaaneshS2DUnits),
+      // keyPicker(BeastsofChaosFaction.Units, SlaaneshBoCUnits),
     ],
 
     // Battalions
     battalions: [
-      keyOmitter(
-        Battalions,
+      keyOmitter(Battalions, [
         'The Vengeful Alliance',
         'Devout Supplicants',
         'Vengeful Throng',
-        'Daemonsteel Contingent'
-      ),
-      keyPicker(SlavestoDarknessFaction.Battalions, 'Pleasurebound Warband'),
-      keyPicker(BeastsofChaosFaction.Battalions, 'Depraved Drove'),
+        'Daemonsteel Contingent',
+      ]),
+      // keyPicker(SlavestoDarknessFaction.Battalions, ['Pleasurebound Warband']),
+      // keyPicker(BeastsofChaosFaction.Battalions, ['Depraved Drove']),
     ],
 
     // Flavors
-    flavors: keyPicker(Flavors, 'Lurid Haze'),
+    flavors: keyPicker(Flavors, ['Lurid Haze']),
   },
 
   PRETENDERS: {
     // Battle Traits
-    battletraits: keyPicker(BattleTraits, 'Thrilling Compulsions', 'Pretenders Host'),
+    battletraits: keyPicker(BattleTraits, ['Thrilling Compulsions', 'Pretenders Host']),
 
     // Command Traits
-    commandtraits: keyPicker(
-      CommandTraits,
+    commandtraits: keyPicker(CommandTraits, [
       'Strength of Godhood',
       'Monarch of Lies',
       'True Child of Slaanesh',
       'Strongest Alone',
       'Hunter of Godbeasts',
-      'Inspirer'
-    ),
+      'Inspirer',
+    ]),
 
     // Artifacts
-    artifacts: keyPicker(
-      Artifacts,
+    artifacts: keyPicker(Artifacts, [
       'The Crown of Dark Secrets',
       'Pendant of Slaanesh',
       'Sliverslash',
       'Sceptre of Domination',
       'Breathtaker',
-      'Mask of Spiteful Beauty'
-    ),
+      'Mask of Spiteful Beauty',
+    ]),
 
     // Spells
     spells: Spells,
@@ -129,51 +124,48 @@ const subFactions = {
     // Units
     units: [
       Units,
-      SlaaneshS2DUnits.forEach(element => keyPicker(SlavestoDarknessFaction.Units, element)),
-      SlaaneshBoCUnits.forEach(element => keyPicker(BeastsofChaosFaction.Units, element)),
+      // keyPicker(SlavestoDarknessFaction.Units, SlaaneshS2DUnits),
+      // keyPicker(BeastsofChaosFaction.Units, SlaaneshBoCUnits),
     ],
 
     // Battalions
     battalions: [
-      keyOmitter(
-        Battalions,
+      keyOmitter(Battalions, [
         'The Vengeful Alliance',
         'Devout Supplicants',
         'Vengeful Throng',
-        'Daemonsteel Contingent'
-      ),
-      keyPicker(SlavestoDarknessFaction.Battalions, 'Pleasurebound Warband'),
-      keyPicker(BeastsofChaosFaction.Battalions, 'Depraved Drove'),
+        'Daemonsteel Contingent',
+      ]),
+      // keyPicker(SlavestoDarknessFaction.Battalions, ['Pleasurebound Warband']),
+      // keyPicker(BeastsofChaosFaction.Battalions, ['Depraved Drove']),
     ],
     // Flavors
-    flavors: keyPicker(Flavors, 'Faultless Blades'),
+    flavors: keyPicker(Flavors, ['Faultless Blades']),
   },
 
   GODSEEKERS: {
     // Battle Traits
-    battletraits: keyPicker(BattleTraits, 'Thrilling Compulsions', 'Godseekers Host'),
+    battletraits: keyPicker(BattleTraits, ['Thrilling Compulsions', 'Godseekers Host']),
 
     // Command Traits
-    commandtraits: keyPicker(
-      CommandTraits,
+    commandtraits: keyPicker(CommandTraits, [
       'Hunter Supreme',
       'Thrill-seeker',
       'Into the Fray',
       'Trail-sniffer',
       'Symphoniac',
-      'Speed-chaser'
-    ),
+      'Speed-chaser',
+    ]),
 
     // Artifacts
-    artifacts: keyPicker(
-      Artifacts,
+    artifacts: keyPicker(Artifacts, [
       'Cameo of the Dark Prince',
       'Girdle of the Realm-racer',
       'Threnody Voicebox',
       'Lash of Despair',
       'Enrapturing Circlet',
-      'Bindings of Slaanesh'
-    ),
+      'Bindings of Slaanesh',
+    ]),
 
     // Spells
     spells: Spells,
@@ -187,29 +179,28 @@ const subFactions = {
     // Units
     units: [
       Units,
-      SlaaneshS2DUnits.forEach(element => keyPicker(SlavestoDarknessFaction.Units, element)),
-      SlaaneshBoCUnits.forEach(element => keyPicker(BeastsofChaosFaction.Units, element)),
+      // SlaaneshS2DUnits.forEach(element => keyPicker(SlavestoDarknessFaction.Units, element)),
+      // SlaaneshBoCUnits.forEach(element => keyPicker(BeastsofChaosFaction.Units, element)),
     ],
 
     // Battalions
     battalions: [
-      keyOmitter(
-        Battalions,
+      keyOmitter(Battalions, [
         'The Vengeful Alliance',
         'Devout Supplicants',
         'Vengeful Throng',
-        'Daemonsteel Contingent'
-      ),
-      keyPicker(SlavestoDarknessFaction.Battalions, 'Pleasurebound Warband'),
-      keyPicker(BeastsofChaosFaction.Battalions, 'Depraved Drove'),
+        'Daemonsteel Contingent',
+      ]),
+      // keyPicker(SlavestoDarknessFaction.Battalions, 'Pleasurebound Warband'),
+      // keyPicker(BeastsofChaosFaction.Battalions, 'Depraved Drove'),
     ],
     // Flavors
-    flavors: keyPicker(Flavors, 'Scarlet Cavalcade'),
+    flavors: keyPicker(Flavors, ['Scarlet Cavalcade']),
   },
 
   SYLLESSKAN: {
     // Battle Traits
-    battletraits: keyPicker(BattleTraits, 'Thrilling Compulsions', 'Syll`Esskan Host'),
+    battletraits: keyPicker(BattleTraits, ['Thrilling Compulsions', 'Syll`Esskan Host']),
 
     // Command Traits
     //commandtraits:
@@ -229,22 +220,20 @@ const subFactions = {
     // Units
     units: [
       Units,
-      SlaaneshS2DUnits.forEach(element => keyPicker(SlavestoDarknessFaction.Units, element)),
-      SlaaneshBoCUnits.forEach(element => keyPicker(BeastsofChaosFaction.Units, element)),
+      // SlaaneshS2DUnits.forEach(element => keyPicker(SlavestoDarknessFaction.Units, element)),
+      // SlaaneshBoCUnits.forEach(element => keyPicker(BeastsofChaosFaction.Units, element)),
     ],
 
     // Battalions
-    battalions: keyPicker(
-      Battalions,
+    battalions: keyPicker(Battalions, [
       'The Vengeful Alliance',
       'Devout Supplicants',
       'Vengeful Throng',
-      'Daemonsteel Contingent'
-    ),
+      'Daemonsteel Contingent',
+    ]),
     // Flavors
     //flavors:
   },
 }
 
-// subFactions.MADE_UP.units.
 export default subFactions
