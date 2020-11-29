182def02e4ad9ec3b156d6bb94c47b94b4cb200c
Davis Ford <davis.e.ford@gmail.com>
2020-11-29 10:48:30 -0500

    Update subfactions.ts

--- src/factions/seraphon/subfactions.ts
+++ src/factions/seraphon/subfactions.ts
@@ -1,4 +1,3 @@
-import { BATTLESHOCK_PHASE } from 'types/phases'
 import { keyPicker } from '../metatagger'
 import Flavors from './flavors'
 import Traits from './traits'
@@ -16,29 +15,6 @@ const subFactions = {
   STARBORNE: {
     units: [Units],
   },
-
-  // Example structure of a subfaction, imagine this repeated for traits, spells, etc
-  MADE_UP: {
-    // Let's pretend this subfaction mandates that you take a Skink Priest + Trog
-    units: keyPicker(Units, ['Skink Priest', 'Skink Oracle on Troglodon']),
-
-    // It applies these traits
-    traits: keyPicker(Traits, ['Thickly Scaled Hide', 'Cunning']),
-
-    // And it enables either of these two flavors to be selected in the UI
-    flavors: keyPicker(Flavors, ['A Different Flavor', 'Made Up Flavor']),
-
-    // General rules to be added because of this subfaction being selected
-    effects: [
-      {
-        name: `Cold-blooded`,
-        desc: `Ignore modifiers (positive or negative) to the Bravery characteristic of MADE_UP units.`,
-        when: [BATTLESHOCK_PHASE],
-      },
-      // etc
-    ],
-  },
 }
 
-// subFactions.MADE_UP.units.
 export default subFactions
