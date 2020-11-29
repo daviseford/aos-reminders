495a51c395e777d7216577f2316418b6faa15bc5
Davis Ford <davis.e.ford@gmail.com>
2020-11-28 21:10:23 -0500

    Update index.ts

--- src/factions/slaanesh/index.ts
+++ src/factions/slaanesh/index.ts
@@ -1,10 +1,13 @@
+import { CHAOS_GRAND_ALLIANCE, SLAANESH } from 'meta/factions'
 import subFactions from './subfactions'
 
 export const SlaaneshFaction = {
-  factionName: 'SLAANESH',
+  factionName: SLAANESH,
   subFactions,
+  grandAlliance: CHAOS_GRAND_ALLIANCE, // Should a Faction declare its grandalliance, or should it not know about that?
   flavorLabel: 'Hosts?', // This will be used in place of "Flavor" in the UI eventually, similar to AllegianceType in the old data
-  // Export everything (to be used for imports/ally stuff)
+
+  // Export everything (to be used for imports/ally stuff) ? Maybe... or maybe just get it through subfactions
   // units,
   // battalions,
   // etc,
