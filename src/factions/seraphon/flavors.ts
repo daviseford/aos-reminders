c770cd921702643748989495cbf136b3be9f46ea
Davis Ford <davis.e.ford@gmail.com>
2020-11-30 18:06:05 -0500

    CommandAbilities

--- src/factions/seraphon/flavors.ts
+++ src/factions/seraphon/flavors.ts
@@ -1,16 +1,14 @@
 import { pickEffects } from 'factions/metatagger'
 import {
-  CHARGE_PHASE,
   COMBAT_PHASE,
   DURING_SETUP,
   END_OF_MOVEMENT_PHASE,
-  END_OF_SHOOTING_PHASE,
-  START_OF_COMBAT_PHASE,
   TURN_FOUR_START_OF_TURN,
   TURN_ONE_MOVEMENT_PHASE,
   WOUND_ALLOCATION_PHASE,
 } from 'types/phases'
 import Artifacts from './artifacts'
+import CommandAbilities from './command_abilities'
 import CommandTraits from './command_traits'
 
 const Flavors = {
@@ -43,12 +41,7 @@ const Flavors = {
         desc: `In the first battle round, add 3" to the Move characteristic of FANGS OF SOTEK SKINK units.`,
         when: [TURN_ONE_MOVEMENT_PHASE],
       },
-      {
-        name: `Parting Shot`,
-        desc: `You can use this command ability when an enemy unit ends a charge move within 3" of a friendly FANGS OF SOTEK unit from the Skinks or Chameleon Skinks warscroll that is wholly within 18" of a friendly FANGS OF SOTEK HERO. If you do so, that unit can shoot. After you have resolved all of that unit's shooting attacks, roll a D6. On a 4+, that unit must retreat but cannot run. A unit cannot benefit from this command ability more than once per phase.`,
-        when: [CHARGE_PHASE],
-        command_ability: true,
-      },
+      ...pickEffects(CommandAbilities, ['Parting Shot']),
       ...pickEffects(CommandTraits, ['Old and Grizzled']),
       ...pickEffects(Artifacts, ['Serpent God Dagger']),
     ],
@@ -61,12 +54,7 @@ const Flavors = {
         desc: `Add 1 to hit rolls for attacks made by friendly KOATL'S CLAW SAURUS units that made a charge move in the same turn.`,
         when: [COMBAT_PHASE],
       },
-      {
-        name: `Controlled Fury`,
-        desc: `You can use this command ability at the start of the combat phase. If you do so, pick 1 friendly KOATL'S CLAW SAURUS unit wholly within 24" of a friendly KOATL'S CLAW SAURUS HERO. That unit counts as having made a charge move in that turn for the purposes of the Savagery Incarnate ability.`,
-        when: [START_OF_COMBAT_PHASE],
-        command_ability: true,
-      },
+      ...pickEffects(CommandAbilities, ['Controlled Fury']),
       ...pickEffects(CommandTraits, ['Dominant Predator']),
       ...pickEffects(Artifacts, ['Eviscerating Blade']),
     ],
@@ -79,12 +67,7 @@ const Flavors = {
         desc: `Add 2 to the Wounds characteristic of THUNDER LIZARD MONSTERS.`,
         when: [WOUND_ALLOCATION_PHASE],
       },
-      {
-        name: `Trove of Old One Technology`,
-        desc: `You can use this command ability at the end of your shooting phase. If you do so, pick 1 friendly THUNDER LIZARDBASTILADON, or friendly THUNDER LIZARDENGINE OF THE GODS, that is wholly within 18" of a friendly THUNDER LIZARD HERO. If that unit is a BASTILADON, it can shoot with its Solar Engine even if it has already done so in that phase. If that unit is an ENGINE OF THE GODS, you can make a cosmic engine roll for it even if you have already done so in that phase. A unit cannot benefit from this command ability more than once per phase.`,
-        when: [END_OF_SHOOTING_PHASE],
-        command_ability: true,
-      },
+      ...pickEffects(CommandAbilities, ['Trove of Old One Technology']),
       ...pickEffects(CommandTraits, ['Prime Warbeast']),
       ...pickEffects(Artifacts, ['Fusil of Conflagration']),
     ],
