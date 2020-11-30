3fd13c30264633b26ed8a92e7ba010a3166258ee
Davis Ford <davis.e.ford@gmail.com>
2020-11-29 19:44:09 -0500

    Updates to battalions

--- src/factions/factionTypes.ts
+++ src/factions/factionTypes.ts
@@ -3,22 +3,28 @@ import { TEffects } from 'types/data'
 export type TObjWithEffects = object & { effects: TEffects[] }
 export type TParentEffectsObjWithEffects = Record<string, TObjWithEffects>
 
+type TSubFactionKeys =
+  | 'units'
+  | 'battalions'
+  | 'spells'
+  | 'command_traits'
+  | 'command_abilities'
+  | 'flavors'
+  | 'artifacts'
+  | 'endless_spells'
+  | 'scenery'
+
 export type TSubFactionEntry = {
-  available: TParentEffectsObjWithEffects[]
-  mandatory: TParentEffectsObjWithEffects[]
+  available?: TParentEffectsObjWithEffects[]
+  mandatory?: TParentEffectsObjWithEffects[]
+  excluded?: TParentEffectsObjWithEffects[]
 }
 
 export type TSubFaction = {
-  units?: TSubFactionEntry
-  battalions?: TSubFactionEntry
-  spells?: TSubFactionEntry
+  [key in TSubFactionKeys]?: TSubFactionEntry
+} & {
   effects: TEffects[]
-  command_traits?: TSubFactionEntry
-  command_abilities?: TSubFactionEntry
-  flavors?: TSubFactionEntry
-  artifacts?: TSubFactionEntry
-  endless_spells?: TSubFactionEntry
-  scenery?: TSubFactionEntry
 }
 
 export type TSubFactions = Record<string, TSubFaction>
+export type TSubFactionsGeneric<T extends Record<string, TSubFaction>> = T
