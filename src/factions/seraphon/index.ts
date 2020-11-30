189a19b5a4fcf63d26f3b1cddce3e1675e1e00db
Davis Ford <davis.e.ford@gmail.com>
2020-11-29 20:50:38 -0500

    more

--- src/factions/seraphon/index.ts
+++ src/factions/seraphon/index.ts
@@ -1,15 +1,13 @@
-import { ORDER } from 'meta/alliances'
+import { TNewFaction } from 'factions/factionTypes'
 import { SERAPHON } from 'meta/factions'
 import subFactions from './subfactions'
 // import AllSeraphonUnits from './units'
 // import AllSeraphonBattalions from './units'
 // import AllEtc from './AllEtc' /// traits spells etc
 
-export const SeraphonFaction = {
+export const SeraphonFaction: TNewFaction = {
   factionName: SERAPHON,
   subFactions,
-
-  grandAlliance: ORDER,
 
   subFactionLabel: 'Idk yet', // similar to AllegianceType in the old structure
   flavorLabel: 'Constellations', // This will be used in place of "Flavor" in the UI eventually
