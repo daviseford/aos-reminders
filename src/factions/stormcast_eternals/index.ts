8c0280645da4ef1660306459bae4b646a8bf0fb3
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-12-02 15:16:34 +0000

    WIP starting to throw SCE together (#1077)
    * WIP starting to throw SCE together
    
    Includes a type change that could be useful for inheritance
    
    * Add commented-out keys to better illustrate inheritance
    
    Co-authored-by: Davis Ford <daviseford@users.noreply.github.com>

---
+++ src/factions/stormcast_eternals/index.ts
@@ -0,0 +1,16 @@
+import { ORDER_GRAND_ALLIANCE } from 'meta/factions'
+import subFactions from './subfactions'
+// import AllSeraphonUnits from './units'
+// import AllSeraphonBattalions from './units'
+// import AllEtc from './AllEtc' /// traits spells etc
+
+export const SeraphonFaction = {
+  factionName: 'STORMCAST_ETERNALS',
+  grandAlliance: ORDER_GRAND_ALLIANCE,
+
+  subFactions,
+  // Export everything (to be used for imports/ally stuff)
+  // units,
+  // battalions,
+  // etc,
+}
