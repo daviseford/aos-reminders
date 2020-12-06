9e54baae7fd8eb66c76ffdabe90e45bf43c1ac4f
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-12-06 03:51:00 +0000

    v4: Partial - Legions of Nagash (#1125)
    * Start adding new LoN
    
    LoN to include Grand Host, the three mortarch Legions, and Soulblight
    Battletraits so far
    
    * urgh
    
    * Dixes
    
    * Update faction_list.ts
    
    * Add template
    
    Co-authored-by: Davis Ford <davis.e.ford@gmail.com>

---
+++ src/factions/legions_of_nagash/artifacts.ts
@@ -0,0 +1,27 @@
+// import { END_OF_SETUP, WOUND_ALLOCATION_PHASE } from 'types/phases'
+//
+// const Artifacts = {
+//   'Artifact 1': {
+//     effects: [
+//       {
+//         name: `Artifact 1`,
+//         desc: `lorem ipsum`,
+//         when: [END_OF_SETUP],
+//       },
+//     ],
+//   },
+//   'Another Artifact': {
+//     effects: [
+//       {
+//         name: `Another Artifact`,
+//         desc: `lorem ipsum`,
+//         when: [WOUND_ALLOCATION_PHASE],
+//       },
+//     ],
+//   },
+// }
+//
+// export default tagAs(Artifacts, 'artifact')
+//
+
+export {}
