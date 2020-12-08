7c6cc7f8131b3175c57046d8fae8472a8c57fc0e
thundergore <52839060+thundergore@users.noreply.github.com>
2020-12-08 20:05:30 +0100

    v4-obr framework (#1138)
    * v4-obr framework
    
    v4-obr add battalions, command traits, scenery
    
    v4-obr CAs, Endless spells & some cleanup
    
    v4-obr Units & Flavours
    
    Cleanup
    
    * Fix errors :)
    
    * Fixed Mortek Shield-Corps
    
    Co-authored-by: Davis Ford <davis.e.ford@gmail.com>

---
+++ src/factions/ossiarch_bonereapers/battle_traits.ts
@@ -0,0 +1,109 @@
+import { tagAs } from 'factions/metatagger'
+import {
+  BATTLESHOCK_PHASE,
+  CHARGE_PHASE,
+  COMBAT_PHASE,
+  DURING_GAME,
+  HERO_PHASE,
+  MOVEMENT_PHASE,
+  SAVES_PHASE,
+  START_OF_COMBAT_PHASE,
+  START_OF_ROUND,
+  WOUND_ALLOCATION_PHASE,
+} from 'types/phases'
+
+const BattleTraits = {
+  // Ossiarch Bonereapers allegiance
+  'The Ossiarch Empire': {
+    effects: [
+      {
+        name: `Deathless Warriors`,
+        desc: `Roll a D6 each time you allocate a wound or mortal wound to a friendly unit that has the HEKATOS keyword, or is wholly within 6" of a friendly Mortek Hekatos, or is wholly within 12" of a friendly OSSIARCH BONEREAPERS HERO. On a 6, that wound or mortal wound is negated.`,
+        when: [WOUND_ALLOCATION_PHASE],
+      },
+      {
+        name: `Ranks Unbroken By Dissent`,
+        desc: `Do not take battleshock tests for friendly OSSIARCH BONEREAPERS units.`,
+        when: [BATTLESHOCK_PHASE],
+      },
+      {
+        name: `Ranks Unbroken By Dissent`,
+        desc: `If your army is an Ossiarch Bonereapers army, you cannot use command points. Instead, you use relentless discipline points. At the start of each battle round, before determining who has the first turn, you receive 1 relentless discipline point for each friendly OSSIARCH BONEREAPERS HERO that is on the battlefield. You receive 1 additional relentless discipline point for each warscroll battalion in your army and each friendly LIEGE that is on the battlefield, and 3 additional relentless discipline points if KATAKROS is your general and is on the battlefield. Then, roll a D6 for each friendly OSSIARCH BONEREAPERS unit on the battlefield (including the HEROES above). For each 6, you receive 1 additional relentless discipline point.
+
+         Relentless discipline points are used in the same manner as command points, but can only be used for command abilities that appear on a warscroll that has the OSSIARCH BONEREAPERS keyword, for Ossiarch Bonereaper Legion command abilities and for the Unstoppable Advance command ability below.
+
+         When you generate your relentless discipline points at the start of the battle round, any that you had left over from the previous battle round are lost.`,
+        when: [START_OF_ROUND],
+      },
+      {
+        name: `Unstoppable Advance`,
+        desc: `You can use this command ability in your movement phase. If you do so, pick 1 friendly unit that has the HEKATOS keyword, or is wholly within 6" of a friendly Mortek Hekatos, or is wholly within 12" of a friendly OSSIARCH BONEREAPERS HERO. Add 3" to that unit's Move characteristic in that phase (it can still run, or charge if it does not run). You cannot pick the same unit to benefit from this command ability more than once per phase.`,
+        when: [MOVEMENT_PHASE],
+        command_ability: true,
+      },
+    ],
+  },
+  // Mortis Praetorians
+  'Mortis Praetorians': {
+    effects: [
+      {
+        name: `The Dread Legion`,
+        desc: `Subtract 1 from the Bravery characteristic of enemy untis while they are within 12" of any friendly MORTIS PRAETORIANS units.`,
+        when: [DURING_GAME],
+      },
+    ],
+  },
+  // Petrifex Elite
+  'Petrifex Elite': {
+    effects: [
+      {
+        name: `Unstoppable Juggernauts`,
+        desc: `Reroll save rolls of 1 for attacks made with melee weapons that target PETRIFEX ELITE units.'`,
+        when: [SAVES_PHASE],
+      },
+    ],
+  },
+  // Stalliarch Lords
+  'Stalliarch Lords': {
+    effects: [
+      {
+        name: `Equumortoi`,
+        desc: `STALLIARCH LORDS units can run and still charge later in the same turn.`,
+        when: [MOVEMENT_PHASE, CHARGE_PHASE],
+      },
+    ],
+  },
+  // Ivory host
+  'Ivory Host': {
+    effects: [
+      {
+        name: `Simmering Rage`,
+        desc: `At the start of the combat phase, each friendly IVORY HOST unit within 6" of a friendly IVORY HOST model that currently has any wounds allocated to it becomes subject to rage until the end of that phase. Add 1 to hit rolls for attacks made by a unit that is subject to rage, but subtract 1 from save rolls for attacks that target a unit that is subject to rage.`,
+        when: [START_OF_COMBAT_PHASE],
+      },
+    ],
+  },
+  // Null Myriad
+  'Null Myriad': {
+    effects: [
+      {
+        name: `Eldritch Nulls`,
+        desc: `Each time a friendly NULL MYRIAD unit is affected by a spell or endless spell, you can roll a D6. If you do so, on a 5+, ignore the effects of that spell or endless spell on that unit.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  // Crematorians
+  Crematorians: {
+    effects: [
+      {
+        name: `Immolation`,
+        desc: `Roll a D6 each time a friendly CREMATORIANS model is slain by an attack made with a melee weapon, before the slain model is removed from play. Add 1 to the roll if the slain model is a HERO or MONSTER. On a 5+, pick 1 enemy unit within 3" of the slain model. That unit suffers 1 mortal wound.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
+  },
+}
+
+// Always export using tagAs
+export default tagAs(BattleTraits, 'battle_trait')
---
+++ src/factions/ossiarch_bonereapers/spells.ts
@@ -0,0 +1,146 @@
+import { tagAs } from 'factions/metatagger'
+import { HERO_PHASE, WOUND_ALLOCATION_PHASE } from 'types/phases'
+
+const Spells = {
+  'Arcane Command': {
+    effects: [
+      {
+        name: `Arcane Command`,
+        desc: `Casting value of 5. You receive D3 relentless discipline points.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  'Empower Nadirite Weapons': {
+    effects: [
+      {
+        name: `Empower Nadirite Weapons`,
+        desc: `Casting value of 5. Pick 1 friendly OSSIARCH BONEREAPERS unit with the Nadirite Weapons ability that is wholly within 24" of the caster and visible to them. Until the start of your next hero phase, that unit's Nadirite's Weapons ability causes 2 hits to be scored on an unmodified hit roll of 5+ instead of 6, or 4+ instead of 6 for a charging KAVALOS DEATHRIDERS unit attacking with Nadirite Spears.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  'Protection of Nagash': {
+    effects: [
+      {
+        name: `Protection of Nagash`,
+        desc: `Casting value of 6. Roll a D6 each time a wound or mortal wounds is allocated to the caster. On a 5+, that wound or mortal wound is negated. If any wounds or mortal wounds are allocated to the caster and not negated, and the caster is not slain, remove them from the battlefield after all wounds or mortal wounds have been allocated. Then, set them up anywhere on the battlefield more than 9" from any enemy units. After setting up this model, this spell is unbound.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  'Reinforce Battle-shields': {
+    effects: [
+      {
+        name: `Reinforce Battle-shields`,
+        desc: `Reinforce Battle-shields has a casting value of 6. Pick 1 friendly OSSIARCH BONEREAPERS unit armed wit Shields or Nadirite Battle-shields that is wholly within 24" of the caster and visible to them. Until the start of your next hero phase, roll a D6 each time you allocate a mortal wound to that unit. On a 5+ that wound is negated.`,
+        when: [HERO_PHASE],
+      },
+      {
+        name: `Reinforce Battle-shields`,
+        desc: `If active, roll a D6 each time you allocate a mortal wound to that unit. On a 5+ that wound is negated.`,
+        when: [WOUND_ALLOCATION_PHASE],
+      },
+    ],
+  },
+  'Drain Vitality': {
+    effects: [
+      {
+        name: `Drain Vitality`,
+        desc: `Casting value of 6. Pick 1 enemy unit within 18" of the caster and visible to them. Until your next hero phase, reroll unmodified hit rolls of 6 for attacks made by that unit, and reroll unmodified save rolls of 6 for attacks that target that unit.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  'Mortal Contract ': {
+    effects: [
+      {
+        name: `Mortal Contract`,
+        desc: `Casting value of 7. Pick 1 enemy unit within 18" of the caster and visible to them. For the rest of the battle, roll a D6 at the end of each phase during which any attacks made by that unit inflicted any damage on a friendly OSSIARCH BONEREAPERS unit. On a 3+, that enemy unit suffers D3 mortal wounds. You cannot pick the same unit to be affected by this spell more than once per battle.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  //Vokmortian spell
+  'Mortal Touch': {
+    effects: [
+      {
+        name: `Mortal Touch`,
+        desc: `Casting value of 8. Pick 1 enemy model within 1" of the caster that is visible to them and roll a D6. On a 5+, that model is slain. The range of this spell cannot be modified.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  //Soulmason spell
+  'Soul-guide': {
+    effects: [
+      {
+        name: `Soul-guide`,
+        desc: `Casting value of 6. Pick 1 friendly OSSIARCH BONEREAPERS unit wholly within 24" of the caster that is visible to them. You can reroll hit rolls of 1 for attacks made by that unit until your next hero phase.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  //Soulreaper spell
+  'Soul-blast': {
+    effects: [
+      {
+        name: `Soul-blast`,
+        desc: `Casting value of 7. You can either roll 1 dice for each enemy unit within 3" of the caster or roll 1 dice for 1 enemy unit within 18" of the caster that is visible to them. On a 1, nothing happens. On a 2-3, that unit suffers 1 mortal wound. On a 4+, that unit suffers D3 mortal wounds.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  // Notice how we've tucked all three spells into one selection
+  // This is a stylistic choice and you need to envision how the user will want to use this
+  'Lore of the Mortisans': {
+    effects: [
+      {
+        name: `Arcane Command`,
+        desc: `Casting value of 5. You receive D3 relentless discipline points.`,
+        when: [HERO_PHASE],
+      },
+      {
+        name: `Empower Nadirite Weapons`,
+        desc: `Casting value of 5. Pick 1 friendly OSSIARCH BONEREAPERS unit with the Nadirite Weapons ability that is wholly within 24" of the caster and visible to them. Until the start of your next hero phase, that unit's Nadirite's Weapons ability causes 2 hits to be scored on an unmodified hit roll of 5+ instead of 6, or 4+ instead of 6 for a charging KAVALOS DEATHRIDERS unit attacking with Nadirite Spears.`,
+        when: [HERO_PHASE],
+      },
+      {
+        name: `Protection of Nagash`,
+        desc: `Casting value of 6. Roll a D6 each time a wound or mortal wounds is allocated to the caster. On a 5+, that wound or mortal wound is negated. If any wounds or mortal wounds are allocated to the caster and not negated, and the caster is not slain, remove them from the battlefield after all wounds or mortal wounds have been allocated. Then, set them up anywhere on the battlefield more than 9" from any enemy units. After setting up this model, this spell is unbound.`,
+        when: [HERO_PHASE],
+      },
+      {
+        name: `Reinforce Battle-shields`,
+        desc: `Reinforce Battle-shields has a casting value of 6. Pick 1 friendly OSSIARCH BONEREAPERS unit armed wit Shields or Nadirite Battle-shields that is wholly within 24" of the caster and visible to them. Until the start of your next hero phase, roll a D6 each time you allocate a mortal wound to that unit. On a 5+ that wound is negated.`,
+        when: [HERO_PHASE],
+      },
+      {
+        name: `Reinforce Battle-shields`,
+        desc: `If active, roll a D6 each time you allocate a mortal wound to that unit. On a 5+ that wound is negated.`,
+        when: [WOUND_ALLOCATION_PHASE],
+      },
+      {
+        name: `Drain Vitality`,
+        desc: `Casting value of 6. Pick 1 enemy unit within 18" of the caster and visible to them. Until your next hero phase, reroll unmodified hit rolls of 6 for attacks made by that unit, and reroll unmodified save rolls of 6 for attacks that target that unit.`,
+        when: [HERO_PHASE],
+      },
+      {
+        name: `Mortal Contract`,
+        desc: `Casting value of 7. Pick 1 enemy unit within 18" of the caster and visible to them. For the rest of the battle, roll a D6 at the end of each phase during which any attacks made by that unit inflicted any damage on a friendly OSSIARCH BONEREAPERS unit. On a 3+, that enemy unit suffers D3 mortal wounds. You cannot pick the same unit to be affected by this spell more than once per battle.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  'Shard-storm': {
+    effects: [
+      {
+        name: `Shard-storm`,
+        desc: `Casting value of 5. Pick 1 enemy unit and roll 1 dice for each model from that unit that is within 18" of the caster and visible to them. For each 6, that unit suffers 1 mortal wound.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+}
+
+export default tagAs(Spells, 'spell')
