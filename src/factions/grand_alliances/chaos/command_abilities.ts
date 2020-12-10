8b7b1482d0fa7e16156b8fef9a47b670db73b16e
JohnSchramm <53314953+JohnSchramm@users.noreply.github.com>
2020-12-10 04:48:32 -0700

    v4 Chaos Overhaul (#1146)
    * Chaos Overhaul
    Added v4 Beasts of Chaos (Fixes #1105, Fixes #1117, Fixes #1114, Fixes #1109, Fixes #1107, Fixes #1092)
    Added v4 Legion of Azgorh
    Added v4 Legion of Chaos Ascendant subfaction (Requires Chaos GA support to fully link)
    Modified Chaos armies with imports.
    Removed old army entries and functions.
    
    * Apply suggestions from code review
    
    Co-authored-by: Michael Blatherwick <michael@michaelblatherwick.co.uk>
    
    Co-authored-by: Davis Ford <daviseford@users.noreply.github.com>
    Co-authored-by: Michael Blatherwick <michael@michaelblatherwick.co.uk>

--- src/factions/grand_alliances/chaos/command_abilities.ts
+++ src/factions/grand_alliances/chaos/command_abilities.ts
@@ -1,5 +1,5 @@
 import { tagAs } from 'factions/metatagger'
-import { BATTLESHOCK_PHASE } from 'types/phases'
+import { BATTLESHOCK_PHASE, END_OF_BATTLESHOCK_PHASE } from 'types/phases'
 
 // Store Command Abilities here. You can add them to units, abilties, flavors, and subfactions later.
 const ChaosCommandAbilities = {
@@ -9,6 +9,16 @@ const ChaosCommandAbilities = {
         name: `Lord of the Monstrous Host`,
         desc: `If the Troggoth King uses this ability, then until your next hero phase all friendly Chaos Troggoths and Chaos Ogors may use this model's Bravery characteristic instead of their own.`,
         when: [BATTLESHOCK_PHASE],
+      },
+    ],
+  },
+  // Legion of the First Prince Flavor
+  'The Shadow Legion': {
+    effects: [
+      {
+        name: `The Shadow Legion`,
+        desc: `Once per turn you can use this ability if Be'lakor is your general an on the battlefield. Roll a D6 for each friendly Legion of the First Prince Bloodletters, Plaguebearers, Daemonettes, and Horrors of Tzeentch unit on the battlefield. On a 3+ you can return D3 slain models to that unit.`,
+        when: [END_OF_BATTLESHOCK_PHASE],
       },
     ],
   },
