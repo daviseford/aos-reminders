676798e8ee41df62c31f9c9f8ebe6a911c1e4ce1
Craig <cturner573@gmail.com>
2020-12-10 19:05:53 +0100

    v4-fyreslayers added

---
+++ src/factions/fyreslayers/index.ts
@@ -0,0 +1,6 @@
+import { Faction } from 'factions/factionClass'
+import { ORDER } from 'meta/alliances'
+import { FYRESLAYERS } from 'meta/factions'
+import SubFactions from './subfactions'
+
+export const FyreslayersFaction = new Faction(FYRESLAYERS, ORDER, SubFactions, 'Subfactions', 'Flavors')
---
+++ src/factions/fyreslayers/subfactions.ts
@@ -0,0 +1,34 @@
+import { TItemDescriptions } from 'factions/factionTypes'
+import { pickEffects } from 'factions/metatagger'
+import { FYRESLAYERS } from 'meta/factions'
+import Artifacts from './artifacts'
+import Battalions from './battalions'
+import BattleTraits from './battle_traits'
+import CommandAbilities from './command_abilities'
+import CommandTraits from './command_traits'
+import EndlessSpells from './endless_spells'
+import Flavors from './flavors'
+import MountTraits from './mount_traits'
+import Prayers from './prayers'
+import Scenery from './scenery'
+import Units from './units'
+
+const subFactions: TItemDescriptions = {
+  [FYRESLAYERS]: {
+    effects: pickEffects(BattleTraits, ['Fyreslayers']),
+    available: {
+      artifacts: [Artifacts],
+      battalions: [Battalions],
+      command_abilities: [CommandAbilities],
+      command_traits: [CommandTraits],
+      endless_spells: [EndlessSpells],
+      flavors: [Flavors],
+      mount_traits: [MountTraits],
+      prayers: [Prayers],
+      scenery: [Scenery],
+      units: [Units],
+    },
+  },
+}
+
+export default subFactions
