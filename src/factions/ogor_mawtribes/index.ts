1b5f9e57633b6665f913377f35f99264f1479ba8
Craig <cturner573@gmail.com>
2020-12-10 22:10:45 +0100

    Fix Mawtribes index.ts

--- src/factions/ogor_mawtribes/index.ts
+++ src/factions/ogor_mawtribes/index.ts
@@ -1,6 +1,6 @@
 import { Faction } from 'factions/factionClass'
-import { ORDER } from 'meta/alliances'
+import { DESTRUCTION } from 'meta/alliances'
 import { OGOR_MAWTRIBES } from 'meta/factions'
 import SubFactions from './subfactions'
 
-export const OgorMawtribesFaction = new Faction(OGOR_MAWTRIBES, ORDER, SubFactions, 'Mawtribes')
+export const OgorMawtribesFaction = new Faction(OGOR_MAWTRIBES, DESTRUCTION, SubFactions, 'Mawtribes')
