e1f2a9009ba67c9981cb456ccf26447bc33f69ee
JohnSchramm <53314953+JohnSchramm@users.noreply.github.com>
2020-12-10 09:56:30 -0700

    Moved v4 Legion of Chaos Ascendant to its own faction. (#1151)

--- src/factions/grand_alliances/chaos/artifacts.ts
+++ src/factions/grand_alliances/chaos/artifacts.ts
@@ -3,7 +3,6 @@ import {
   BATTLESHOCK_PHASE,
   COMBAT_PHASE,
   DURING_GAME,
-  SAVES_PHASE,
   START_OF_COMBAT_PHASE,
   WOUND_ALLOCATION_PHASE,
 } from 'types/phases'
@@ -61,34 +60,6 @@ const ChaosArtifacts = {
         name: `Crown of Conquest (Chaos)`,
         desc: `Friendly CHAOS units do not have to take battleshock tests while they are within 6" of the bearer.`,
         when: [BATTLESHOCK_PHASE],
-      },
-    ],
-  },
-  // Legion of Chaos Ascendant
-  'Fourfold Blade': {
-    effects: [
-      {
-        name: `Fourfold Blade`,
-        desc: `If the unmodified hit roll for an attack made with this weapon is a 5+, that attack inflicts D3 mortal wounds on the target and the attack sequence ends.`,
-        when: [COMBAT_PHASE],
-      },
-    ],
-  },
-  'Armour of the Pact': {
-    effects: [
-      {
-        name: `Armour of the Pact`,
-        desc: `You can reroll save rolls for attacks made with melee weapons that target the bearer.`,
-        when: [SAVES_PHASE],
-      },
-    ],
-  },
-  'Saintskin Banner': {
-    effects: [
-      {
-        name: `Saintskin Banner`,
-        desc: `Subtract 1 from the bravery characteristic of enemy units while they are within 9" of the bearer.`,
-        when: [DURING_GAME],
       },
     ],
   },
---
+++ src/factions/legion_of_chaos_ascendant/subfactions.ts
@@ -0,0 +1,119 @@
+import { TItemDescriptions } from 'factions/factionTypes'
+import KhorneUnits from 'factions/khorne/units'
+import { keyPicker, pickEffects } from 'factions/metatagger'
+import NurgleUnits from 'factions/nurgle/units'
+import SkavenUnits from 'factions/skaventide/units'
+import SlaaneshUnits from 'factions/slaanesh/units'
+import SlavesUnits from 'factions/slaves_to_darkness/units'
+import TzeentchUnits from 'factions/tzeentch/units'
+import { LEGION_OF_CHAOS_ASCENDANT } from 'meta/factions'
+import Artifacts from './artifacts'
+import Battalions from './battalions'
+import BattleTraits from './battle_traits'
+import CommandAbilities from './command_abilities'
+import CommandTraits from './command_traits'
+import Flavors from './flavors'
+import Spells from './spells'
+
+const subFactions: TItemDescriptions = {
+  [LEGION_OF_CHAOS_ASCENDANT]: {
+    effects: pickEffects(BattleTraits, ['Unwavering Devotion']),
+
+    available: {
+      artifacts: [Artifacts],
+      battalions: [Battalions],
+      command_abilities: [CommandAbilities],
+      command_traits: [CommandTraits],
+      flavors: [Flavors],
+      spells: [Spells],
+      units: [
+        keyPicker(KhorneUnits, [
+          'Bloodcrushers',
+          'Bloodletters',
+          'Bloodmaster, Herald of Khorne',
+          'Bloodthirster of Insensate Rage',
+          'Bloodthirster of Unfettered Fury',
+          'Exalted Greater Daemon of Khorne',
+          'Flesh Hounds',
+          'Herald of Khorne on Blood Throne',
+          'Karanak',
+          'Mazarall the Butcher',
+          'Skarbrand',
+          'Skull Cannons',
+          'Skullmaster, Herald of Khorne',
+          'Skulltaker',
+          'Wrath of Khorne Bloodthirster',
+        ]),
+        keyPicker(NurgleUnits, [
+          'Beasts of Nurgle',
+          'Epidemius, Tallyman of Nurgle',
+          'Great Unclean One',
+          'Horticulous Slimux',
+          'Nurglings',
+          'Plague Drones',
+          'Plaguebearers',
+          'Poxbringer, Herald of Nurgle',
+          'Pusgoyle Blightlords',
+          'Rotigus',
+          'Sloppity Bilepiper, Herald of Nurgle',
+          'Spoilpox Scrivener, Herald of Nurgle',
+        ]),
+        keyPicker(SlaaneshUnits, [
+          'Bladebringer, Herald on Exalted Chariot',
+          'Bladebringer, Herald on Hellflayer',
+          'Bladebringer, Herald on Seeker Chariot',
+          'Daemonettes',
+          'Exalted Chariot',
+          'Fiends',
+          'Hellflayer',
+          'Infernal Enrapturess, Herald of Slaanesh',
+          'Keeper of Secrets w/ Living Whip',
+          'Keeper of Secrets w/ Ritual Knife',
+          'Keeper of Secrets w/ Shining Aegis',
+          'Keeper of Secrets w/ Sinistrous Hand',
+          'Seeker Chariots',
+          'Seekers',
+          'Shalaxi Helbane',
+          'Soulfeaster Keeper of Secrets',
+          'Syll`Esske, the Vengeful Allegiance',
+          'The Contorted Epitome',
+          'The Masque',
+          'Viceleader, Herald of Slaanesh',
+        ]),
+        keyPicker(TzeentchUnits, [
+          'Burning Chariots of Tzeentch',
+          'Changecaster, Herald of Tzeentch',
+          'Exalted Flamers of Tzeentch',
+          'Exalted Greater Demon of Tzeentch',
+          'Fateskimmer, Herald of Tzeentch on Burning Chariot',
+          'Flamers of Tzeentch',
+          'Fluxmaster, Herald of Tzeentch on Disc',
+          'Gaunt Summoner of Tzeentch',
+          'Horrors of Tzeentch',
+          'Kairos Fateweaver',
+          'Lord of Change',
+          'Screamers of Tzeentch',
+          'The Blue Scribes',
+          'The Changeling',
+        ]),
+        keyPicker(SlavesUnits, [
+          'Archaon the Everchosen',
+          'Be`Lakor',
+          'Daemon Prince',
+          'Gaunt Summoner on Disc of Tzeentch',
+          'Soul Grinder',
+        ]),
+        keyPicker(SkavenUnits, [
+          'Lord Skreech Verminking',
+          'Verminlord Corruptor',
+          'Verminlord Deceiver',
+          'Verminlord Warbringer',
+          'Verminlord Warpseer',
+          'Warpgnaw Verminlord',
+        ]),
+      ],
+    },
+  },
+}
+
+export default subFactions
