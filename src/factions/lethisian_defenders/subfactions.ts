18a4fa87423e7b9bfdc340d6049c9fff0076e683
Davis Ford <daviseford@users.noreply.github.com>
2020-12-11 10:34:07 -0500

    Update subfactions.ts

--- src/factions/lethisian_defenders/subfactions.ts
+++ src/factions/lethisian_defenders/subfactions.ts
@@ -35,21 +35,13 @@ import Units from './units'
 // Importing valid KO units.
 // const getKharadronUnits = () => KharadronOverlords.Units
 
-// Importing valid FS units.
-// All units valid except Fjul-Grimnir and Chosen Axes.
-// const getFyreslayerUnits = () => {
-//   return removeUnits(Fyreslayers.Units, [`Fjul-Grimnir`, `The Chosen Axes`])
-// }
-
-// TODO: Explain what a subfaction is (vs faction, vs flavor)
-
 const subFactions: TItemDescriptions = {
   [LETHISIAN_DEFENDERS]: {
     effects: pickEffects(BattleTraits, [LETHISIAN_DEFENDERS]),
     available: {
       allied_units: [
         keyOmitter(FyreslayersUnits, ['Fjul-Grimnir', 'The Chosen Axes']),
-        //TODO
+        // TODO:
         // ...getIdonethUnits(),
         // ...getKharadronUnits(),
         // ...getStormcastUnits(),
