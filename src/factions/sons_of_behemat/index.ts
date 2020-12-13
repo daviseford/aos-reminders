1b3b680f89e98b8b8ca51569b2a0a40050d25d58
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-12-13 17:56:56 +0000

    Start sketching out source annotation

--- src/factions/sons_of_behemat/index.ts
+++ src/factions/sons_of_behemat/index.ts
@@ -1,6 +1,9 @@
 import { Faction } from 'factions/factionClass'
 import { DESTRUCTION } from 'meta/alliances'
 import { SONS_OF_BEHEMAT } from 'meta/factions'
+import { BATTLETOME_SONS_OF_BEHEMAT } from 'meta/sources'
 import SubFactions from './subfactions'
 
-export const SonsOfBehematFaction = new Faction(SONS_OF_BEHEMAT, DESTRUCTION, SubFactions, 'Tribes')
+export const SonsOfBehematFaction = new Faction(SONS_OF_BEHEMAT, DESTRUCTION, SubFactions, 'Tribes', [
+  BATTLETOME_SONS_OF_BEHEMAT,
+])
---
+++ src/meta/sources.ts
@@ -0,0 +1,2 @@
+export const BATTLETOME_SONS_OF_BEHEMAT = 'Battletome: Sons of Behemat'
+export const BATTLETOME_SONS_OF_BEHEMAT_NOV_2020 = 'Battletome: Sons of Behemat Errata November 2020'
