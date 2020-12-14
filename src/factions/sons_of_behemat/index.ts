88e5a32545a9da7bf4700a2d1fc302333140b6d8
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-12-14 21:37:45 +0000

    Sons of Behemat belong in Destruction

--- src/factions/sons_of_behemat/index.ts
+++ src/factions/sons_of_behemat/index.ts
@@ -1,6 +1,6 @@
 import { Faction } from 'factions/factionClass'
-import { CHAOS } from 'meta/alliances'
+import { DESTRUCTION } from 'meta/alliances'
 import { SONS_OF_BEHEMAT } from 'meta/factions'
 import SubFactions from './subfactions'
 
-export const SonsOfBehematFaction = new Faction(SONS_OF_BEHEMAT, CHAOS, SubFactions, 'Tribes')
+export const SonsOfBehematFaction = new Faction(SONS_OF_BEHEMAT, DESTRUCTION, SubFactions, 'Tribes')
