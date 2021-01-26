4649b3006837d9d8ea30703ed94b67541b32e323
wspencermiller <wspencer.miller@gmail.com>
2021-01-26 14:27:38 -0600

    Fixes #1205 - Grimscuttle Addition (#1207)
    * Adding Grimscuttle
    
    Co-authored-by: Davis Ford <daviseford@users.noreply.github.com>

--- src/factions/gloomspite_gitz/flavors.ts
+++ src/factions/gloomspite_gitz/flavors.ts
@@ -1,5 +1,5 @@
 import { keyPicker } from 'factions/metatagger'
-import { HERO_PHASE, MOVEMENT_PHASE } from 'types/phases'
+import { COMBAT_PHASE, DURING_GAME, HERO_PHASE, MOVEMENT_PHASE } from 'types/phases'
 import Artifacts from './artifacts'
 import Battlalions from './battalions'
 import CommandAbilities from './command_abilities'
@@ -38,6 +38,35 @@ const Flavors = {
         name: `Monstrous Regeneration`,
         desc: `Add 1 to the dice that determines if a friendly GLOGG'S MEGAMOB TROGGOTH unit heals any wounds when it uses its Regeneration ability.`,
         when: [HERO_PHASE],
+      },
+    ],
+  },
+
+  'Grimscuttle Tribes': {
+    mandatory: {
+      artifacts: [keyPicker(Artifacts, ['Shyishan Spider-sigils'])],
+      command_abilities: [keyPicker(CommandAbilities, ['Masters of Feigned Flight'])],
+      command_traits: [keyPicker(CommandTraits, ['Prophet of da Spider God'])],
+    },
+    available: {
+      batttalions: [
+        keyPicker(Battlalions, [
+          'Grimscuttle Spider Cluster',
+          'Grimscuttle Skitterswarm',
+          'Grimscuttle Nest',
+        ]),
+      ],
+    },
+    effects: [
+      {
+        name: `Deff Grotz of Shyish`,
+        desc: `Each time a friendly GRIMSCUTTLE SPIDERFANG unit is affected by a spell or endless spell, you can roll a dice. If you do so, on a 5+, ignore the effects of that spell or endless spell on that unit.`,
+        when: [DURING_GAME],
+      },
+      {
+        name: `Drawn to the Aetherglow`,
+        desc: `You can reroll hit rolls for attacks made with melee weapons by friendly SKITTERSTRAND models if the target is a Wizard of Priest.`,
+        when: [COMBAT_PHASE],
       },
     ],
   },
--- src/factions/gloomspite_gitz/scenery.ts
+++ src/factions/gloomspite_gitz/scenery.ts
@@ -10,7 +10,7 @@ const Scenery = {
         when: [START_OF_SETUP],
       },
       {
-        name: `Swarms of Lair Lurkers`,
+        name: `Swarms of Lair Lurkers (SQUIG general)`,
         desc: `If your general has the SQUIG keyword, after you set up this terrain feature, you can replace its Moonclan Lairs ability with:
 
         'At the end of each of your turns, you can pick 1 friendly SQUIG HERD, SQUIG HOPPERS, or BOINGROT BOUNDERZ unit that has been destroyed. If you do so, roll a D6. On a 4+, a new replacement unit with half of the models from the unit that was destroyed (rounding fractions up) is added to your army. You must set up the replacement unit wholly within 12" of a friendly BAD MOON LOONSHRINE and more than 3" from any enemy units. Each destroyed unit can only be replaced once - replacement units cannot themselves be replaced.'`,
@@ -22,14 +22,23 @@ const Scenery = {
         when: [BATTLESHOCK_PHASE],
       },
       {
-        name: `Moonclan Lairs (Non-TROGGOTH general)`,
+        name: `Moonclan Lairs`,
         desc: `At the end of each of your turns, you can pick 1 friendly STABBAS or SHOOTAS unit that has been destroyed. If you do so, roll a D6. On a 4+ a new replacement unit with half of the models from the unit that was destroyed (rounding fractions up) is added to your army. You must set up the replacement unit wholly within 12" of a friendly BAD MOON LOONSHRINE, and more than 3" from any enemy units. Each destroyed unit can only be replaced once - replacement units cannot themselves be replaced.`,
         when: [END_OF_TURN],
       },
       {
         name: `Hidden Troggholes (TROGGOTH general)`,
-        desc: `At the end of each of your turns, yo can pick 1 friendly FELLWATER TROGGOTH or ROCKGUT TROGGOTH unit that has been destroyed. If you do so, roll a D6. On a 4+, a new replacement unit with half of the models from the unit that was destroyed (rounding fractions up) is added to your army. You must set up the replacement unit wholly within 12" of a friendly BAD MOON LOONSHRINE and more than 3" from any enemy units. Each destroyed unit can only be replaced once - replacement units cannot themselves be replaced.`,
-        when: [END_OF_TURN],
+        desc: `If your general has the TROGGOTH keyword, after you set up this terrain feature, you can replace its Moonclan Lairs ability with: 
+        
+        'At the end of each of your turns, you can pick 1 friendly FELLWATER TROGGOTH or ROCKGUT TROGGOTH unit that has been destroyed. If you do so, roll a D6. On a 4+, a new replacement unit with half of the models from the unit that was destroyed (rounding fractions up) is added to your army. You must set up the replacement unit wholly within 12" of a friendly BAD MOON LOONSHRINE and more than 3" from any enemy units. Each destroyed unit can only be replaced once - replacement units cannot themselves be replaced.`,
+        when: [START_OF_SETUP, END_OF_TURN],
+      },
+      {
+        name: `Endless Skitterswarsm (SPIDERFANG general)`,
+        desc: `If your general has the SPIDERFANG keyword, after you set up this terrain feature, you can replace its Moonclan Lairs ability with: 
+        
+        'At the end of each of your turns, you can pick 1 friendly SPIDER RIDERS unit that has been destroyed. If you do so, roll a dice. On a 4+, a new replacement unit with half of the models from the unit that was destroyed (rounding fractions up) is added to your army. You must set up the replacement unit wholly within 12" of a friendly BAD MOON LOONSHRINE and more than 3" from any enemy units. Each destroyed unit can only be replaced once - replacement units cannot themselves be replaced.`,
+        when: [START_OF_SETUP, END_OF_TURN],
       },
     ],
   },
