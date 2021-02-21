2eb9a4e080bf3f14284b9812330a86ded598e0b3
Davis Ford <davis.e.ford@gmail.com>
2021-02-21 13:41:24 -0500

    Better FAQ style

--- src/components/routes/Faq.tsx
+++ src/components/routes/Faq.tsx
@@ -3,7 +3,6 @@ import Footer from 'components/page/footer'
 import { useTheme } from 'context/useTheme'
 import React, { lazy, Suspense, useEffect } from 'react'
 import { logPageView } from 'utils/analytics'
-import useWindowSize from 'utils/hooks/useWindowSize'
 
 const Navbar = lazy(() => import('components/page/navbar'))
 
@@ -25,8 +24,18 @@ const Faq = () => {
       <PageHeader />
 
       <div className={`container ${theme.bgColor} ${theme.text}`}>
-        <LoginFaq />
-        <UnsubscribeFaq />
+        <div className={`row`}>
+          <FaqEntry
+            title="I can't recover my password!"
+            text={`If you're attempting to recover your password, and you're not seeing a recovery email - please try clicking "Continue with Google" when you see the log in.`}
+            imgUrl={'/img/faq_continue_with_google.png'}
+          />
+          <FaqEntry
+            title="How do I unsubscribe?"
+            text={`Log in and then visit your Profile. From there, please click "Cancel Subscription"`}
+            imgUrl={'/img/faq_unsubscribe.png'}
+          />
+        </div>
       </div>
 
       <Footer />
@@ -34,44 +43,29 @@ const Faq = () => {
   )
 }
 
-const LoginFaq = () => {
-  return (
-    <>
-      <FaqEntry
-        title="I can't recover my password!"
-        text={`If you're attempting to recover your password, and you're not seeing a recovery email - please try clicking "Continue with Google" when you see the log in.`}
-        imgUrl={'/img/faq_continue_with_google.png'}
-      />
-    </>
-  )
-}
+type TFaqEntryProps = { title: string; text: string; imgUrl?: string }
 
-const UnsubscribeFaq = () => {
+const FaqEntry = ({ title, text, imgUrl = '' }: TFaqEntryProps) => {
   return (
-    <>
-      <FaqEntry
-        title="How do I unsubscribe?"
-        text={`Log in and then visit your Profile. From there, please click "Cancel Subscription"`}
-      />
-    </>
-  )
-}
-
-const FaqEntry = ({ title, text, imgUrl = '' }: { title: string; text: string; imgUrl?: string }) => {
-  const { theme } = useTheme()
-  const { isMobile } = useWindowSize()
-  const textColClass = imgUrl ? (isMobile ? 'col-12' : 'col-6') : 'col'
-  return (
-    <div className={`row ${theme.bgColor} ${theme.text} mt-3`}>
-      <div className={textColClass}>
-        <h5>{title}</h5>
-        <p>{text}</p>
-      </div>
-      {imgUrl && (
-        <div className={isMobile ? 'col-12' : 'col-6'}>
-          <img className={`d-block mx-auto mb-4 img-fluid bg-white`} src={imgUrl} alt="" />
+    <div className={'col-12 col-md-8 col-lg-6 col-xl-5 mx-xl-1'}>
+      <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div className="col p-4 d-flex flex-column position-static">
+          <h3 className="mb-0">{title}</h3>
+          <p className="card-text mb-auto">{text}</p>
         </div>
-      )}
+        {imgUrl && (
+          <div className="col-auto d-none d-block d-sm-block d-md-block d-lg-block align-self-center">
+            <img
+              className={`mx-auto mb-4 img-fluid bg-white`}
+              src={imgUrl}
+              alt=""
+              width="200"
+              height="250"
+              role="img"
+            />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
@@ -81,6 +75,7 @@ const PageHeader = () => {
   return (
     <div className={`container ${theme.bgColor} ${theme.text} text-center mt-3 pb-2`}>
       <h2>Frequently Asked Questions</h2>
+      <hr />
     </div>
   )
 }
