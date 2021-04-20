fc9c09d6ff2dfeb5a733374a46cec69a448047f1
Davis Ford <davis.e.ford@gmail.com>
2021-04-19 22:16:19 -0400

    Update getTag.ts

--- src/utils/getTag.ts
+++ src/utils/getTag.ts
@@ -1,6 +1,20 @@
 import { entryKeyToSelectionsKey, TEntry } from 'types/data'
 
-export const getTagFromEntry = (entry: TEntry) => {
+/**
+ * This utility helps extract a given tag from an entry
+ *
+ * Useful for figuring out if something is a command_ability, spell, etc.
+ *
+ * @example
+ *
+ * const tag = getTagFromEntry({ name: 'A Name', effects: [], spell: true })
+ * // returns "spell"
+ *
+ *
+ * @param entry
+ * @returns string
+ */
+export const getTagFromEntry = (entry: TEntry): string => {
   let tag = ''
   Object.keys(entryKeyToSelectionsKey).forEach(k => {
     if (tag) return
