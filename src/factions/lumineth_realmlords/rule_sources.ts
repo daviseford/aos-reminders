f9a90af8a0dbdd77d04a948f4a085c3c0de714d9
Davis Ford <davis.e.ford@gmail.com>
2021-05-10 20:54:10 -0400

    Add rule_sources and FAQs

---
+++ src/factions/lumineth_realmlords/rule_sources.ts
@@ -0,0 +1,15 @@
+const rule_sources = {
+  BATTLETOME_LUMINETH: {
+    name: 'Battletome: Lumineth Realm-Lords',
+    publication_date: '29-03-21',
+    type: 'battletome',
+  },
+  ERRATA_LUMINETH_MARCH_2021: {
+    name: 'Errata (May 2021)',
+    publication_date: '10-05-21',
+    type: 'errata',
+    url: 'https://www.warhammer-community.com/wp-content/uploads/2020/08/mWMEOpCm7uWKs6FN.pdf',
+  },
+}
+
+export default rule_sources
