e23b6694a7ed391b5880345def9792faaf1a1fc2
Davis Ford <daviseford@users.noreply.github.com>
2021-05-24 16:28:03 -0400

    Update sample army

---
+++ src/factions/sample/rule_sources.ts
@@ -0,0 +1,15 @@
+const rule_sources = {
+  BATTLETOME_SAMPLE_ARMY: {
+    name: 'Battletome: Sample Army',
+    publication_date: '20-10-17',
+    type: 'battletome',
+  },
+  ERRATA_SAMPLE_ARMY_NOV_2020: {
+    name: 'Errata (November 2020)',
+    publication_date: '20-11-18',
+    type: 'errata',
+    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  },
+}
+
+export default rule_sources
