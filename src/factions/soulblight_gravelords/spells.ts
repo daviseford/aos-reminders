14eb5a94f4aa64a6c7f1c602bd3d89e4693b5b91
Davis Ford <daviseford@users.noreply.github.com>
2021-05-25 08:44:56 -0400

    Update spells.ts

--- src/factions/soulblight_gravelords/spells.ts
+++ src/factions/soulblight_gravelords/spells.ts
@@ -1,6 +1,73 @@
 import { tagAs } from 'factions/metatagger'
+import { HERO_PHASE } from 'types/phases'
 
 const Spells = {
+  // Every Soulblight hero knows this spell!
+  'Invigorating Aura': {
+    effects: [
+      {
+        name: `Invigorating Aura`,
+        desc: `Casting value of 8. Add 1 to the roll for each friendly SOULBLIGHT GRAVELORDS HERO on the battlefield. If successfully cast, pick 1 friendly SOULBLIGHT GRAVELORDS SUMMONABLE unit wholly within 18" of the caster. You can either heal up to 3 wounds allocated to that unit or, if no wounds are allocated to it, you can return a number of slain models to that unit that have a combined Wounds characteristic of 3 or less. The same unit cannot benefit from this spell more than once per turn.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+
+  // Lore of the Vampires
+  'Blades of Shyish': {
+    effects: [
+      {
+        name: `Blades of Shyish`,
+        desc: `Casting value of 5. If successfully cast, roll a dice for each enemy unit within 12" of the caster. On a 3+, that unit suffers 1 mortal wound.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  'Spirit Gale': {
+    effects: [
+      {
+        name: `Spirit Gale`,
+        desc: `Casting value of 5. If successfully cast, pick 1 enemy unit within 18" of the caster that is visible to them and roll 2D6. If the roll is greater than that unit's Bravery characteristic, that unit suffers a number of mortal wounds equal to the difference between its Bravery characteristic and the roll.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  Soulpike: {
+    effects: [
+      {
+        name: `Soulpike`,
+        desc: `Casting value of 6. If successfully cast, pick 1 enemy unit within 18" of the caster that is visible to them. Until your next hero phase, if that unit makes a charge move, roll a number of dice equal to the charge roll for that unit. For each 4+, that unit suffers 1 mortal wound. The same unit cannot be affected by this spell more than once per turn.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  'Amethystine Pinions': {
+    effects: [
+      {
+        name: `Amethystine Pinions`,
+        desc: `Casting value of 5. If successfully cast, until your next hero phase, add 6" to the caster's Move characteristic. The same unit cannot benefit from this spell more than once per turn.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  'Vile Transference': {
+    effects: [
+      {
+        name: `Vile Transference`,
+        desc: `Casting value of 4. If successfully cast, pick 1 enemy unit within 6" of the caster that is visible to them. Roll a number of dice equal to half of that enemy unit's Wounds characteristic (rounding up). For each 6, you can heal 1 wound allocated to the caster.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
+  'Amaranthine Orb': {
+    effects: [
+      {
+        name: `Amaranthine Orb`,
+        desc: `Casting value of 6. If successfully cast, pick 1 point on the battlefield within 9" of the caster that is visible to them and draw an imaginary straight line 1mm wide between that point and the closest part of the caster's base. Roll a dice for each unit that has models passed across by this line. On a 2+, that unit suffers D3 mortal wounds. DEATH units are not affected by this spell.`,
+        when: [HERO_PHASE],
+      },
+    ],
+  },
   // '': {
   //   effects: [
   //     {
