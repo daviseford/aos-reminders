afa08c34f1cb72d96383a01f0ef1f30890f933aa
Davis Ford <daviseford@users.noreply.github.com>
2021-07-04 13:58:45 -0400

    Update app banner

--- src/components/info/banners/app_banner.tsx
+++ src/components/info/banners/app_banner.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const AppBanner = () => {
   const { isDark } = useTheme()
-  const name = 'aos-3-core-rules-callout'
+  const name = 'aos-3-core-rules-update'
 
   return (
     <NotificationBanner
@@ -15,7 +15,13 @@ const AppBanner = () => {
       variant={isDark ? `info` : `info`}
     >
       <span>
-        <strong>We are up to date with all of the AoS 3.0 July FAQs!</strong>
+        <strong>We are fully up to date with AoS 3.0!</strong>
+
+        <li>Added Core Rules 2021</li>
+        <li>Added General's Handbook 2021</li>
+        <li>
+          Added <strong>all</strong> of the July 2021 FAQs! <em>(phew)</em>
+        </li>
       </span>
     </NotificationBanner>
   )
