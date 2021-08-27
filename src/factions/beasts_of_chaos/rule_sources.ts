7f698ed7d41ec2e695e20669c1770348fb67e4db
Davis Ford <daviseford@users.noreply.github.com>
2021-08-27 09:06:30 -0400

    BoC

--- src/factions/beasts_of_chaos/rule_sources.ts
+++ src/factions/beasts_of_chaos/rule_sources.ts
@@ -8,6 +8,11 @@ const rule_sources = {
     type: 'errata',
     url: 'https://www.warhammer-community.com/wp-content/uploads/2018/10/mDYcmmF6EAUzUO6e.pdf',
   },
+  ERRATA_BEASTS_OF_CHAOS_AUGUST_2021: {
+    name: 'Errata (August 2021)',
+    type: 'errata',
+    url: 'https://www.warhammer-community.com/wp-content/uploads/2018/10/jUJe1sqnaUJm1RyN.pdf',
+  },
 }
 
 export default rule_sources
