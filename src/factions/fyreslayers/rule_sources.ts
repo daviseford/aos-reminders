ca205f2075b9e762031c14bab482378e2c5576e4
Davis Ford <daviseford@users.noreply.github.com>
2021-08-27 09:23:48 -0400

    Fyreslayers

--- src/factions/fyreslayers/rule_sources.ts
+++ src/factions/fyreslayers/rule_sources.ts
@@ -8,6 +8,11 @@ const rule_sources = {
     type: 'errata',
     url: 'https://www.warhammer-community.com/wp-content/uploads/2019/07/agEKwFyENg7bVmOO.pdf',
   },
+  ERRATA_FYRESLAYERS_AUGUST_2021: {
+    name: 'Errata (August 2021)',
+    type: 'errata',
+    url: 'https://www.warhammer-community.com/wp-content/uploads/2019/07/osmvxXg6iy9AM3vU.pdf',
+  },
 }
 
 export default rule_sources
