a994826d09fc9b049ed3cd81722ba1d400c90809
Davis Ford <daviseford@users.noreply.github.com>
2021-09-19 11:56:25 -0400

    Add prayers

--- src/factions/fyreslayers/prayers.ts
+++ src/factions/fyreslayers/prayers.ts
@@ -14,7 +14,7 @@ const Prayers = {
     effects: [
       {
         name: `Searing Heat`,
-        desc: `Searing Heat is a prayer that has an answer value of 3 and a range of 18". If answered, pick 1 enemy unit within range and visible to the chanter. Subtract 1 from hit rolls for attacks made by that unit until the start of your next hero phase.`,
+        desc: `Answer value of 3 and a range of 18". If answered, pick 1 enemy unit within range and visible to the chanter. Subtract 1 from hit rolls for attacks made by that unit until the start of your next hero phase.`,
         when: [HERO_PHASE],
         rule_sources: [rule_sources.BATTLETOME_FYRESLAYERS, rule_sources.ERRATA_FYRESLAYERS_JULY_2021],
       },
@@ -30,7 +30,7 @@ const Prayers = {
     effects: [
       {
         name: `Prayer of Ash`,
-        desc: `Prayer of Ash is a prayer that has an answer value of 4 and a range of 18". If answered, pick 1 friendly FYRESLAYERS unit wholly within range and visible to the chanter. Add 1 to save rolls for attacks that target that unit until the start of your next hero phase.`,
+        desc: `Answer value of 4 and a range of 18". If answered, pick 1 friendly FYRESLAYERS unit wholly within range and visible to the chanter. Add 1 to save rolls for attacks that target that unit until the start of your next hero phase.`,
         when: [HERO_PHASE],
         rule_sources: [rule_sources.BATTLETOME_FYRESLAYERS, rule_sources.ERRATA_FYRESLAYERS_JULY_2021],
       },
@@ -46,7 +46,7 @@ const Prayers = {
     effects: [
       {
         name: `Ember Storm`,
-        desc: `Ember Storm is a prayer that has an answer value of 3 and a range of 18". If answered, pick 1 friendly VULKITE BERZERKERS or HEARTHGUARD BERZERKERS unit wholly within range and visible to the chanter. That unit can run and still charge later in that turn.`,
+        desc: `Answer value of 3 and a range of 18". If answered, pick 1 friendly VULKITE BERZERKERS or HEARTHGUARD BERZERKERS unit wholly within range and visible to the chanter. That unit can run and still charge later in that turn.`,
         when: [HERO_PHASE],
         rule_sources: [rule_sources.BATTLETOME_FYRESLAYERS, rule_sources.ERRATA_FYRESLAYERS_JULY_2021],
       },
@@ -62,7 +62,7 @@ const Prayers = {
     effects: [
       {
         name: `Prayer of Grimnir's Fury`,
-        desc: `Prayer of Grimnir's Fury is a prayer that has an answer value of 3 and a range of 12". If answered, pick 1 friendly FYRESLAYERS HERO that does not have a mount, is within range and visible to the chanter, and is within 3" of an enemy unit. That HERO can fight.`,
+        desc: `Answer value of 3 and a range of 12". If answered, pick 1 friendly FYRESLAYERS HERO that does not have a mount, is within range and visible to the chanter, and is within 3" of an enemy unit. That HERO can fight.`,
         when: [HERO_PHASE],
         rule_sources: [rule_sources.BATTLETOME_FYRESLAYERS, rule_sources.ERRATA_FYRESLAYERS_JULY_2021],
       },
@@ -72,7 +72,7 @@ const Prayers = {
     effects: [
       {
         name: `Gilded Claws`,
-        desc: `Gilded Claws is a prayer that has an answer value of 3 and a range of 12". If answered, pick 1 friendly Magmadroth unit within range and visible to the chanter. Add 1 to wound rolls for attacks made by that unit with its Claws and Horns until the start of your next hero phase.`,
+        desc: `Answer value of 3 and a range of 12". If answered, pick 1 friendly Magmadroth unit within range and visible to the chanter. Add 1 to wound rolls for attacks made by that unit with its Claws and Horns until the start of your next hero phase.`,
         when: [HERO_PHASE],
         rule_sources: [rule_sources.BATTLETOME_FYRESLAYERS, rule_sources.ERRATA_FYRESLAYERS_JULY_2021],
       },
@@ -89,7 +89,7 @@ const Prayers = {
     effects: [
       {
         name: `Volcano's Call`,
-        desc: `Volcano's Call is a prayer that has an answer value of 3 and a range of 18". If answered, pick 1 terrain feature wholly within range and visible to the chanter. Roll a dice for each model within 1" of that terrain feature. For each 6, that model's unit suffers 1 mortal wound.`,
+        desc: `Answer value of 3 and a range of 18". If answered, pick 1 terrain feature wholly within range and visible to the chanter. Roll a dice for each model within 1" of that terrain feature. For each 6, that model's unit suffers 1 mortal wound.`,
         when: [HERO_PHASE],
         rule_sources: [rule_sources.BATTLETOME_FYRESLAYERS, rule_sources.ERRATA_FYRESLAYERS_JULY_2021],
       },
@@ -99,7 +99,7 @@ const Prayers = {
     effects: [
       {
         name: `Runic Empowerment`,
-        desc: `Runic Empowerment is a prayer that has an answer value of 3 and a range of 12". If the chanter carries a Forge Key, this prayer has a range of 18" instead. If answered, pick 1 friendly FYRESLAYERS unit wholly within range and visible to the chanter. Add 1 to wound rolls for attacks made by that unit until the start of your next hero phase.`,
+        desc: `Answer value of 3 and a range of 12". If the chanter carries a Forge Key, this prayer has a range of 18" instead. If answered, pick 1 friendly FYRESLAYERS unit wholly within range and visible to the chanter. Add 1 to wound rolls for attacks made by that unit until the start of your next hero phase.`,
         when: [HERO_PHASE],
         rule_sources: [rule_sources.BATTLETOME_FYRESLAYERS, rule_sources.ERRATA_FYRESLAYERS_JULY_2021],
       },
--- src/factions/stormcast_eternals/prayers.ts
+++ src/factions/stormcast_eternals/prayers.ts
@@ -1,49 +1,13 @@
 import { tagAs } from 'factions/metatagger'
-import { COMBAT_PHASE, HERO_PHASE, SHOOTING_PHASE } from 'types/phases'
-import rule_sources from './rule_sources'
+import { HERO_PHASE } from 'types/phases'
 
 const Prayers = {
   'Divine Light': {
     effects: [
       {
         name: `Divine Light`,
-        desc: `Divine Light is a prayer that has an answer value of 3 and a range of 12" if the chanter is a KNIGHT or 18" if the chanter is a LORD. If answered, pick 1 enemy unit within range and visible to the chanter. You can reroll hit rolls of 1 for attacks that target that unit until your next hero phase.`,
+        desc: `Answer value of 3 and a range of 12" if the chanter is a KNIGHT or 18" if the chanter is a LORD. If answered, pick 1 enemy unit within range and visible to the chanter. You can reroll hit rolls of 1 for attacks that target that unit until your next hero phase.`,
         when: [HERO_PHASE],
-        rule_sources: [
-          rule_sources.BATTLETOME_STORMCAST_ETERNALS,
-          rule_sources.ERRATA_STORMCAST_ETERNALS_JULY_2021,
-        ],
-      },
-      {
-        name: `Divine Light`,
-        desc: `If active, you can reroll hit rolls of 1 for attacks that target that unit until your next hero phase.`,
-        when: [SHOOTING_PHASE, COMBAT_PHASE],
-        rule_sources: [
-          rule_sources.BATTLETOME_STORMCAST_ETERNALS,
-          rule_sources.ERRATA_STORMCAST_ETERNALS_JULY_2021,
-        ],
-      },
-    ],
-  },
-  'Bless Weapons': {
-    effects: [
-      {
-        name: `Bless Weapons`,
-        desc: `Bless Weapons is a prayer that has an answer value of 3 and a range of 12" if the chanter is a KNIGHT or 18" if the chanter is a LORD. If answered, pick 1 friendly STORMCAST ETERNALS unit wholly within range and visible to the chanter. Until your next hero phase, if the unmodified hit roll for an attack made by that unit is 6, that attack scores 2 hits on the target instead of 1. Make a wound and save roll for each hit.`,
-        when: [HERO_PHASE],
-        rule_sources: [
-          rule_sources.BATTLETOME_STORMCAST_ETERNALS,
-          rule_sources.ERRATA_STORMCAST_ETERNALS_JULY_2021,
-        ],
-      },
-      {
-        name: `Bless Weapons`,
-        desc: `If active, until your next hero phase, if the unmodified hit roll for an attack made by that unit is 6, that attack scores 2 hits on the target instead of 1. Make a wound and save roll for each hit.`,
-        when: [SHOOTING_PHASE, COMBAT_PHASE],
-        rule_sources: [
-          rule_sources.BATTLETOME_STORMCAST_ETERNALS,
-          rule_sources.ERRATA_STORMCAST_ETERNALS_JULY_2021,
-        ],
       },
     ],
   },
@@ -51,12 +15,17 @@ const Prayers = {
     effects: [
       {
         name: `Translocation`,
-        desc: `Translocation is a prayer that has an answer value of 3 and a range of 9". If answered, pick 1 friendly STORMCAST ETERNALS unit wholly within range and visible to the chanter. You can remove that unit from the battlefield and set it up again anywhere on the battlefield more than 9" from all enemy units.`,
+        desc: `Answer value of 3 and a range of 9" if the chanter is a KNIGHT or 12" if the chanter is a LORD, If answered, pick 1 friendly STORMCAST ETERNALS unit wholly within range and visible to the chanter. You can remove that unit from the battlefield and set it up again anywhere on the battlefield more than 9" from all enemy units.`,
         when: [HERO_PHASE],
-        rule_sources: [
-          rule_sources.BATTLETOME_STORMCAST_ETERNALS,
-          rule_sources.ERRATA_STORMCAST_ETERNALS_JULY_2021,
-        ],
+      },
+    ],
+  },
+  'Bless Weapons': {
+    effects: [
+      {
+        name: `Bless Weapons`,
+        desc: `Answer value of 3 and a range of 12" if the chanter is a KNIGHT or 18" if the chanter is a LORD. If answered, pick 1 friendly STORMCAST ETERNALS unit wholly within range and visible to the chanter. Until your next hero phase, if the unmodified hit roll for an attack made by that unit is 6, that attack scores 2 hits on the target instead of 1. Make a wound and save roll for each hit.`,
+        when: [HERO_PHASE],
       },
     ],
   },
