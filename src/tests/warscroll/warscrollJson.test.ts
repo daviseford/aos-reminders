39ef11f8190eda8d7a4a739a33ed12100e760881
Davis Ford <daviseford@users.noreply.github.com>
2021-09-21 08:50:14 -0400

    WSB fixes

--- src/tests/warscroll/warscrollJson.test.ts
+++ src/tests/warscroll/warscrollJson.test.ts
@@ -38,6 +38,25 @@ const getFile = (filename: string): string[] => {
 }
 
 describe('getWarscrollArmyFromJson', () => {
+  it('should correctly read 1632218334853-Warscroll_Builder', () => {
+    const parsedText = getFile('1632218334853-Warscroll_Builder')
+    const res = getWarscrollArmyFromPdf(parsedText)
+    expect(res.selections.units).toContain('Bastian Carthalos')
+    expect(res.errors).toEqual([])
+  })
+
+  it('should correctly read 1632227524917-Warscroll_Builder', () => {
+    const parsedText = getFile('1632227524917-Warscroll_Builder')
+    const res = getWarscrollArmyFromPdf(parsedText)
+    expect(res.selections.units).toContain('Tzaangor Enlightened')
+    expect(res.errors).toEqual([
+      {
+        severity: 'ally-warn',
+        text: 'Allied Daemon Prince can belong to Khorne or Legion Of The First Prince or Nurgle or Slaanesh or Slaves To Darkness or Tzeentch. Please add this unit manually.',
+      },
+    ])
+  })
+
   it('should correctly read 1628815492817-Warscroll_Builder', () => {
     const parsedText = getFile('1628815492817-Warscroll_Builder')
     const res = getWarscrollArmyFromPdf(parsedText)
