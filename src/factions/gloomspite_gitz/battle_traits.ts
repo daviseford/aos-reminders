e989ac9dec2c3c39a7fcf4f719191d8d4b81899e
Davis Ford <daviseford@users.noreply.github.com>
2021-10-20 09:38:34 -0400

    Add Bad Moon effects, Spider Rider rules (fixes #1431)

--- src/factions/gloomspite_gitz/battle_traits.ts
+++ src/factions/gloomspite_gitz/battle_traits.ts
@@ -1,19 +1,61 @@
 import { tagAs } from 'factions/metatagger'
 import { GLOOMSPITE_GITZ } from 'meta/factions'
-import { START_OF_ROUND, TURN_ONE_START_OF_ROUND } from 'types/phases'
+import {
+  COMBAT_PHASE,
+  HERO_PHASE,
+  START_OF_CHARGE_PHASE,
+  START_OF_HERO_PHASE,
+  START_OF_ROUND,
+  TURN_ONE_START_OF_ROUND,
+} from 'types/phases'
 
 const BattleTraits = {
   [GLOOMSPITE_GITZ]: {
     effects: [
       {
-        name: `Bad Moon Setup`,
+        name: `The Bad Moon Setup`,
         desc: `At the start of the first battle round, before determining who has the first turn, the player commanding the Gloomspite Gitz army must pick one corner of the battlefield as the starting location of the Bad Moon. If both players have Gloomspite Gitz armies, then they must roll off and the winner picks the starting location. The Bad Moon is located at the edge of the battlefield in that corner.`,
         when: [TURN_ONE_START_OF_ROUND],
       },
       {
-        name: `Bad Moon Movement`,
+        name: `The Bad Moon Movement`,
         desc: `Starting from the second battle round, before determining who has the first turn, the player commanding the Gloomspite Gitz army must roll a D6. On a 1 the Bad Moon does not move. On a 2-5 it makes 1 move. On a 6 it makes 2 moves.`,
         when: [START_OF_ROUND],
+      },
+      {
+        name: `Light of the Bad Moon - Fangz of the Bad Moon`,
+        desc: `If your army is a Gloomspite Gitz army, at the start of your hero phase you can pick 1 enemy unit and roll a dice. If the roll is equal to or less than the number of models in that unit affected by the light of the Bad Moon, that unit suffers D3 mortal wounds.`,
+        when: [START_OF_HERO_PHASE],
+      },
+      {
+        name: `Light of the Bad Moon - Bad Moon Magic`,
+        desc: `Add 1 to casting rolls for WIZARDS affected by the light of the Bad Moon if they have the GLOOMSPITE GITZ keyword, and subtract 1 from casting rolls for WIZARDS affected by the light of the Bad Moon that do not have the GLOOMSPITE GITZ keyword.`,
+        when: [HERO_PHASE],
+      },
+      {
+        name: `Light of the Bad Moon - Loonatic Inspiration`,
+        desc: `If your general has the GLOOMSPITE GITZ keyword and is affected by the light of the Bad Moon at the start of your hero phase, you receive 1 extra command point.`,
+        when: [START_OF_HERO_PHASE],
+      },
+      {
+        name: `Light of the Bad Moon - Lunar Squigs`,
+        desc: `If all of the models in a friendly SQUIG unit are affected by the light of the Bad Moon at the start of your charge phase, that unit can attempt to charge even if it ran in the same turn.`,
+        when: [START_OF_CHARGE_PHASE],
+      },
+      {
+        name: `Light of the Bad Moon - Moonclan Fungus Brew`,
+        desc: `You can reroll hit rolls of 1 for attacks made by MOONCLAN GROT models while they are affected by the light of the Bad Moon.`,
+        when: [COMBAT_PHASE],
+      },
+      {
+        name: `Light of the Bad Moon - Spiderfang Venom`,
+        desc: `While a SPIDERFANG model is affected by the light of the Bad Moon, its Spider Venom ability causes mortal wounds on an unmodified hit roll of 5+ instead of 6.`,
+        when: [COMBAT_PHASE],
+      },
+      {
+        name: `Light of the Bad Moon - Troggoth Renewal`,
+        desc: `If all of the models in a friendly TROGGOTH unit are affected by the light of the Bad Moon when it uses its Regeneration ability, you can reroll the dice that determines if the ability heals any wounds, or you can double the number of wounds that are healed by the ability if the first roll is successful.`,
+        when: [HERO_PHASE],
       },
     ],
   },
