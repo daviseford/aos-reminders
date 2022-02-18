60d0acb11c9d0edec0d6c220be4997157814103b
Davis Ford <daviseford@users.noreply.github.com>
2022-02-18 16:54:57 -0500

    [ADD-RULE] BoC White Dwarf rules (fixes #1475) (#1483)
    * Add grand strategies, battle tactics, monstrous rampages
    
    * Update scenery.ts
    
    * Fix deps

--- src/factions/beasts_of_chaos/subfactions.ts
+++ src/factions/beasts_of_chaos/subfactions.ts
@@ -7,19 +7,21 @@ import CommandAbilities from './command_abilities'
 import CommandTraits from './command_traits'
 import EndlessSpells from './endless_spells'
 import Flavors from './flavors'
+import GrandStrategies from './grand_strategies'
 import Scenery from './scenery'
 import Spells from './spells'
 import Units from './units'
 
 const subFactions = {
   [BEASTS_OF_CHAOS]: {
-    effects: pickEffects(BattleTraits, [BEASTS_OF_CHAOS]),
+    effects: pickEffects(BattleTraits, [BEASTS_OF_CHAOS, 'Battle Tactics', 'Monstrous Rampages']),
 
     available: {
       artifacts: [Artifacts],
       command_abilities: [CommandAbilities],
       command_traits: [CommandTraits],
       endless_spells: [EndlessSpells],
+      grand_strategies: [GrandStrategies],
       scenery: [Scenery],
       spells: [Spells],
       units: [Units, keyPicker(SlavesToDarknessUnits, ['Chaos Spawn'])],
