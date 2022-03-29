a429da099361e196072cb63b65a4c34bd55ea219
Davis Ford <daviseford@users.noreply.github.com>
2022-03-29 09:04:12 -0400

    Flavors

--- src/factions/fyreslayers/flavors.ts
+++ src/factions/fyreslayers/flavors.ts
@@ -1,41 +1,51 @@
-import { keyPicker, pickEffects } from 'factions/metatagger'
-import Artifacts from './artifacts'
-import BattleTraits from './battle_traits'
-import CommandAbilities from './command_abilities'
-import CommandTraits from './command_traits'
+import { BATTLESHOCK_PHASE, COMBAT_PHASE, START_OF_SETUP, WOUND_ALLOCATION_PHASE } from 'types/phases'
 
 const Flavors = {
-  'Vostarg (Lodge)': {
-    mandatory: {
-      artifacts: [keyPicker(Artifacts, ['Vosaxe'])],
-      command_traits: [keyPicker(CommandTraits, ['Fiery Endurance'])],
-      command_abilities: [keyPicker(CommandAbilities, ['Honour Our Ancestors'])],
-    },
-    effects: [...pickEffects(BattleTraits, ['Vostarg (Lodge)'])],
+  Vostarg: {
+    effects: [
+      {
+        name: `Fearsome Surge`,
+        desc: `Add 1 to hit rolls and wound rolls for attacks made with melee weapons by friendly VOSTARG VULKITE BERZERKERS units that made a charge move in the same turn.`,
+        when: [COMBAT_PHASE],
+      },
+    ],
   },
-  'Greyfyrd (Lodge)': {
-    mandatory: {
-      artifacts: [keyPicker(Artifacts, ['Helm of Obsidia'])],
-      command_traits: [keyPicker(CommandTraits, ['Battle-scarred Veteran'])],
-      command_abilities: [keyPicker(CommandAbilities, ['Expert Cohesion'])],
-    },
-    effects: [...pickEffects(BattleTraits, ['Greyfyrd (Lodge)'])],
+  Greyfyrd: {
+    effects: [
+      {
+        name: `Spoils of Victory`,
+        desc: `Add 1 to the Wounds characteristic of friendly GREYFYRD HEROES that do not have a mount.`,
+        when: [WOUND_ALLOCATION_PHASE],
+      },
+      {
+        name: `Spoils of Victory`,
+        desc: `You can pick 2 additional artefacts of power (pg 59) and give them to 2 GREYFYRD HEROES in your army that do not have a mount.`,
+        when: [START_OF_SETUP],
+      },
+    ],
   },
-  'Hermdar (Lodge)': {
-    mandatory: {
-      artifacts: [keyPicker(Artifacts, ['Tyrant Slayer'])],
-      command_traits: [keyPicker(CommandTraits, ['Warrior Indominate'])],
-      command_abilities: [keyPicker(CommandAbilities, ['Skull-breakers and Oath-takers'])],
-    },
-    effects: [...pickEffects(BattleTraits, ['Hermdar (Lodge)'])],
+  Hermdar: {
+    effects: [
+      {
+        name: `Seize by Force`,
+        desc: `If a friendly HERMDAR unit wholly within enemy territory or wholly within 12" of an objective fails a battleshock test, halve the number of models that flee (rounding up).`,
+        when: [BATTLESHOCK_PHASE],
+      },
+    ],
   },
-  'Lofnir (Lodge)': {
-    mandatory: {
-      artifacts: [keyPicker(Artifacts, ['Igneous Battle-throne'])],
-      command_traits: [keyPicker(CommandTraits, ['Explosive Charge'])],
-      command_abilities: [keyPicker(CommandAbilities, ['Torrent of Magma'])],
-    },
-    effects: [...pickEffects(BattleTraits, ['Lofnir (Lodge)'])],
+  Lofnir: {
+    effects: [
+      {
+        name: `Venerators of Vulcatrix`,
+        desc: `Add 2 to the Wounds characteristic of friendly LOFNIR MAGMADROTH units.`,
+        when: [WOUND_ALLOCATION_PHASE],
+      },
+      {
+        name: `Venerators of Vulcatrix`,
+        desc: `Up to 3 LOFNIR MAGMADROTH units in your army can be given a different mount trait (pg 60) instead of only 1.`,
+        when: [START_OF_SETUP],
+      },
+    ],
   },
 }
 
