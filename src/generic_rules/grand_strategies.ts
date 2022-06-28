4cf0b4878b7a91150ba2b838f856a46dc825ff1b
Davis Ford <daviseford@users.noreply.github.com>
2022-06-28 08:55:44 -0600

    Update grand_strategies.ts

--- src/generic_rules/grand_strategies.ts
+++ src/generic_rules/grand_strategies.ts
@@ -92,6 +92,74 @@ const GenericGrandStrategies: TEntry[] = [
       },
     ],
   },
+
+  // GHB 2022 Grand Strategies
+  {
+    name: `No Place for the Weak`,
+    effects: [
+      {
+        name: `No Place for the Weak`,
+        desc: `When the battle ends, you complete this grand strategy if there are no Battleline units from your opponent's starting army on the battlefield.`,
+        when: [END_OF_GAME],
+        rule_sources: [meta_rule_sources.GHB_2022],
+      },
+    ],
+  },
+  {
+    name: `Tame the Land`,
+    effects: [
+      {
+        name: `Tame the Land`,
+        desc: `When the battle ends, you complete this grand strategy if you control all of the objectives on the battlefield that are wholly outside your territory.`,
+        when: [END_OF_GAME],
+        rule_sources: [meta_rule_sources.GHB_2022],
+      },
+    ],
+  },
+  {
+    name: `Defend What's Ours`,
+    effects: [
+      {
+        name: `Defend What's Ours`,
+        desc: `When the battle ends, you complete this grand strategy if there are no enemy units wholly within your territory.`,
+        when: [END_OF_GAME],
+        rule_sources: [meta_rule_sources.GHB_2022],
+      },
+    ],
+  },
+  {
+    name: `Take What's Theirs`,
+    effects: [
+      {
+        name: `Take What's Theirs`,
+        desc: `When the battle ends, you complete this grand strategy if there are more friendly units than enemy units wholly within your opponent's territory.`,
+        when: [END_OF_GAME],
+        rule_sources: [meta_rule_sources.GHB_2022],
+      },
+    ],
+  },
+  {
+    name: `Demonstration of Strength`,
+    effects: [
+      {
+        name: `Demonstration of Strength`,
+        desc: `When the battle ends, you complete this grand strategy if there are 3 or more GALLETIAN VETERANS units from your starting army on the battlefield.`,
+        when: [END_OF_GAME],
+        rule_sources: [meta_rule_sources.GHB_2022],
+      },
+    ],
+  },
+  {
+    name: `Show of Dominance`,
+    effects: [
+      {
+        name: `Show of Dominance`,
+        desc: `When the battle ends, you complete this grand strategy if there are any friendly GALLETIAN VETERANS units in each quarter of the battlefield.`,
+        when: [END_OF_GAME],
+        rule_sources: [meta_rule_sources.GHB_2022],
+      },
+    ],
+  },
 ]
 
 export default GenericGrandStrategies
