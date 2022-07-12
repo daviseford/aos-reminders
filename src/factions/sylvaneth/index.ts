c389b819bacb56bd6316141f53fb769c7f0e830b
Davis Ford <daviseford@users.noreply.github.com>
2022-07-12 07:59:22 -0600

    Rework shared battle traits (#1539)
    * Re-work shared battle traits
    
    * Update dependencies

--- src/factions/sylvaneth/index.ts
+++ src/factions/sylvaneth/index.ts
@@ -1,6 +1,8 @@
 import { Faction } from 'factions/factionClass'
+import { pickEffects } from 'factions/metatagger'
 import { ORDER } from 'meta/alliances'
 import { SYLVANETH } from 'meta/factions'
+import BattleTraits from './battle_traits'
 import rule_sources from './rule_sources'
 import SubFactions from './subfactions'
 
@@ -9,5 +11,6 @@ export const SylvanethFaction = new Faction(
   ORDER,
   SubFactions,
   'Glades',
-  rule_sources.BATTLETOME_SYLVANETH
+  rule_sources.BATTLETOME_SYLVANETH,
+  pickEffects(BattleTraits, [SYLVANETH, 'Battle Tactics'])
 )
