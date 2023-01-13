8bae9c0e135d8387d6e6bc5abc8e99b7227f0371
Davis Ford <daviseford@users.noreply.github.com>
2023-01-13 12:42:53 -0700

    Sylvaneth January 2023 FAQ

--- src/factions/sylvaneth/rule_sources.ts
+++ src/factions/sylvaneth/rule_sources.ts
@@ -8,6 +8,11 @@ const rule_sources = {
     type: 'errata',
     url: 'https://www.warhammer-community.com/wp-content/uploads/2019/08/aYMD9OEkOOdb02oN.pdf',
   },
+  ERRATA_JANUARY_2023: {
+    name: 'Errata (January 2023)',
+    type: 'errata',
+    url: 'https://www.warhammer-community.com/wp-content/uploads/2019/08/7iIV0GHcihoKuRXU.pdf',
+  },
 }
 
 export default rule_sources
