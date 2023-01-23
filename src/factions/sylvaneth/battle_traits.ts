20fa5873ef070ae915c032ed4dbdc5115cec52f9
Dave Fatkin <dfatkin@fool.com>
2023-01-23 15:16:13 -0500

    Sylvaneth

--- src/factions/sylvaneth/battle_traits.ts
+++ src/factions/sylvaneth/battle_traits.ts
@@ -7,7 +7,6 @@ import {
   END_OF_MOVEMENT_PHASE,
   HERO_PHASE,
   START_OF_HERO_PHASE,
-  START_OF_ROUND,
   WARDS_PHASE,
 } from 'types/phases'
 import rule_sources from './rule_sources'
@@ -90,27 +89,27 @@ const BattleTraits = {
       {
         name: 'Eradicate Trespassers',
         desc: `Pick 1 enemy unit within 6" of a friendly Awakened Wyldwood. You complete this battle tactic if that unit is destroyed during this turn.`,
-        when: [START_OF_ROUND],
+        when: [START_OF_HERO_PHASE],
       },
       {
         name: `Harness the Spirit Paths`,
         desc: `You complete this battle tactic if any models in a friendly SYLVANETH unit that was set up using the From the Woodland Depths battle trait this turn make a charge move this turn.`,
-        when: [START_OF_ROUND],
+        when: [START_OF_HERO_PHASE],
       },
       {
         name: `Balance the Cycle`,
         desc: `Pick 1 enemy unit within 12" of an overgrown terrain feature or friendly Awakened Wyldwood. You complete this battle tactic if that unit is destroyed by an attack made by a friendly SYLVANETH unit that was added to your army this turn.`,
-        when: [START_OF_ROUND],
+        when: [START_OF_HERO_PHASE],
       },
       {
         name: `March of the Forest Lords`,
         desc: `Pick 1 enemy MONSTER on the battlefield. You complete this battle tactic if that MONSTER is slain by an attack made by a friendly SPIRIT OF DURTHU, TREELORD ANCIENT or TREELORD during this turn.`,
-        when: [START_OF_ROUND],
+        when: [START_OF_HERO_PHASE],
       },
       {
         name: `Unleash Ghyran's Wrath`,
         desc: `Pick 1 friendly SYLVANETH WIZARD on the battlefield. You complete this battle tactic if a spell successfully cast by that WIZARD, or an endless spell summoned by that WIZARD, destroys an enemy unit this turn.`,
-        when: [START_OF_ROUND],
+        when: [START_OF_HERO_PHASE],
       },
     ],
   },
