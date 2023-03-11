b32cf612584be084e36f5719d78e6fc8c56871b1
Tom Gattenhof <tom@pixeltom.net>
2023-03-11 22:40:29 +1100

    KO Ship command abilities

--- src/factions/kharadron_overlords/command_abilities.ts
+++ src/factions/kharadron_overlords/command_abilities.ts
@@ -1,8 +1,41 @@
 import { tagAs } from 'factions/metatagger'
-import { START_OF_CHARGE_PHASE, START_OF_HERO_PHASE, START_OF_SHOOTING_PHASE } from 'types/phases'
+import {
+  END_OF_MOVEMENT_PHASE,
+  MOVEMENT_PHASE,
+  START_OF_CHARGE_PHASE,
+  START_OF_HERO_PHASE,
+  START_OF_SHOOTING_PHASE,
+} from 'types/phases'
 import rule_sources from './rule_sources'
 
 const CommandAbilities = {
+  'Combat Landing': {
+    effects: [
+      {
+        name: `Combat Landing`,
+        desc: `You can use this command ability at the end of your movement phase. The unit that receives the command must be a friendly TRANSPORT VESSEL. Any units embarked in that TRANSPORT VESSEL can immediately disembark. A unit that disembarks in this way cannot move in the same turn.`,
+        when: [END_OF_MOVEMENT_PHASE],
+      },
+    ],
+  },
+  'Fly High': {
+    effects: [
+      {
+        name: `Fly High`,
+        desc: `You can use this command ability at the end of your movement phase. The unit that receives the command must be a friendly SKYVESSEL more than 3" from all enemy units. Remove that unit from the battlefield and set it up anywhere on the battlefield more than 9" from all enemy units and more than 1" from all terrain features.`,
+        when: [END_OF_MOVEMENT_PHASE],
+      },
+    ],
+  },
+  Disengage: {
+    effects: [
+      {
+        name: `Disengage`,
+        desc: `You can use this command ability in your movement phase. The unit that receives the command must be a friendly SKYVESSEL that has not yet moved in that phase. That unit and any units embarked in it can retreat and still shoot later in the turn.`,
+        when: [MOVEMENT_PHASE],
+      },
+    ],
+  },
   'By Grungni, I Have My Eye On You!': {
     effects: [
       {
