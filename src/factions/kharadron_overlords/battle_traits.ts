0eafbfdbab87fedd8ee30c1fae43d6752b89952a
Tom Gattenhof <tom@pixeltom.net>
2023-03-27 14:06:35 +1100

    Update KO Battle Tactics

--- src/factions/kharadron_overlords/battle_traits.ts
+++ src/factions/kharadron_overlords/battle_traits.ts
@@ -8,7 +8,6 @@ import {
   TURN_THREE_START_OF_HERO_PHASE,
   TURN_TWO_START_OF_HERO_PHASE,
 } from 'types/phases'
-import rule_sources from './rule_sources'
 
 const BattleTraits = {
   [KHARADRON_OVERLORDS]: {
@@ -25,26 +24,38 @@ const BattleTraits = {
     effects: [
       {
         name: `Bombing Run`,
-        desc: `Pick 1 enemy unit. You complete this battle tactic if that unit is destroyed during this turn by a friendly model using the 'Bomb Racks' ability.`,
+        desc: `Pick 1 enemy unit. You complete this battle tactic if that unit is destroyed during this turn by the Bomb Racks ability of a friendly unit.`,
         when: [START_OF_HERO_PHASE],
-        rule_sources: [rule_sources.WHITE_DWARF_MAY_2022],
       },
       {
         name: `Mobilise the Fleet`,
-        desc: `You cannot pick this battle tactic in the first battle round. Pick 3 friendly units that are not garrisoned within SKYVESSELS. You complete this battle tactic at the end of this turn if those units are all garrisoned within SKYVESSELS.`,
+        desc: `You cannot pick this battle tactic in the first battle round. Pick 3 friendly units that are not embarked in SKYVESSELS. You complete this battle tactic at the end of the turn if those units are all embarked in SKYVESSELS.`,
         when: [
           TURN_TWO_START_OF_HERO_PHASE,
           TURN_THREE_START_OF_HERO_PHASE,
           TURN_FOUR_START_OF_HERO_PHASE,
           TURN_FIVE_START_OF_HERO_PHASE,
         ],
-        rule_sources: [rule_sources.WHITE_DWARF_MAY_2022, rule_sources.ERRATA_JANUARY_2023],
       },
       {
         name: `Boots on the Ground`,
-        desc: `Pick 3 friendly units garrisoned in SKYVESSELS. You complete this battle tactic at the end of the turn if those units are all on the battlefield, are not garrisoned and are wholly within enemy territory.`,
+        desc: `Pick 3 friendly units embarked in SKYVESSELS. You complete this battle tactic at the end of the turn if those units are not embarked and are wholly within enemy territory.`,
         when: [START_OF_HERO_PHASE],
-        rule_sources: [rule_sources.WHITE_DWARF_MAY_2022, rule_sources.ERRATA_OCTOBER_2022],
+      },
+      {
+        name: `Opening Salvo`,
+        desc: `You can only pick this battle tactic if no units have been destroyed in the battle. You complete this battle tactic if an enemy unit is destroyed in your shooting phase this turn.`,
+        when: [START_OF_HERO_PHASE],
+      },
+      {
+        name: `Blast 'em to Smithereens`,
+        desc: `Pick 1 objective on the battlefield within 12" of any enemy units. You complete this battle tactic if there are no enemy units within 12" of that objective at the end of this turn.`,
+        when: [START_OF_HERO_PHASE],
+      },
+      {
+        name: `Stake a Claim`,
+        desc: `You can pick this tactic only if you control fewer objectives than your opponent. You complete this battle tactic if you control more objectives than your opponent at the end of this turn.`,
+        when: [START_OF_HERO_PHASE],
       },
     ],
   },
