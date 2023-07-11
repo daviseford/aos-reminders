ac0685aff4f5935f2b4faf24c22b5b856603269a
rfrank14 <regan.frank90@gmail.com>
2023-07-12 00:19:57 +1000

    battlescroll update combined the effects

--- src/factions/sons_of_behemat/battalions.ts
+++ src/factions/sons_of_behemat/battalions.ts
@@ -6,20 +6,29 @@ import meta_rule_sources from 'meta/rule_sources'
 
 // Battalions from White Dwarf August 2021
 const Battalions = {
-  'Bosses of the Stomp (Unified)': {
-    effects: [{ ...OneDropDeploymentEffect, rule_sources: [rule_sources.WHITE_DWARF_AUGUST_2021] }],
-  },
-  'Bosses of the Stomp (Magnificent)': {
+  'Bosses of the Stomp': {
     effects: [
-      { ...MagnificentBattalionEffect, rule_sources: [rule_sources.WHITE_DWARF_AUGUST_2021] },
-      { ...OneDropDeploymentEffect, rule_sources: [meta_rule_sources.BATTLESCROLL_ANDTOR_JULY_2023] },
+      {
+        ...MagnificentBattalionEffect,
+        rule_sources: [rule_sources.WHITE_DWARF_AUGUST_2021, meta_rule_sources.BATTLESCROLL_ANDTOR_JULY_2023],
+      },
+      {
+        ...OneDropDeploymentEffect,
+        rule_sources: [rule_sources.WHITE_DWARF_AUGUST_2021, meta_rule_sources.BATTLESCROLL_ANDTOR_JULY_2023],
+      },
     ],
   },
-  'Footsloggas (Unified)': {
-    effects: [{ ...OneDropDeploymentEffect, rule_sources: [rule_sources.WHITE_DWARF_AUGUST_2021] }],
-  },
-  'Footsloggas (Swift)': {
-    effects: [{ ...SwiftBattalionEffect, rule_sources: [rule_sources.WHITE_DWARF_AUGUST_2021] }],
+  Footsloggas: {
+    effects: [
+      {
+        ...OneDropDeploymentEffect,
+        rule_sources: [rule_sources.WHITE_DWARF_AUGUST_2021, meta_rule_sources.BATTLESCROLL_ANDTOR_JULY_2023],
+      },
+      {
+        ...SwiftBattalionEffect,
+        rule_sources: [rule_sources.WHITE_DWARF_AUGUST_2021, meta_rule_sources.BATTLESCROLL_ANDTOR_JULY_2023],
+      },
+    ],
   },
 }
 
