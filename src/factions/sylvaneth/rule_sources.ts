8447b432f8d1264dd33f476d86d03e40e7db023c
Davis Ford <daviseford@users.noreply.github.com>
2023-11-26 08:18:30 -0700

    Add Sylvaneth November Errata

--- src/factions/sylvaneth/rule_sources.ts
+++ src/factions/sylvaneth/rule_sources.ts
@@ -13,6 +13,11 @@ const rule_sources = {
     type: 'errata',
     url: 'https://www.warhammer-community.com/wp-content/uploads/2019/08/7iIV0GHcihoKuRXU.pdf',
   },
+  ERRATA_NOVEMBER_2023: {
+    name: 'Errata (November 2023)',
+    type: 'errata',
+    url: 'https://www.warhammer-community.com/wp-content/uploads/2019/08/OoFjYBMRphTA30o3.pdf',
+  },
 }
 
 export default rule_sources
