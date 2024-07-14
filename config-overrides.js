82e5f49af609672d2335bf3f8705e5c3e904538c
Davis Ford <daviseford@users.noreply.github.com>
2024-07-14 13:00:10 -0600

    Upgrade to react-scripts v5 (#1706)

--- config-overrides.js
+++ config-overrides.js
@@ -18,5 +18,25 @@ module.exports = function override(config, env) {
   // Why do both pdf libraries make Webpack unhappy?
   // That is a good question :)
 
+  /**
+   * Fixes this error:
+   * Module not found: Error: Can't resolve 'D:/Projects/aos-reminders/node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/inherits' in 'D:\Projects\aos-reminders\node_modules\luxon\src\zones'
+   * Did you mean 'inherits.js'?
+   * BREAKING CHANGE: The request 'D:/Projects/aos-reminders/node_modules/babel-preset-react-app/node_modules/@babel/runtime/helpers/esm/inherits' failed to resolve only because it was resolved as fully specified
+   * (probably because the origin is strict EcmaScript Module, e. g. a module with javascript mimetype, a '*.mjs' file, or a '*.js' file where the package.json contains '"type": "module"').
+   * The extension in the request is mandatory for it to be fully specified.
+   * Add the extension to the request.
+   *
+   *
+   * Solution found here:
+   * https://stackoverflow.com/questions/70964723/webpack-5-in-ceate-react-app-cant-resolve-not-fully-specified-routes
+   */
+  config.module.rules.push({
+    test: /\.m?js/,
+    resolve: {
+      fullySpecified: false,
+    },
+  })
+
   return config
 }
